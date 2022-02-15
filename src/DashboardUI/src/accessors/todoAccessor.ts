78f9937b0e4f7a90cfa40333247a1a7dde882811
Branden Barber <bbarber@dontpaniclabs.com>
2022-02-15 13:22:59 -0600

    Add formatting settings for 2 spaces for ts/scss files

--- src/DashboardUI/src/accessors/todoAccessor.ts
+++ src/DashboardUI/src/accessors/todoAccessor.ts
@@ -2,8 +2,8 @@ import axios from "axios";
 import { Todo } from "../data-contracts/todo";
 
 export const getTodos = async () => {
-    const { data } = await axios.get<Todo[]>(
-        `${process.env.REACT_APP_WEBAPI_URL}/todo/findalltodos`
-    );
-    return data;
+  const { data } = await axios.get<Todo[]>(
+    `${process.env.REACT_APP_WEBAPI_URL}/todo/findalltodos`
+  );
+  return data;
 };
