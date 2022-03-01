9cf7a911ffeed23d372135394130cf70599816bb
Branden Barber <bbarber@dontpaniclabs.com>
2022-03-01 15:51:52 -0600

    Format string cleaner

--- src/API/WesternStatesWater.WestDaat.Engines/WaterAllocationEngine.cs
+++ src/API/WesternStatesWater.WestDaat.Engines/WaterAllocationEngine.cs
@@ -1,5 +1,8 @@
 ﻿using WesternStatesWater.WestDaat.Accessors;
 using System;
+using System.Dynamic;
+using System.Linq;
+using System.Text.Json;
 using Microsoft.Extensions.Logging;
 using WesternStatesWater.WestDaat.Accessors.EntityFramework;
 
@@ -11,70 +14,70 @@ namespace WesternStatesWater.WestDaat.Engines
         {
         }
 
-        public string BuildGeoconnexJson(SitesDim sitesDim)
+        public string BuildGeoconnexJson(SitesDim data)
         {
-            var goeConnexJson = "{"
-                                + "   \"@context\": ["
-                                + "       {"
-                                + "           \"schema\": \"https://schema.org/\","
-                                + "           \"geojson\": \"https://purl.org/geojson/vocab#\","
-                                + "           \"Feature\": \"geojson:Feature\","
-                                + "           \"FeatureCollection\": \"geojson:FeatureCollection\","
-                                + "           \"Point\": \"geojson:Point\","
-                                + "           \"bbox\": {"
-                                + "               \"@container\": \"@list\","
-                                + "               \"@id\": \"geojson:bbox\""
-                                + "           },"
-                                + "           \"coordinates\": {"
-                                + "               \"@container\": \"@list\","
-                                + "               \"@id\": \"geojson:coordinates\""
-                                + "           },"
-                                + "           \"features\": {"
-                                + "               \"@container\": \"@set\","
-                                + "               \"@id\": \"geojson:features\""
-                                + "           },"
-                                + "           \"geometry\": \"geojson:geometry\","
-                                + "           \"id\": \"@id\","
-                                + "           \"properties\": \"geojson:properties\","
-                                + "           \"type\": \"@type\""
-                                + "       },"
-                                + "       {"
-                                + "           \"schema\": \"https://schema.org/\","
-                                + "           \"id\": \"schema:name\","
-                                + "           \"LINK\": \"schema:url\""
-                                + "       },"
-                                + "       {"
-                                + "           \"uri\": \"@id\""
-                                + "       }"
-                                + "   ],"
-                                + "   \"type\": \"Feature\","
-                                + "   \"geometry\": {"
-                                + "       \"type\": \"Point\","
-                                + "       \"coordinates\": ["
-                                + data.longitude + ","
-                                + data.latitude
-                                + "       ]"
-                                + "   },"
-                                + "   \"properties\": {"
-                                + "       \"fid\": 101913,"
-                                + "       \"StateID\":"
-                                + data.allocationBridgeSitesFacts[0].allocationAmount.organization.state + ","
-                                + "       \"DataSourceOrganizationID\":"
-                                + data.allocationBridgeSitesFacts[0].allocationAmount.organization.organizationUuid +
-                                ","
-                                + "       \"id\": \"https://geoconnex.us/wade/sites/\"" + data.siteUuid + ","
-                                + "       \"uri\": \"https://geoconnex.us/wade/sites/\"" + data.siteUuid + ","
-                                + "       \"LINK\": \"https://wade-api-qa.azure-api.net/v1/SiteAllocationAmounts?SiteUUID=\""
-                                + data.siteUuid
-                                + "   },"
-                                + "   \"id\": \"https://geoconnex.us/wade/sites/\"" + data.siteUuid
-                                + "}";
-            
-            var data = new SitesDim();
+            var geoConnexJson = string.Format(GEOCONNEX_JSON_TEMPLATE,
+                data.Longitude,
+                data.Latitude,
+                data.AllocationBridgeSitesFact.First().AllocationAmount.Organization.State,
+                data.AllocationBridgeSitesFact.First().AllocationAmount.Organization.OrganizationUuid,
+                data.SiteUuid
+            );
 
-            return "";
+            return geoConnexJson;
         }
-    }
-}
+        
+        private const string GEOCONNEX_JSON_TEMPLATE = @"
+{{
+    ""@context"": [
+        {{
+            ""schema"": ""https://schema.org/"",
+            ""geojson"": ""https://purl.org/geojson/vocab#"",
+            ""Feature"": ""geojson:Feature"",
+            ""FeatureCollection"": ""geojson:FeatureCollection"",
+            ""Point"": ""geojson:Point"",
+            ""bbox"": {{
+                ""@container"": ""@list"",
+                ""@id"": ""geojson:bbox""
+            }},
+            ""coordinates"": {{
+                ""@container"": ""@list"",
+                ""@id"": ""geojson:coordinates""
+            }},
+            ""features"": {{
+                ""@container"": ""@set"",
+                ""@id"": ""geojson:features""
+            }},
+            ""geometry"": ""geojson:geometry"",
+            ""id"": ""@id"",
+            ""properties"": ""geojson:properties"",
+            ""type"": ""@type""
+        }},
+        {{
+            ""schema"": ""https://schema.org/"",
+            ""id"": ""schema:name"",
+            ""LINK"": ""schema:url""
+        }},
+        {{
+            ""uri"": ""@id""
+        }}
+    ],
+    ""type"": ""Feature"",
+    ""geometry"": {{
+        ""type"": ""Point"",
+        ""coordinates"": [{0},{1}]
+    }},
+    ""properties"": {{
+        ""fid"": 101913,
+        ""StateID"": {2},
+        ""DataSourceOrganizationID"": {3},
+        ""id"": ""https://geoconnex.us/wade/sites/{4}"",
+        ""uri"": ""https://geoconnex.us/wade/sites/{4}"",
+        ""LINK"": ""https://wade-api-qa.azure-api.net/v1/SiteAllocationAmounts?SiteUUID={4}""
+    }},
+    ""id"": ""https://geoconnex.us/wade/sites/{4}""
+}}
+";
 
+    }
 }
--- src/API/WesternStatesWater.WestDaat.Managers/WaterAllocationManager.cs
+++ src/API/WesternStatesWater.WestDaat.Managers/WaterAllocationManager.cs
@@ -1,30 +1,28 @@
 ﻿using WesternStatesWater.WestDaat.Engines;
 using Microsoft.Extensions.Logging;
 using System;
-using System.Text.Json;
 using WesternStatesWater.WestDaat.Accessors;
-using WesternStatesWater.WestDaat.Accessors.EntityFramework;
 
 namespace WesternStatesWater.WestDaat.Managers
 {
     public class WaterAllocationManager : ManagerBase, IWaterAllocationManager
     {
+        private readonly IWaterAllocationEngine _waterAllocationEngine;
         private readonly ISiteAccessor _siteAccessor;
 
-        public WaterAllocationManager(ISiteAccessor siteAccessor, ILogger<WaterAllocationManager> logger) : base(logger)
+        public WaterAllocationManager(ISiteAccessor siteAccessor, IWaterAllocationEngine waterAllocationEngine,
+            ILogger<WaterAllocationManager> logger) : base(logger)
         {
             _siteAccessor = siteAccessor;
+            _waterAllocationEngine = waterAllocationEngine;
         }
 
         public string GetWaterAllocationSiteDetailsById(string siteUuid)
         {
             var data = _siteAccessor.GetWaterAllocationSiteDetailsById(siteUuid);
-            var json = JsonSerializer.Serialize(new
-            {
-                A = data.County,
-                B = data.Latitude,
-                C = data.Longitude
-            });
+
+            var json = _waterAllocationEngine.BuildGeoconnexJson(data);
+
             return json;
         }
     }
