6b25a87cfb60029ffe22166dd96a8c5144e92489
Nathan Wilkinson <nwilkinson@dontpaniclabs.com>
2022-03-03 14:41:34 -0600

    more lf merge issues

--- src/API/WesternStatesWater.WestDaat.Tests.Utilities.Tests/Assembly.cs
+++ src/API/WesternStatesWater.WestDaat.Tests.Utilities.Tests/Assembly.cs
@@ -1,16 +1,8 @@
 ﻿global using Bogus;
-
 global using FluentAssertions;
-
 global using Microsoft.VisualStudio.TestTools.UnitTesting;
-
 global using Moq;
-
 global using System;
-
 global using System.Collections.Generic;
-
 global using System.Linq;
-
 global using System.Threading.Tasks;
-
