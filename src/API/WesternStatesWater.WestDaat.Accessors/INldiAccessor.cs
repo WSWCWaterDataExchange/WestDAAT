dfac78fd278c82290a706c3f269f0134ddbbf730
Nathan Wilkinson <nwilkinson@dontpaniclabs.com>
2022-03-03 14:06:36 -0600

    fix weird linespace issue from merge

--- src/API/WesternStatesWater.WestDaat.Accessors/INldiAccessor.cs
+++ src/API/WesternStatesWater.WestDaat.Accessors/INldiAccessor.cs
@@ -1,17 +1,10 @@
 ﻿using GeoJSON.Text.Feature;
-
 using WesternStatesWater.WestDaat.Common.Contracts;
 
-
 namespace WesternStatesWater.WestDaat.Accessors
-
 {
     public interface INldiAccessor
-
     {
-
         Task<FeatureCollection> GetNldiFeatures(double latitude, double longitude, NldiDirections directions, NldiDataPoints dataPoints);
-
     }
-
 }
