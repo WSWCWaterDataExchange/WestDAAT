71d462dec46840c1952d64786c62241e9b1a71ca
Nathan Wilkinson <nwilkinson@dontpaniclabs.com>
2022-03-03 11:54:13 -0600

    Shuffle some classes around

---
+++ src/API/WesternStatesWater.WestDaat.Common/Contracts/NldiDataPoints.cs
@@ -0,0 +1,11 @@
+﻿namespace WesternStatesWater.WestDaat.Common.Contracts
+{
+    [Flags]
+    public enum NldiDataPoints
+    {
+        None = 0,
+        Wade = 1 << 1,
+        Usgs = 1 << 2,
+        Epa = 1 << 3
+    }
+}
