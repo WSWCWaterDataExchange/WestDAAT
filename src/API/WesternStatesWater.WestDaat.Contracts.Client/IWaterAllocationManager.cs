f07b30ddf26bc18d53d944504153ecc081606472
Nathan Wilkinson <nwilkinson@dontpaniclabs.com>
2022-03-03 11:54:35 -0600

    Create WaterAllocationManager

---
+++ src/API/WesternStatesWater.WestDaat.Contracts.Client/IWaterAllocationManager.cs
@@ -0,0 +1,10 @@
+﻿using GeoJSON.Text.Feature;
+using WesternStatesWater.WestDaat.Common.Contracts;
+
+namespace WesternStatesWater.WestDaat.Contracts.Client
+{
+    public interface IWaterAllocationManager
+    {
+        Task<FeatureCollection> GetNldiFeatures(double latitude, double longitude, NldiDirections directions, NldiDataPoints dataPoints);
+    }
+}
---
+++ src/API/WesternStatesWater.WestDaat.Managers/WaterAllocationManager.cs
@@ -0,0 +1,23 @@
+﻿using GeoJSON.Text.Feature;
+using Microsoft.Extensions.Logging;
+using WesternStatesWater.WestDaat.Accessors;
+using WesternStatesWater.WestDaat.Common.Contracts;
+using WesternStatesWater.WestDaat.Contracts.Client;
+
+namespace WesternStatesWater.WestDaat.Managers
+{
+    public sealed class WaterAllocationManager : ManagerBase, IWaterAllocationManager
+    {
+        public WaterAllocationManager(INldiAccessor nldiAccessor, ILogger<WaterAllocationManager> logger) : base(logger)
+        {
+            _nldiAccessor = nldiAccessor;
+        }
+
+        private readonly INldiAccessor _nldiAccessor;
+
+        async Task<FeatureCollection> IWaterAllocationManager.GetNldiFeatures(double latitude, double longitude, NldiDirections directions, NldiDataPoints dataPoints)
+        {
+            return await _nldiAccessor.GetNldiFeatures(latitude, longitude, directions, dataPoints);
+        }
+    }
+}
