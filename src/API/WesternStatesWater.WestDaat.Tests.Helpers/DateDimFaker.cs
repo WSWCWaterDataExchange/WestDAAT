dae5571adaff4b3132ff334254539384d4e236b7
Nathan Wilkinson <nwilkinson@dontpaniclabs.com>
2022-03-22 23:46:45 -0500

    fix bad priority date mapping

--- src/API/WesternStatesWater.WestDaat.Tests.Helpers/DateDimFaker.cs
+++ src/API/WesternStatesWater.WestDaat.Tests.Helpers/DateDimFaker.cs
@@ -2,12 +2,12 @@ using WesternStatesWater.WestDaat.Accessors.EntityFramework;
 
 namespace WesternStatesWater.WestDaat.Tests.Helpers
 {
-    internal class DateDimFaker : Faker<DateDim>
+    public class DateDimFaker : Faker<DateDim>
     {
         public DateDimFaker()
         {
             this.RuleFor(a => a.Date, b => b.Date.Past(100).Date)
-                .RuleFor(a => a.Year, b => b.Date.Past(100).Year.ToString());
+                .RuleFor(a => a.Year, (b, o) => o.Date.Year.ToString());
         }
     }
 }
