208438a6a51807f3c9b6d355fa7c8e2b239f855e
Nathan Wilkinson <nwilkinson@dontpaniclabs.com>
2022-03-29 07:45:07 -0500

    better whitespace management

--- src/DashboardUI/src/components/MapPopupCard.tsx
+++ src/DashboardUI/src/components/MapPopupCard.tsx
@@ -12,7 +12,11 @@ interface MapPopupCardProps {
 export function MapPopupCard(props: MapPopupCardProps) {
   return (
     <Card className="map-popup-card">
-      <CardHeader><div className="d-flex justify-content-between flex-nowrap">{props.children.header}<button type="button" onClick={props.onClosePopup} className="btn-close map-popup-close-btn"></button></div></CardHeader>
+      <CardHeader>
+        <div className="d-flex justify-content-between flex-nowrap">
+          {props.children.header}<button type="button" onClick={props.onClosePopup} className="btn-close map-popup-close-btn"></button>
+        </div>
+      </CardHeader>
       <div className="card-body">
         {props.children.body}
       </div>
