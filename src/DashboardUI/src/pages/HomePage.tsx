9e4abb177e9b730dfbc1c946b6bbc5b57f42868d
Rafael Simosa <rsimosa@dontpaniclabs.com>
2022-05-06 13:06:26 -0500

    removing added space

--- src/DashboardUI/src/pages/HomePage.tsx
+++ src/DashboardUI/src/pages/HomePage.tsx
@@ -40,7 +40,6 @@ function HomePage() {
   return (
     <div className="home-page d-flex flex-column">
       <SiteNavbar onTabClick={setCurrentTab} currentTab={currentTab} showContactModal={shouldShowContactModal} />
-
       <div className="d-flex flex-grow-1 overflow-hidden">
         <SidePanel currentTab={currentTab} />
         <div className="flex-grow-1">
