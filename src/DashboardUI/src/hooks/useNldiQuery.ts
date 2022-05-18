d43c4c7905d3f4c5dec4444aa0fa50d58891c826
Rafael Simosa <rsimosa@dontpaniclabs.com>
2022-05-18 09:41:10 -0500

    removing ternary and use the enabled from the useQuery

--- src/DashboardUI/src/hooks/useNldiQuery.ts
+++ src/DashboardUI/src/hooks/useNldiQuery.ts
@@ -5,9 +5,9 @@ import { DataPoints, Directions } from '../data-contracts/nldi';
 export function useNldiFeatures(latitude: number | null, longitude: number | null, isEnabled: boolean) {
   return useQuery(
     ['nldi.features', latitude, longitude],
-    () => isEnabled ? getNldiFeatures(latitude ?? 0, longitude ?? 0, Directions.Upsteam | Directions.Downsteam, DataPoints.Usgs | DataPoints.Epa | DataPoints.Wade) : null,
+    () => getNldiFeatures(latitude ?? 0, longitude ?? 0, Directions.Upsteam | Directions.Downsteam, DataPoints.Usgs | DataPoints.Epa | DataPoints.Wade),
     {
-      enabled: !!latitude && !!longitude
+      enabled: !!latitude && !!longitude && isEnabled
     }
   );
 }
