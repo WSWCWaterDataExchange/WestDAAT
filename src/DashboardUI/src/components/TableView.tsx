688d51e4d0ca483cd4047fc97c1aba469d04e486
Everett Rhodes <everett.rhodes@gmail.com>
2022-05-24 15:28:36 -0500

    fixed some file formatting

--- src/DashboardUI/src/components/TableView.tsx
+++ src/DashboardUI/src/components/TableView.tsx
@@ -12,27 +12,27 @@ function TableView(props: TableViewProps) {
   const [show, setshow] = useState(false);
 
   const handleClose = () => setshow(false);
-  const toggleShow = () => setshow((s:boolean) => !s);
+  const toggleShow = () => setshow((s: boolean) => !s);
 
   return <>
     <Button type="button" className={`table-view-toggle-btn ${show ? "toggle-on" : null}`} onClick={toggleShow}>
       ANALYTICS & TABLE
       <Icon path={show ? mdiChevronDown : mdiChevronUp} size="1.5em" />
-    </Button>    
-    <Offcanvas 
-    show={show} 
-    onHide={handleClose} 
-    placement={'bottom'} 
-    backdrop={false} 
-    scroll={true} 
-    container={props.containerRef}>
-        <Offcanvas.Header closeButton>
-          <Offcanvas.Title>Loading filter data</Offcanvas.Title>
-        </Offcanvas.Header>
-        <Offcanvas.Body>
-          <ProgressBar animated now={100} />
-        </Offcanvas.Body>
-      </Offcanvas>
+    </Button>
+    <Offcanvas
+      show={show}
+      onHide={handleClose}
+      placement={'bottom'}
+      backdrop={false}
+      scroll={true}
+      container={props.containerRef}>
+      <Offcanvas.Header closeButton>
+        <Offcanvas.Title>Loading filter data</Offcanvas.Title>
+      </Offcanvas.Header>
+      <Offcanvas.Body>
+        <ProgressBar animated now={100} />
+      </Offcanvas.Body>
+    </Offcanvas>
   </>
 }
 
