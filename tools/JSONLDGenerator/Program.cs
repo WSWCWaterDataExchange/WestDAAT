a455f0b6ac56160f42462467d487e1cda2d31f30
Rafael Simosa <rsimosa@dontpaniclabs.com>
2022-06-14 08:34:19 -0500

    changing blob name and pushing to app directory

--- tools/JSONLDGenerator/Program.cs
+++ tools/JSONLDGenerator/Program.cs
@@ -85,7 +85,7 @@ namespace WesternStatesWater.WestDaat.Tools.JSONLDGenerator
 
             if (blobStorageSdk != null)
             {
-                await blobStorageSdk.CreateAndUploadAsync("jsonlds", "Jsonld.json", stream, true);
+                await blobStorageSdk.UploadAsync("$web", "Jsonld.json", stream, true);
             }
             else
             {
