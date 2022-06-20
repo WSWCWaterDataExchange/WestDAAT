7dbc8c139f6acebc67bc22ec6260fea2db47a847
Everett Rhodes <everett.rhodes@gmail.com>
2022-06-20 09:36:52 -0500

    adding FE data contracts

---
+++ src/DashboardUI/src/data-contracts/WaterRightsSearchCriteria.ts
@@ -0,0 +1,18 @@
+export interface WaterRightsSearchCriteria {
+  pageNumber: number;
+  beneficialUses: string[];
+  ownerClassifications: string[];
+  waterSourceTypes: string[];
+  riverBasinNames: string[];
+  filterGeometry: string; //geojson string
+  states: string[];
+  allocationOwner: string;
+  expemptofVolumeFlowPriority: string;
+  minimumFlow?: number;
+  maximumFlow?: number;
+  minimumVolume?: number;
+  maximumVolume?: number;
+  podOrPou: string;
+  minimumPriorityDate?: Date;
+  maximumPriorityDate?: Date;
+}
---
+++ src/DashboardUI/src/data-contracts/WaterRightsSearchResults.ts
@@ -0,0 +1,6 @@
+import { WaterRightsSearchDetail } from "./WaterRightsSearchDetail";
+
+export interface WaterRightsSearchResults {
+  currentPageNumber: number;
+  waterRightsDetails: WaterRightsSearchDetail[];  
+}
