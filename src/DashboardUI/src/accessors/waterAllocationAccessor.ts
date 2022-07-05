6f01d0ae102c67d58466f98cc82f29f58129b109
Sarah Kenny <skenny@dontpaniclabs.com>
2022-07-05 14:33:55 -0500

    Fixed bug where water rights page wasn't loading

--- src/DashboardUI/src/accessors/waterAllocationAccessor.ts
+++ src/DashboardUI/src/accessors/waterAllocationAccessor.ts
@@ -7,7 +7,7 @@ import axios from 'axios';
 import { WaterRightsSearchCriteria } from '../data-contracts/WaterRightsSearchCriteria';
 import { WaterRightsSearchResults } from '../data-contracts/WaterRightsSearchResults';
 
-export const getWaterRightDetails = async (waterRightId: number) => {
+export const getWaterRightDetails = async (waterRightId: string) => {
   const { data } = await axios.get<WaterRightDetails>(
     `${process.env.REACT_APP_WEBAPI_URL}WaterRights/${waterRightId}`
   );
@@ -19,21 +19,21 @@ export const findWaterRight = async (searchCriteria: WaterRightsSearchCriteria) 
   return data;
 };
 
-export const getWaterRightSiteInfoList = async (waterRightId: number) => {
+export const getWaterRightSiteInfoList = async (waterRightId: string) => {
   const { data } = await axios.get<SiteInfoListItem[]>(
     `${process.env.REACT_APP_WEBAPI_URL}WaterRights/${waterRightId}/Sites`
   );
   return data;
 };
 
-export const getWaterRightSourceInfoList = async (waterRightId: number) => {
+export const getWaterRightSourceInfoList = async (waterRightId: string) => {
   const { data } = await axios.get<WaterSourceInfoListItem[]>(
     `${process.env.REACT_APP_WEBAPI_URL}WaterRights/${waterRightId}/Sources`
   );
   return data;
 };
 
-export const getWaterRightSiteLocations = async (waterRightId: number) => {
+export const getWaterRightSiteLocations = async (waterRightId: string) => {
   const { data } = await axios.get<
     GeoJSON.FeatureCollection<GeoJSON.Geometry, GeoJSON.GeoJsonProperties>
   >(
