1f0955e45085498b9b4444a9fb46635677e04dbb
Sarah Kenny <skenny@dontpaniclabs.com>
2022-07-13 13:09:33 -0500

    Custom class for offcanvas analytics

--- src/DashboardUI/src/components/TableView.tsx
+++ src/DashboardUI/src/components/TableView.tsx
@@ -94,7 +94,7 @@ function TableView(props: TableViewProps) {
     </Button>
     <Tab.Container id="analytics-and-table" defaultActiveKey="dataTable">
       <Offcanvas
-        className="analytics-and-table"
+        className="offcanvas-analytics"
         show={show}
         onHide={handleClose}
         placement={'bottom'}
