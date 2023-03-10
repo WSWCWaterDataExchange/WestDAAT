dfd2df77ea5e6a5cd298e11cfe8db5c42fed82cc
Andrew Tarr <andrew.tarr@hotmail.com>
2023-03-10 14:14:44 -0600

    Tweaking CSS to have scrollable content between header and footer

--- src/DashboardUI/src/pages/DetailLayout.tsx
+++ src/DashboardUI/src/pages/DetailLayout.tsx
@@ -1,18 +1,33 @@
 import { Outlet } from "react-router-dom";
+import '../styles/detail-layout.scss';
+import SiteFooter from "../components/SiteFooter";
+import {useState} from "react";
+import SiteNavbar from "../components/SiteNavbar";
+import FeedbackModal from "../components/FeedbackModal";
 
 // TODO: Do we need logic to redirect if we don't have detail type or detail id in route?
 
 function DetailLayout() {
+  const [showFeedbackModal, setShowFeedbackModal] = useState(false);
+
+  const shouldShowFeedbackModal = (show: boolean) => {
+    setShowFeedbackModal(show);
+  }
+
   return (
     <>
-      {/* A "layout route" is a good place to put markup you want to
-          share across all the pages on your site, like navigation. */}
+      <div className="detail-layout d-flex flex-column">
+        {/* A "layout route" is a good place to put markup you want to
+            share across all the pages on your site, like navigation. */}
 
-      {/* An <Outlet> renders whatever child route is currently active,
-          so you can think about this <Outlet> as a placeholder for
-          the child routes we defined above. */}
-      <Outlet />
-      
+        {/* An <Outlet> renders whatever child route is currently active,
+            so you can think about this <Outlet> as a placeholder for
+            the child routes we defined above. */}
+        <SiteNavbar />
+        <Outlet />
+        <SiteFooter showFeedbackModal={shouldShowFeedbackModal} />
+      </div>
+      <FeedbackModal show={showFeedbackModal} setShow={shouldShowFeedbackModal} />
     </>
   );
 }
