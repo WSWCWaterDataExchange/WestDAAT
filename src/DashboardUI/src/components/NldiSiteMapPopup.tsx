73c5fb65cbf9689172634f14f137d3075abeef68
Yan Xin Lee <ylee@dontpaniclabs.com>
2023-03-16 10:25:00 -0500

    added popup card for NLDI sites

---
+++ src/DashboardUI/src/components/NldiSiteMapPopup.tsx
@@ -0,0 +1,45 @@
+import Icon from "@mdi/react";
+import { MapPopupCard } from "./MapPopupCard";
+import { mdiOpenInNew } from '@mdi/js';
+
+interface NldiSiteMapPopupProps {
+  sourceName: string,
+  identifier: string, 
+  uri: string,
+  name: string,
+  onClosePopup: () => void;
+
+}
+function NldiSiteMapPopUp(props: NldiSiteMapPopupProps) {
+  const { sourceName, identifier, uri, name, onClosePopup } = props;
+  return (
+    <MapPopupCard onClosePopup={onClosePopup}>
+      {{
+        header: <div>{identifier}</div>,
+        body: <div className="map-popup-card-water-rights-body">
+        <div className="mb-2">
+          <div>
+            <strong>Source:</strong>
+          </div>
+          {sourceName}
+        </div>
+        <div className="mb-2">
+          <div>
+            <strong>Name:</strong>
+          </div>
+            {name}
+        </div>
+        <div className="mb-2">
+          <div className="map-popup-card-water-rights-native-id-row">
+            <strong>Identifier:</strong>
+          </div>
+          <div>
+            <a href={uri} target="_blank" rel="noopener noreferrer">{identifier} <Icon path={mdiOpenInNew} className="map-popup-card-water-rights-link-icon" /></a>
+          </div>
+        </div>
+      </div>
+      }}
+    </MapPopupCard>
+  );
+}
+export default NldiSiteMapPopUp;
--- src/DashboardUI/src/hooks/useWaterRightsMapPopup.tsx
+++ src/DashboardUI/src/hooks/useWaterRightsMapPopup.tsx
@@ -2,8 +2,10 @@ import { useCallback, useEffect, useMemo, useState } from "react";
 import useMapPopupOnClick from "./useMapPopupOnClick";
 import { MapPopupCard } from "../components/MapPopupCard";
 import WaterRightsMapPopup from "../components/WaterRightsMapPopup";
-import { waterRightsProperties } from "../config/constants";
+import { nldiSiteProperties, waterRightsProperties } from "../config/constants";
 import { useWaterRightsDigests } from "./useSiteQuery";
+import { Feature, GeoJsonProperties, Geometry } from "geojson";
+import NldiSiteMapPopUp from "../components/NldiSiteMapPopup";
 
 export function useWaterRightsMapPopup() {
   const { updatePopup, clickedFeatures } = useMapPopupOnClick();
@@ -29,7 +31,13 @@ export function useWaterRightsMapPopup() {
 
   const result = useMemo(() => {
     if (!clickedSiteUuid) {
-      return undefined;
+      if (clickedFeatures && clickedFeatures.length > 0) {
+        const nldiSite = getNldiSite(clickedFeatures[0]);
+        if(!nldiSite || !nldiSite.sourceName){
+          return undefined;
+        }
+        return <NldiSiteMapPopUp sourceName={nldiSite.sourceName} identifier={nldiSite.identifier} uri={nldiSite.uri} name={nldiSite.name} onClosePopup={handleClosePopup} />;
+      }
     }
     if (isFetching) {
       return <MapPopupCard onClosePopup={handleClosePopup}>
@@ -53,4 +61,16 @@ export function useWaterRightsMapPopup() {
   useEffect(() => {
     updatePopup(result);
   }, [updatePopup, result]);
+}
+
+function getNldiSite(nldiFeature: Feature<Geometry, GeoJsonProperties> ) {
+  if(nldiFeature.properties) {
+    return { 
+      sourceName: nldiFeature.properties[nldiSiteProperties.sourceName as string],
+      identifier: nldiFeature.properties[nldiSiteProperties.identifier as string],
+      name: nldiFeature.properties[nldiSiteProperties.name as string],
+      uri: nldiFeature.properties[nldiSiteProperties.uri as string]        
+    };
+  };
+  return undefined;
 }
