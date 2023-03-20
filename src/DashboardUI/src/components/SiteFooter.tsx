7059e6e2a0b19b98627aef0b6267cf4edc4435e0
Yan Xin Lee <ylee@dontpaniclabs.com>
2023-03-20 15:16:14 -0500

    fixed build error; fixed side panel css

--- src/DashboardUI/src/components/SiteFooter.tsx
+++ src/DashboardUI/src/components/SiteFooter.tsx
@@ -18,7 +18,7 @@ function SiteFooter(props: SiteFooterProps) {
           <Nav>
             <Nav.Link className="p-0" target="_blank" rel="noopener noreferrer" href="https://westernstateswater.org/wade/">WaDE</Nav.Link>
             <Nav.Link className="p-0" target="_blank" rel="noopener noreferrer" href="https://westernstateswater.org/">WSWC</Nav.Link>
-            <Nav.Link className="p-0 s-1" target="_blank" rel="noopener noreferrer" href="https://github.com/WSWCWaterDataExchange/WestDAAT/"><img className="github-logo" src={github_mark_white}/></Nav.Link>
+            <Nav.Link className="p-0 s-1" target="_blank" rel="noopener noreferrer" href="https://github.com/WSWCWaterDataExchange/WestDAAT/"><img className="github-logo" src={github_mark_white} alt="GitHub logo with link to WestDAAT source code"/></Nav.Link>
           </Nav>
         </div>
 
