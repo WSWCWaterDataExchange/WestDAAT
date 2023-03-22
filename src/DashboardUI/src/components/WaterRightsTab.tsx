5f7d63677735c4d0f2748fc62a5d52ac854c34b0
Yan Xin Lee <ylee@dontpaniclabs.com>
2023-03-22 09:03:20 -0500

    updated selection filter title

--- src/DashboardUI/src/components/WaterRightsTab.tsx
+++ src/DashboardUI/src/components/WaterRightsTab.tsx
@@ -654,7 +654,7 @@ function WaterRightsTab() {
             </Accordion.Body>
           </Accordion.Item>
           <Accordion.Item eventKey="siteSelectionFilters">
-            <Accordion.Header>SITE SELECTION FILTERS</Accordion.Header>
+            <Accordion.Header>WATER RIGHT SELECTION FILTERS</Accordion.Header>
             <Accordion.Body>
               <div className="mb-3">
                 <a href="https://westernstateswater.org/wade/westdaat-filter-documentation/" target="_blank" rel="noopener noreferrer">Learn about WestDAAT filters</a>
