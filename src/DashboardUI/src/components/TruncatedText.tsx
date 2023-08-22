b600ac73ae1b0a3806e4855a20c1ff36af41586e
Nathan Wilkinson <nwilkinson@whatiffoundry.com>
2023-08-22 16:26:23 -0500

    Fixed Issues w/ truncated text
    Fixed displaying the show more button when it wasn't necessary
    Fixed accessibility issues
    Made the CSS smarter about what to display
    Animated the transition
    Cleaned up unnecessary logic in the rendering of the components

--- src/DashboardUI/src/components/TruncatedText.tsx
+++ src/DashboardUI/src/components/TruncatedText.tsx
@@ -1,46 +1,51 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 
 interface TruncatedTextProps {
   text: string;
 }
 
 const TruncatedText: React.FC<TruncatedTextProps> = ({ text }) => {
-  const [showFullText, setShowFullText] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+  const [isClamped, setIsClamped] = useState(true);
+  const paragraphRef = useRef<HTMLParagraphElement>(null);
 
   const toggleShowFullText = () => {
-    setShowFullText(!showFullText);
+    setIsOpen(!isOpen);
   };
+
+  useEffect(() => {
+    let clone: HTMLDivElement | undefined = undefined;
+    if(paragraphRef && paragraphRef.current){
+      clone = paragraphRef.current.parentElement?.cloneNode(true) as HTMLDivElement || document.createElement('div');
+      clone.style.visibility='hidden';
+      clone.style.position = 'fixed';
+      clone.style.width = `${paragraphRef.current.parentElement?.clientWidth}px`;
+      paragraphRef.current.parentElement?.parentElement?.appendChild(clone);
+    }
+    function handleResize() {
+      if (paragraphRef && paragraphRef.current && clone) {
+        clone.style.width = `${paragraphRef.current.parentElement?.clientWidth}px`;
+        const clientHeight = clone.querySelector('p')?.clientHeight || 0;
+        const scrollHeight = clone.querySelector('p')?.scrollHeight || 0;
+        paragraphRef.current.style.setProperty('--truncated-height', `${clientHeight}px`)
+        paragraphRef.current.style.setProperty('--expanded-height', `${scrollHeight}px`)
+        setIsClamped(
+          scrollHeight > clientHeight
+        )
+      }
+    }
+
+    window.addEventListener('resize', handleResize)
+    handleResize();
+    return () => window.removeEventListener('resize', handleResize)
+  }, [paragraphRef, setIsClamped, setIsOpen])
 
   return (
     <div>
-      {showFullText ? (
-        <>
-          <p>{text}</p>
-          <span
-            style={{ color: 'blue', cursor: 'pointer' }}
-            onClick={toggleShowFullText}
-          >
-            {' '}
-            View less
-          </span>
-        </>
-      ) : (
-        <>
-          <p>
-            <div className='paragraph-truncate'>
-                {   text}
-            </div>
-            
-            <span
-              style={{ color: 'blue', cursor: 'pointer' }}
-              onClick={toggleShowFullText}
-            >
-              {' '}
-              View more
-            </span>
-          </p>
-        </>
-      )}
+      <div className={`paragraph-truncate${isOpen ? ' open' : ''}${isClamped ? ' truncated' : ''}`}>
+        <p ref={paragraphRef}>{text}</p>
+        <button className='btn btn-link p-0 m-0' onClick={toggleShowFullText}></button>
+      </div>
     </div>
   );
 };
