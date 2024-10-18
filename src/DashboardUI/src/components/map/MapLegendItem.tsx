026b0860b9d3ffee62504f1c100b22fc8bfa64d9
Andrew Tarr <andrew.tarr@hotmail.com>
2024-10-18 13:56:37 -0500

    Fixing empty object lint erros

--- src/DashboardUI/src/components/map/MapLegendItem.tsx
+++ src/DashboardUI/src/components/map/MapLegendItem.tsx
@@ -1,36 +1,32 @@
-import React from 'react';
-import { mdiMapMarker } from "@mdi/js";
-import Icon from "@mdi/react";
-import { PropsWithChildren } from "react";
+import React, { PropsWithChildren } from 'react';
+import { mdiMapMarker } from '@mdi/js';
+import Icon from '@mdi/react';
+import { EmptyPropsWithChildren } from '../../HelperTypes';
 
-export function MapLegendItem({children}: PropsWithChildren<{}>) {
-  return (
-    <div className="legend-item">
-      {children}
-    </div>
-  )
+export function MapLegendItem({ children }: Readonly<EmptyPropsWithChildren>) {
+  return <div className='legend-item'>{children}</div>;
 }
 
 interface MapLegendCircleItemProps {
-  color: string
+  color: string;
 }
-export function MapLegendCircleItem({color, children}: PropsWithChildren<MapLegendCircleItemProps>) {
+export function MapLegendCircleItem({ color, children }: PropsWithChildren<MapLegendCircleItemProps>) {
   return (
     <MapLegendItem>
-      <span className="legend-circle" style={{ "backgroundColor": color }}></span>
+      <span className='legend-circle' style={{ backgroundColor: color }}></span>
       {children}
     </MapLegendItem>
-  )
+  );
 }
 
 interface MapLegendMapPointerItemProps {
-  color: string
+  color: string;
 }
-export function MapLegendMarkerItem({color, children}: PropsWithChildren<MapLegendMapPointerItemProps>) {
+export function MapLegendMarkerItem({ color, children }: PropsWithChildren<MapLegendMapPointerItemProps>) {
   return (
     <MapLegendItem>
-      <Icon path={mdiMapMarker} size="16px" color={color} />
+      <Icon path={mdiMapMarker} size='16px' color={color} />
       {children}
     </MapLegendItem>
-  )
+  );
 }
