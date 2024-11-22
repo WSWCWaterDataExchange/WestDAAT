a60d3f8705c2c80c308be41c19b51db5cce52ce2
Jack Nolley <jnolley@dontpaniclabs.com>
2024-11-22 12:40:24 -0600

    got overlays returning

--- src/API/WesternStatesWater.WestDaat.Common/DataContracts/DashboardFilters.cs
+++ src/API/WesternStatesWater.WestDaat.Common/DataContracts/DashboardFilters.cs
@@ -5,6 +5,7 @@ public class DashboardFilters
     public BeneficialUseItem[] BeneficialUses { get; set; }
     public string[] WaterSources { get; set; }
     public string[] OwnerClassifications { get; set; }
+    public string[] Overlays { get; set; }
     public string[] States { get; set; }
     public string[] AllocationTypes { get; set; }
     public string[] LegalStatuses { get; set; }
