b5590729394f639c273670db03316ffddc91d209
Zach Lannin <zlannin@dontpaniclabs.com>
2024-11-26 16:17:07 -0600

    cleanup

--- src/API/WesternStatesWater.WestDaat.Tests.Helpers/ReportingUnitsFactFaker.cs
+++ src/API/WesternStatesWater.WestDaat.Tests.Helpers/ReportingUnitsFactFaker.cs
@@ -7,8 +7,6 @@ public class ReportingUnitsFactFaker : Faker<RegulatoryReportingUnitsFact>
     public ReportingUnitsFactFaker()
     {
         RuleFor(r => r.Organization, new OrganizationsDimFaker().Generate());
-        // RuleFor(r => r.ReportingUnit, new ReportingUnitsDimFaker().Generate());
-        // RuleFor(r => r.RegulatoryOverlay, new OverlayDimFaker().Generate());
         RuleFor(r => r.DataPublicationDate, new DateDimFaker().Generate());
     }
 }
