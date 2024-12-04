fc97bababea4b281f591364f3570c0be4a52cb1e
Jack Nolley <jnolley@dontpaniclabs.com>
2024-12-04 09:41:01 -0600

    filter toggle UI stubbed out

--- src/DashboardUI/src/components/home-page/water-rights-tab/filters/Overlays.tsx
+++ src/DashboardUI/src/components/home-page/water-rights-tab/filters/Overlays.tsx
@@ -1,6 +1,5 @@
 import React, { useState, ChangeEvent } from 'react';
 import { Form } from 'react-bootstrap';
-import './overylays-filters.scss';
 
 export function Overlays() {
   const [overlayFilters, setOverlayFilters] = useState({
@@ -21,49 +20,47 @@ export function Overlays() {
     <div className="position-relative flex-grow-1">
       <h5 className="fw-bold">OVERLAY INFO</h5>
       <div className="mb-3">
-        <Form.Label className=" form-check fw-bolder">Overlay Type</Form.Label>
-        <div className="d-flex flex-column">
-          <Form.Group className="d-flex align-items-center mb-3">
-            <Form.Check
-              className="toggle"
-              type="switch"
-              id="overlayAdmin"
-              checked={overlayFilters.admin}
-              onChange={(e) => handleToggleChange(e, 'admin')}
-              label="Admin"
-            />
-          </Form.Group>
-          <Form.Group className="d-flex align-items-center mb-3">
-            <Form.Check
-              className="toggle"
-              type="switch"
-              id="overlayRegulatory"
-              checked={overlayFilters.regulatory}
-              onChange={(e) => handleToggleChange(e, 'regulatory')}
-              label="Regulatory"
-            />
-          </Form.Group>
-          <Form.Group className="d-flex align-items-center mb-3">
-            <Form.Check
-              className="toggle"
-              type="switch"
-              id="overlayAquifer"
-              checked={overlayFilters.aquifer}
-              onChange={(e) => handleToggleChange(e, 'aquifer')}
-              label="Aquifer"
-            />
-          </Form.Group>
-          <Form.Group className="d-flex align-items-center mb-3">
-            <Form.Check
-              className="toggle"
-              type="switch"
-              id="overlayHuc"
-              checked={overlayFilters.huc}
-              onChange={(e) => handleToggleChange(e, 'huc')}
-              label="HUC"
-            />
-          </Form.Group>
-        </div>
+        <label className="form-label fw-bolder">Overlay Type</label>
+        <Form.Group className="mb-3">
+          <Form.Check
+            className="toggle"
+            type="switch"
+            id="overlayAdmin"
+            checked={overlayFilters.admin}
+            onChange={(e) => handleToggleChange(e, 'admin')}
+            label="Admin"
+          />
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Check
+            className="toggle"
+            type="switch"
+            id="overlayRegulatory"
+            checked={overlayFilters.regulatory}
+            onChange={(e) => handleToggleChange(e, 'regulatory')}
+            label="Regulatory"
+          />
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Check
+            className="toggle"
+            type="switch"
+            id="overlayAquifer"
+            checked={overlayFilters.aquifer}
+            onChange={(e) => handleToggleChange(e, 'aquifer')}
+            label="Aquifer"
+          />
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Check
+            className="toggle"
+            type="switch"
+            id="overlayHuc"
+            checked={overlayFilters.huc}
+            onChange={(e) => handleToggleChange(e, 'huc')}
+            label="HUC"
+          />
+        </Form.Group>
       </div>
     </div>
   );
