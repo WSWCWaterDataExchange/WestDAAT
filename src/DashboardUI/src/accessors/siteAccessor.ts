907e8c4eda61c31c97b018843f4b6d5c1265d957
Kameron Heyen <kheyen@dontpaniclabs.com>
2024-12-11 09:08:33 -0600

    Create table for tab for Variable Info on Site pages

--- src/DashboardUI/src/accessors/siteAccessor.ts
+++ src/DashboardUI/src/accessors/siteAccessor.ts
@@ -1,58 +1,36 @@
-import {
-  WaterSourceInfoListItem,
-  SiteDetails,
-  WaterRightInfoListItem,
-} from '@data-contracts';
+import { WaterSourceInfoListItem, SiteDetails, WaterRightInfoListItem } from '@data-contracts';
 import axios from 'axios';
 import WaterRightDigest from '../data-contracts/WaterRightsDigest';
 import SiteDigest from '../data-contracts/SiteDigest';
-import {SiteUsage} from "../data-contracts/SiteUsage";
+import { SiteUsage } from '../data-contracts/SiteUsage';
+import { VariableInfoListItem } from '../data-contracts/VariableInfoListItem';
 
-export const getWaterRightsDigests = async (
-  siteUuid: string,
-): Promise<WaterRightDigest[]> => {
-  const url = new URL(
-    `Sites/${siteUuid}/WaterRightsDigest`,
-    process.env.REACT_APP_WEBAPI_URL,
-  );
+export const getWaterRightsDigests = async (siteUuid: string): Promise<WaterRightDigest[]> => {
+  const url = new URL(`Sites/${siteUuid}/WaterRightsDigest`, process.env.REACT_APP_WEBAPI_URL);
   const { data } = await axios.get<WaterRightDigest[]>(url.toString());
   return data;
 };
 
 export const getSiteDigest = async (siteUuid: string): Promise<SiteDigest> => {
-  const url = new URL(
-    `Sites/${siteUuid}/Digest`,
-    process.env.REACT_APP_WEBAPI_URL,
-  );
+  const url = new URL(`Sites/${siteUuid}/Digest`, process.env.REACT_APP_WEBAPI_URL);
   const { data } = await axios.get<SiteDigest>(url.toString());
   return data;
 };
 
 export const getWaterSiteLocation = async (siteUuid: string) => {
-  const url = new URL(
-    `Sites/${siteUuid}/SiteLocation`,
-    process.env.REACT_APP_WEBAPI_URL,
-  );
-  const { data } = await axios.get<
-    GeoJSON.Feature<GeoJSON.Geometry, GeoJSON.GeoJsonProperties>
-  >(url.toString());
+  const url = new URL(`Sites/${siteUuid}/SiteLocation`, process.env.REACT_APP_WEBAPI_URL);
+  const { data } = await axios.get<GeoJSON.Feature<GeoJSON.Geometry, GeoJSON.GeoJsonProperties>>(url.toString());
   return data;
 };
 
 export const getWaterSiteSourceInfoList = async (siteUuid: string) => {
-  const url = new URL(
-    `Sites/${siteUuid}/Sources`,
-    process.env.REACT_APP_WEBAPI_URL,
-  );
+  const url = new URL(`Sites/${siteUuid}/Sources`, process.env.REACT_APP_WEBAPI_URL);
   const { data } = await axios.get<WaterSourceInfoListItem[]>(url.toString());
   return data;
 };
 
 export const getWaterRightInfoList = async (siteUuid: string) => {
-  const url = new URL(
-    `Sites/${siteUuid}/Rights`,
-    process.env.REACT_APP_WEBAPI_URL,
-  );
+  const url = new URL(`Sites/${siteUuid}/Rights`, process.env.REACT_APP_WEBAPI_URL);
   const { data } = await axios.get<WaterRightInfoListItem[]>(url.toString());
   return data;
 };
@@ -72,5 +50,11 @@ export const getSiteUsage = async (siteUuid: string) => {
     amount: d.amount,
   }));
 
+  return data;
+};
+
+export const getSiteVariableInfoList = async (siteUuid: string) => {
+  const url = new URL(`Sites/${siteUuid}/Variables`, process.env.REACT_APP_WEBAPI_URL);
+  const { data } = await axios.get<VariableInfoListItem[]>(url.toString());
   return data;
 };
