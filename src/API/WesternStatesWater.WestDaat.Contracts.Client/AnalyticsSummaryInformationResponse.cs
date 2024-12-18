9ecb7cd5ceae757eec39d13e71085dbdc445029d
Ethan Weber <eweber@dontpaniclabs.com>
2024-12-18 12:02:34 -0600

    Create AnalyticsSummaryInformationResponse.cs

---
+++ src/API/WesternStatesWater.WestDaat.Contracts.Client/AnalyticsSummaryInformationResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace WesternStatesWater.WestDaat.Contracts.Client;
+
+public class AnalyticsSummaryInformationResponse
+{
+    public AnalyticsSummaryInformation[] AnalyticsSummaryInformations { get; set; }
+    public DropdownOption[] DropdownOptions { get; set; }
+    public int SelectedValue { get; set; }
+}
