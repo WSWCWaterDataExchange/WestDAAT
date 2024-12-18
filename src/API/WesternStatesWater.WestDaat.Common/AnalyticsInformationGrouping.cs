14a67255739f3b874852e1b74dca2923594de1c7
Ethan Weber <eweber@dontpaniclabs.com>
2024-12-18 11:57:29 -0600

    Create AnalyticsInformationGrouping.cs

---
+++ src/API/WesternStatesWater.WestDaat.Common/AnalyticsInformationGrouping.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WesternStatesWater.WestDaat.Common
+{
+    public enum AnalyticsInformationGrouping
+    {
+        [Display(Name = "Beneficial Use")]
+        BeneficialUse = 1,
+
+        [Display(Name = "Water Source Type")]
+        WaterSourceType = 2,
+
+        [Display(Name = "Owner Type")]
+        OwnerType = 3,
+
+        [Display(Name = "Allocation Type")]
+        AllocationType = 4,
+
+        [Display(Name = "Legal Status")]
+        LegalStatus = 5,
+
+        [Display(Name = "Site Type")]
+        SiteType = 6,
+    }
+}
