9154a9d6f56b431faa00799e01828a7cdbbd5a56
Branden Barber <bbarber@dontpaniclabs.com>
2024-12-19 12:03:26 -0600

    Summaries for services

--- src/API/WesternStatesWater.WestDaat.Engines/INotificationFilteringEngine.cs
+++ src/API/WesternStatesWater.WestDaat.Engines/INotificationFilteringEngine.cs
@@ -2,6 +2,10 @@ using WesternStatesWater.WestDaat.Common;
 
 namespace WesternStatesWater.WestDaat.Engines;
 
+/// <summary>
+/// The NotificationFilteringEngine is responsible for determining the recipients of a notification.
+/// Additionally, it constructs the notification meta, which includes all necessary data for sending the notification.
+/// </summary>
 public interface INotificationFilteringEngine : IServiceContractBase
 {
     Task<DTO.NotificationMetaBase[]> Filter<T>(T filterEvent);
--- src/API/WesternStatesWater.WestDaat.Engines/INotificationFormattingEngine.cs
+++ src/API/WesternStatesWater.WestDaat.Engines/INotificationFormattingEngine.cs
@@ -2,6 +2,10 @@ using WesternStatesWater.WestDaat.Common;
 
 namespace WesternStatesWater.WestDaat.Engines;
 
+/// <summary>
+/// The NotificationFormattingEngine is responsible for formatting the notification meta into a
+/// format that can be sent to the recipients. Notification examples include Email, SMS, etc.
+/// </summary>
 public interface INotificationFormattingEngine : IServiceContractBase
 {
     DTO.NotificationBase[] Format<T>(T[] notificationMetas);
