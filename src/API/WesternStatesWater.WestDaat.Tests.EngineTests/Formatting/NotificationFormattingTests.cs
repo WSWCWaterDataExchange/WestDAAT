ef6e804d8b4c87c046f6534f7e064f25e5777608
Branden Barber <bbarber@dontpaniclabs.com>
2024-12-19 12:16:25 -0600

    Test

--- src/API/WesternStatesWater.WestDaat.Tests.EngineTests/Formatting/NotificationFormattingTests.cs
+++ src/API/WesternStatesWater.WestDaat.Tests.EngineTests/Formatting/NotificationFormattingTests.cs
@@ -1,5 +1,32 @@
+using WesternStatesWater.WestDaat.Common.DataContracts;
+using WesternStatesWater.WestDaat.Engines;
+
 namespace WesternStatesWater.WestDaat.Tests.EngineTests.Formatting;
 
-public class NotificationFormattingTests
+[TestClass]
+public class NotificationFormattingTests : EngineTestBase
 {
+    private INotificationFormattingEngine _notificationFormattingEngine;
+
+    [TestInitialize]
+    public override void TestInitialize()
+    {
+        _notificationFormattingEngine = new FormattingEngine(
+            CreateLogger<FormattingEngine>()
+        );
+    }
+
+    [TestMethod]
+    public void Format_FakeFormatType_ShouldThrow()
+    {
+        // Arrange
+        var meta = new NotificationMetaBase[] { new FakeNotificationMeta() };
+
+        // Act + Assert
+        Assert.ThrowsException<NotImplementedException>(() => _notificationFormattingEngine.Format(meta));
+    }
+
+    private class FakeNotificationMeta : NotificationMetaBase
+    {
+    }
 }
