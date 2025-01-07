cd3550fd687cf52fe916b3e928b6bbbc776fede5
Ethan Weber <eweber@dontpaniclabs.com>
2025-01-07 15:35:55 -0600

    use component in existing use case

--- src/DashboardUI/src/components/details-page/overlay/OverlayTabs.tsx
+++ src/DashboardUI/src/components/details-page/overlay/OverlayTabs.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Tabs, Tab, Table } from 'react-bootstrap';
 import { useOverlayDetailsContext } from './Provider';
+import OverlayDetailsTable from '../OverlayDetailsTable';
 
 export enum OverlayTab {
   Admin = 'overlay',
@@ -25,44 +26,7 @@ function OverlayTabs() {
       className="mb-3 custom-tabs"
     >
       <Tab eventKey={OverlayTab.Admin} title="Administrative/Regulatory Overlay Info">
-        <Table hover>
-          <thead>
-            <tr>
-              <th>WaDE Overlay UUID</th>
-              <th>Overlay Native ID</th>
-              <th>Overlay Name</th>
-              <th>Overlay Type</th>
-              <th>Water Source Type</th>
-              <th>Overlay Status</th>
-              <th>Statute Link</th>
-              <th>Statutory Effective Date</th>
-              <th>Statutory End Date</th>
-              <th>Overlay Statue Description</th>
-            </tr>
-          </thead>
-          <tbody>
-            {waterRightsInfoListByReportingUnit?.map((entry) => (
-              <tr key={entry.waDEOverlayUuid}>
-                <td>{entry.waDEOverlayUuid}</td>
-                <td>{entry.overlayNativeID}</td>
-                <td>{entry.overlayName}</td>
-                <td>{entry.overlayType}</td>
-                <td>{entry.waterSourceType}</td>
-                <td>{entry.overlayStatus}</td>
-                <td>
-                  <a href={entry.statuteLink} target="_blank" rel="noopener noreferrer">
-                    Link
-                  </a>
-                </td>
-                <td>
-                  {entry.statutoryEffectiveDate ? new Date(entry.statutoryEffectiveDate).toLocaleDateString() : '-'}
-                </td>
-                <td>{entry.statutoryEndDate ? new Date(entry.statutoryEndDate).toLocaleDateString() : '-'}</td>
-                <td>{entry.overlayStatusDesc}</td>
-              </tr>
-            ))}
-          </tbody>
-        </Table>
+        <OverlayDetailsTable waterRightsInfoList={waterRightsInfoListByReportingUnit} />
       </Tab>
 
       <Tab eventKey={OverlayTab.WaterRight} title="Related Water Right Information">
