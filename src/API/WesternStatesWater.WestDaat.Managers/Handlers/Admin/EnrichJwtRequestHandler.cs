b9ba66fc6f3c2bfd5fc052dbc11ba7b1077c3115
Ethan Weber <eweber@dontpaniclabs.com>
2025-01-08 17:57:20 -0600

    stub in request handler

---
+++ src/API/WesternStatesWater.WestDaat.Managers/Handlers/Admin/EnrichJwtRequestHandler.cs
@@ -0,0 +1,13 @@
+﻿using WesternStatesWater.Shared.Resolver;
+using WesternStatesWater.WestDaat.Contracts.Client.Requests.Admin;
+using WesternStatesWater.WestDaat.Contracts.Client.Responses.Admin;
+
+namespace WesternStatesWater.WestDaat.Managers.Handlers.Admin;
+
+public class EnrichJwtRequestHandler : IRequestHandler<EnrichJwtRequest, EnrichJwtResponse>
+{
+    public Task<EnrichJwtResponse> Handle(EnrichJwtRequest request)
+    {
+        throw new NotImplementedException();
+    }
+}
