b64b7d7d48ebcab30a22c9248e551e0453a5e3e8
Ethan Weber <eweber@dontpaniclabs.com>
2025-01-13 13:32:59 -0600

    [wip] sdk implementation

--- src/API/WesternStatesWater.WestDaat.Utilities/OpenEtSdk.cs
+++ src/API/WesternStatesWater.WestDaat.Utilities/OpenEtSdk.cs
@@ -1,6 +1,9 @@
 using Microsoft.Extensions.Logging;
 using System.Net.Http;
+using System.Text;
+using System.Text.Json;
 using WesternStatesWater.WestDaat.Common.DataContracts;
+using WesternStatesWater.WestDaat.Common.Exceptions;
 
 namespace WesternStatesWater.WestDaat.Utilities;
 
@@ -17,7 +20,38 @@ internal class OpenEtSdk : IOpenEtSdk
 
     public async Task<RasterTimeseriesPolygonResponse> RasterTimeseriesPolygon(RasterTimeseriesPolygonRequest request)
     {
-        await Task.CompletedTask;
-        throw new NotImplementedException();
+        var requestDictionary = new Dictionary<string, object>
+        {
+            {
+                "date_range", 
+                new string[] 
+                {
+                    request.DateRangeStart.ToString("YYYY-MM-dd"), 
+                    request.DateRangeEnd.ToString("YYYY-MM-dd")
+                }
+            },
+            { "file_format", request.OutputExtension.ToString() },
+            { "geometry", request.Geometry.Envelope.Coordinates.Select(c => new double[] { c.X, c.Y }).ToArray() },
+            { "interval", request.Interval.ToString() },
+            { "model", request.Model.ToString() },
+            { "reducer", request.PixelReducer.ToString() },
+            { "reference_et", request.ReferenceEt.ToString() },
+            { "units", request.OutputUnits.ToString() },
+            { "variable", request.Variable.ToString() },
+        };
+
+        var httpContent = new StringContent(JsonSerializer.Serialize(requestDictionary), Encoding.UTF8, "application/json");
+
+        var uri = new Uri("https://openet-api.org/raster/timeseries/polygon");
+        var response = await _httpClient.PostAsync(uri, httpContent);
+
+        if (!response.IsSuccessStatusCode)
+        {
+            throw new WestDaatException($"OpenET API returned status code {response.StatusCode}");
+        }
+
+        // convert response to RasterTimeseriesPolygonResponse
+        var responseContent = await response.Content.ReadAsStringAsync();
+        return JsonSerializer.Deserialize<RasterTimeseriesPolygonResponse>(responseContent);
     }
 }
