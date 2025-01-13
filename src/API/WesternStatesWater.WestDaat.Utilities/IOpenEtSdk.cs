2e053206670c3f88ab3b866840c935e153563ece
Ethan Weber <eweber@dontpaniclabs.com>
2025-01-13 11:37:29 -0600

    interface / default implementation

--- src/API/WesternStatesWater.WestDaat.Utilities/IOpenEtSdk.cs
+++ src/API/WesternStatesWater.WestDaat.Utilities/IOpenEtSdk.cs
@@ -1,5 +1,8 @@
+using WesternStatesWater.WestDaat.Common.DataContracts;
+
 namespace WesternStatesWater.WestDaat.Utilities;
 
 public interface IOpenEtSdk
 {
+    Task<RasterTimeseriesPolygonResponse> RasterTimeseriesPolygon(RasterTimeseriesPolygonRequest request);
 }
--- src/API/WesternStatesWater.WestDaat.Utilities/OpenEtSdk.cs
+++ src/API/WesternStatesWater.WestDaat.Utilities/OpenEtSdk.cs
@@ -1,5 +1,23 @@
+using Microsoft.Extensions.Logging;
+using System.Net.Http;
+using WesternStatesWater.WestDaat.Common.DataContracts;
+
 namespace WesternStatesWater.WestDaat.Utilities;
 
 internal class OpenEtSdk : IOpenEtSdk
 {
+    private readonly HttpClient _httpClient;
+    private readonly ILogger _logger;
+
+    public OpenEtSdk(HttpClient httpClient, ILogger<UsgsNldiSdk> logger)
+    {
+        _httpClient = httpClient;
+        _logger = logger;
+    }
+
+    public async Task<RasterTimeseriesPolygonResponse> RasterTimeseriesPolygon(RasterTimeseriesPolygonRequest request)
+    {
+        await Task.CompletedTask;
+        throw new NotImplementedException();
+    }
 }
