216ddd753d7a05aaf9c57165fadf755e830b2e41
Ethan Weber <eweber@dontpaniclabs.com>
2025-01-14 11:55:48 -0600

    update contract

--- src/API/WesternStatesWater.WestDaat.Database/EntityFramework/Organization.cs
+++ src/API/WesternStatesWater.WestDaat.Database/EntityFramework/Organization.cs
@@ -2,7 +2,14 @@
 
 public class Organization
 {
+    public Organization()
+    {
+        UserOrganizations = new HashSet<UserOrganization>();
+    }
+
     public Guid Id { get; set; }
 
     public string Name { get; set; }
+
+    public virtual ICollection<UserOrganization> UserOrganizations { get; set; }
 }
