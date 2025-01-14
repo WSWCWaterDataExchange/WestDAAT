086df2df3ce56b670c14093bc93edebb63316198
Ethan Weber <eweber@dontpaniclabs.com>
2025-01-14 11:44:20 -0600

    Update UserFaker.cs

--- src/API/WesternStatesWater.WestDaat.Tests.Helpers/UserFaker.cs
+++ src/API/WesternStatesWater.WestDaat.Tests.Helpers/UserFaker.cs
@@ -5,6 +5,7 @@ public class UserFaker : Faker<EF.User>
     public UserFaker()
     {
         RuleFor(u => u.Email, f => f.Person.Email)
+            .RuleFor(u => u.ExternalAuthId, f => f.Random.Guid().ToString())
             .RuleFor(u => u.CreatedAt, f => f.Date.PastOffset(1, DateTimeOffset.UtcNow));
     }
 }
