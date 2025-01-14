27303eec75f2eb0cd29793c8e3d23258a3a87d49
Ethan Weber <eweber@dontpaniclabs.com>
2025-01-14 11:06:15 -0600

    add missing field to contract

--- src/API/WesternStatesWater.WestDaat.Database/EntityFramework/UserRole.cs
+++ src/API/WesternStatesWater.WestDaat.Database/EntityFramework/UserRole.cs
@@ -4,5 +4,9 @@ public class UserRole
 {
     public Guid Id { get; set; }
 
+    public Guid UserId { get; set; }
+
     public string Role { get; set; }
+
+    public virtual User User { get; set; }
 }
