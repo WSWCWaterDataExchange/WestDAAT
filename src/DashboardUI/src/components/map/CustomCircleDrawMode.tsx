47bb13c26984619a0eefae37682344ba702fd0f0
Ethan Weber <eweber@dontpaniclabs.com>
2025-01-17 14:52:30 -0600

    [wip] trying to resize circle on drag

--- src/DashboardUI/src/components/map/CustomCircleDrawMode.tsx
+++ src/DashboardUI/src/components/map/CustomCircleDrawMode.tsx
@@ -3,12 +3,16 @@ import circle from '@turf/circle';
 import distance from '@turf/distance';
 import { Feature, GeoJSON, GeoJsonProperties, Polygon } from 'geojson';
 
+type CircleFeature = Feature<Polygon, GeoJsonProperties>;
+
 interface CircleDrawModeState {
   inProgressCircleCenterPoint: [number, number] | undefined;
+  inProgressCircleId: string | undefined;
 }
 
 const defaultState = (): CircleDrawModeState => ({
   inProgressCircleCenterPoint: undefined,
+  inProgressCircleId: undefined,
 });
 
 export const CustomCircleDrawMode: DrawCustomMode = {
@@ -21,12 +25,40 @@ export const CustomCircleDrawMode: DrawCustomMode = {
   },
 
   onClick: function (state: CircleDrawModeState, e: MapMouseEvent) {
+    const coords = e.lngLat.toArray();
     const circleHasBeenStarted = state.inProgressCircleCenterPoint !== undefined;
 
-    const coords = e.lngLat.toArray();
     if (circleHasBeenStarted) {
+      // finish the circle
+      // unset the id
+      state.inProgressCircleId = undefined;
+
+      // unset the center point
+      state.inProgressCircleCenterPoint = undefined;
+    } else {
+      // starting a new circle
+      // set the center point
+      state.inProgressCircleCenterPoint = coords;
+    }
+  },
+
+  onMouseMove: function (state: CircleDrawModeState, e: MapMouseEvent) {
+    const coords = e.lngLat.toArray();
+    const circleHasBeenStarted = state.inProgressCircleCenterPoint !== undefined;
+
+    if (!circleHasBeenStarted) {
+      return;
+    }
+
+    // render the in-progress circle:
+
+    let circleFeature = this.getFeature(state.inProgressCircleId!);
+
+    // first time this activates, the circle should not exist yet. create it
+    if (!circleFeature) {
+      console.log('generate new circle...');
       const circle = generateCircleWithRadiusFromCenterPointToEdgePoint(state.inProgressCircleCenterPoint!, coords);
-      const circleFeature = this.newFeature({
+      circleFeature = this.newFeature({
         type: 'Feature',
         properties: {},
         geometry: {
@@ -34,14 +66,13 @@ export const CustomCircleDrawMode: DrawCustomMode = {
           coordinates: circle.geometry.coordinates,
         },
       });
+      // newFeature generates an id for the feature automatically; track it:
+      state.inProgressCircleId = String(circleFeature.id);
       this.addFeature(circleFeature);
-    } else {
-      state.inProgressCircleCenterPoint = coords;
     }
-  },
 
-  onMouseMove: function (state: CircleDrawModeState, e: MapMouseEvent) {
-    // Handle mouse movement
+    console.log('circleFeature', circleFeature);
+    circleFeature.setProperty('radius', distance(state.inProgressCircleCenterPoint!, coords, { units: 'kilometers' }));
   },
 
   onKeyUp: function (state: CircleDrawModeState, e: KeyboardEvent) {
@@ -57,8 +88,6 @@ export const CustomCircleDrawMode: DrawCustomMode = {
     display(geojson); // Pass features to Mapbox Draw to render
   },
 };
-
-type CircleFeature = Feature<Polygon, GeoJsonProperties>;
 
 const generateCircleWithRadiusFromCenterPointToEdgePoint = (
   circleCenterPoint: number[],
