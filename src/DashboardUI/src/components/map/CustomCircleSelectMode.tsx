745fefc61c51c7146ece0ca09fce2fff6fdea2da
Ethan Weber <eweber@dontpaniclabs.com>
2025-01-20 11:11:22 -0600

    extend the correct mode

--- src/DashboardUI/src/components/map/CustomCircleSelectMode.tsx
+++ src/DashboardUI/src/components/map/CustomCircleSelectMode.tsx
@@ -3,9 +3,5 @@ import { DrawCustomMode } from '@mapbox/mapbox-gl-draw';
 import { GeoJSON } from 'geojson';
 
 export const CustomCircleSelectMode: DrawCustomMode = {
-  ...MapboxDraw.modes.simple_select,
-  toDisplayFeatures: function (state: any, geojson: GeoJSON, display: (feature: GeoJSON) => void) {
-    console.log('display using custom circle mode', state);
-    display(geojson);
-  },
+  ...MapboxDraw.modes.direct_select,
 };
