11e9cd12cac7eefa1cca41ade8cbc81e7fe30bc1
Ethan Weber <eweber@dontpaniclabs.com>
2025-01-20 11:37:05 -0600

    [wip] attempting to override select mode

--- src/DashboardUI/src/components/map/CustomCircleSelectMode.tsx
+++ src/DashboardUI/src/components/map/CustomCircleSelectMode.tsx
@@ -2,6 +2,28 @@ import MapboxDraw from '@mapbox/mapbox-gl-draw';
 import { DrawCustomMode } from '@mapbox/mapbox-gl-draw';
 import { GeoJSON } from 'geojson';
 
+const selectMode = MapboxDraw.modes.simple_select;
+
+interface CircleSelectModeState {
+  canDragMove: boolean;
+  dragMoveLocation: GeoJSON.Position | null; //?
+  dragMoving: boolean;
+  featureId: string;
+  feature: GeoJSON.Feature | null | undefined; //?
+  selectedCoordPaths: string[];
+  [key: string]: any; // unknown properties
+}
+
 export const CustomCircleSelectMode: DrawCustomMode = {
-  ...MapboxDraw.modes.direct_select,
+  ...selectMode,
+  onSetup: function (opts: any): CircleSelectModeState {
+    const state = selectMode.onSetup?.call(this, opts);
+    console.log('state', state);
+    return state;
+  },
+
+  onDrag: function (state: CircleSelectModeState, e: any) {
+    console.log('onDrag', state, e);
+    selectMode.onDrag?.call(this, state, e);
+  },
 };
