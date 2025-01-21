bdd58e33856cfc36b1aed0bf20d02c1ecf7ec561
Ethan Weber <eweber@dontpaniclabs.com>
2025-01-21 16:18:15 -0600

    render rectangle on mouse move rather than on click

--- src/DashboardUI/src/components/map/CustomRectangleDrawMode.tsx
+++ src/DashboardUI/src/components/map/CustomRectangleDrawMode.tsx
@@ -3,7 +3,7 @@ import { Feature, GeoJSON, GeoJsonProperties, Geometry } from 'geojson';
 import bboxPolygon from '@turf/bbox-polygon';
 
 interface RectangleDrawModeState {
-  rectangleDrawFeature: MapboxDraw.DrawFeature | undefined;
+  rectangleDrawFeature: MapboxDraw.DrawPolygon | undefined;
   firstPointCoords: [number, number] | undefined;
 }
 
@@ -22,13 +22,15 @@ export const CustomRectangleDrawMode: DrawCustomMode = {
       type: 'Feature',
       properties: {
         isRectangle: true,
+        isInProgress: true,
       },
       geometry: {
         type: 'Polygon',
         coordinates: [[]],
       },
     };
-    const rectangleDrawFeature = this.newFeature(rectangleFeature);
+    const rectangleDrawFeature = this.newFeature(rectangleFeature) as MapboxDraw.DrawPolygon;
+    this.addFeature(rectangleDrawFeature);
 
     const state = defaultState();
     state.rectangleDrawFeature = rectangleDrawFeature;
@@ -42,22 +44,27 @@ export const CustomRectangleDrawMode: DrawCustomMode = {
       // place the first point
       state.firstPointCoords = mouseClickCoords;
     } else {
-      // finalize the rectangle
-      const [minx, miny] = state.firstPointCoords;
-      const [maxx, maxy] = mouseClickCoords;
-
-      const rectangleFeature = bboxPolygon([minx, miny, maxx, maxy]);
-      const rectangleDrawFeature = this.newFeature(rectangleFeature);
-      this.addFeature(rectangleDrawFeature);
-
-      // exit rectangle drawing mode
-      this.map.fire('draw.create', { features: [rectangleDrawFeature] });
+      // rectangle is done. exit rectangle drawing mode
+      this.map.fire('draw.create', { features: [state.rectangleDrawFeature!] });
       this.changeMode('simple_select');
     }
   },
 
   onMouseMove: function (state: RectangleDrawModeState, e: MapboxDraw.MapMouseEvent) {
-    // temp - do nothing
+    const rectangleHasBeenStarted = state.firstPointCoords !== undefined;
+    if (!rectangleHasBeenStarted) {
+      return;
+    }
+
+    // update the rectangle feature with the new coordinates
+    const mouseClickCoords = e.lngLat.toArray();
+
+    const [minx, miny] = state.firstPointCoords!;
+    const [maxx, maxy] = mouseClickCoords;
+
+    const rectangleFeature = bboxPolygon([minx, miny, maxx, maxy]);
+
+    state.rectangleDrawFeature!.setCoordinates(rectangleFeature.geometry.coordinates);
   },
 
   onStop: function (state: RectangleDrawModeState) {
