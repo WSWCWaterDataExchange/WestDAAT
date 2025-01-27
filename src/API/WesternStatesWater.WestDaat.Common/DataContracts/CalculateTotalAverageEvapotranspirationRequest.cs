9a5cfe40870a75c864f352275eafe7b1a58b950f
Ethan Weber <eweber@dontpaniclabs.com>
2025-01-27 14:11:51 -0600

    engine contracts

---
+++ src/API/WesternStatesWater.WestDaat.Common/DataContracts/CalculateTotalAverageEvapotranspirationRequest.cs
@@ -0,0 +1,6 @@
+﻿namespace WesternStatesWater.WestDaat.Common.DataContracts;
+
+public class CalculateTotalAverageEvapotranspirationRequest
+{
+    public RasterTimeSeriesPolygonResponseDatapoint[][] EtDataPointsGroupedByPolygon { get; set; }
+}
---
+++ src/API/WesternStatesWater.WestDaat.Common/DataContracts/CalculateTotalAverageEvapotranspirationResponse.cs
@@ -0,0 +1,6 @@
+﻿namespace WesternStatesWater.WestDaat.Common.DataContracts;
+
+public class CalculateTotalAverageEvapotranspirationResponse
+{
+    public double TotalAverageEvapotranspiration { get; set; }
+}
