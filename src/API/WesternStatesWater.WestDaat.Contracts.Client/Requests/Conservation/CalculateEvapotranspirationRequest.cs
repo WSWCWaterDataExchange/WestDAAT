dd098309ef9256425c128746c0b2aa70dd1997ca
Ethan Weber <eweber@dontpaniclabs.com>
2025-01-27 13:26:21 -0600

    request object and validator

---
+++ src/API/WesternStatesWater.WestDaat.Contracts.Client/Requests/Conservation/CalculateEvapotranspirationRequest.cs
@@ -0,0 +1,13 @@
+﻿using WesternStatesWater.WestDaat.Common.DataContracts;
+
+namespace WesternStatesWater.WestDaat.Contracts.Client.Requests.Conservation;
+
+public class CalculateEvapotranspirationRequest : ApplicationLoadRequestBase
+{
+    public string[] Polygons { get; set; } // WKT format
+    public RasterTimeSeriesModel Model { get; set; }
+    public DateOnly DateRangeStart { get; set; } // inclusive
+    public DateOnly DateRangeEnd { get; set; } // inclusive
+    public int? DesiredCompensation { get; set; } // dollar amount
+    public DesiredCompensationUnits? Units { get; set; } // acre, acre-ft
+}
---
+++ src/API/WesternStatesWater.WestDaat.Contracts.Client/Requests/Conservation/CalculateEvapotranspirationRequestValidator.cs
@@ -0,0 +1,38 @@
+﻿using FluentValidation;
+using NetTopologySuite.IO;
+using WesternStatesWater.WestDaat.Contracts.Client.Requests.Conservation;
+using WesternStatesWater.WestDaat.Utilities;
+
+namespace WesternStatesWater.WestDaat.Contracts.Client.Requests.Admin;
+
+public class CalculateEvapotranspirationRequestValidator : AbstractValidator<CalculateEvapotranspirationRequest>
+{
+    public CalculateEvapotranspirationRequestValidator()
+    {
+        RuleFor(x => x.Polygons).NotEmpty()
+            .Must(polygons => polygons.All(polygon =>
+            {
+                try
+                {
+                    GeometryHelpers.GetGeometryByWkt(polygon);
+                }
+                catch (ParseException)
+                {
+                    return false;
+                }
+
+                return true;
+            }));
+
+        RuleFor(x => x.Model).NotEmpty();
+
+        RuleFor(x => x.DateRangeStart).NotEmpty();
+        RuleFor(x => x.DateRangeEnd).NotEmpty();
+
+        RuleFor(x => x.DateRangeEnd).GreaterThanOrEqualTo(x => x.DateRangeStart);
+
+        // if one property is non-null, then they both must be non-null
+        RuleFor(x => x.DesiredCompensation).NotEmpty().When(x => x.Units.HasValue);
+        RuleFor(x => x.Units).NotEmpty().When(x => x.DesiredCompensation.HasValue);
+    }
+}
