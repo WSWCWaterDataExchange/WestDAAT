e29a698accc7ef25a2459174e12f94eec4a3ce6e
Ethan Weber <eweber@dontpaniclabs.com>
2025-01-27 15:09:16 -0600

    spacing

--- src/API/WesternStatesWater.WestDaat.Contracts.Client/Requests/Conservation/CalculateEvapotranspirationRequest.cs
+++ src/API/WesternStatesWater.WestDaat.Contracts.Client/Requests/Conservation/CalculateEvapotranspirationRequest.cs
@@ -5,10 +5,16 @@ namespace WesternStatesWater.WestDaat.Contracts.Client.Requests.Conservation;
 public class CalculateEvapotranspirationRequest : ApplicationLoadRequestBase
 {
     public Guid FundingOrganizationId { get; set; }
+
     public string[] Polygons { get; set; } // WKT format
+
     public RasterTimeSeriesModel Model { get; set; }
+
     public DateOnly DateRangeStart { get; set; } // inclusive
+
     public DateOnly DateRangeEnd { get; set; } // inclusive
+
     public int? DesiredCompensation { get; set; } // dollar amount
+
     public DesiredCompensationUnits? Units { get; set; } // acre, acre-ft
 }
--- src/API/WesternStatesWater.WestDaat.Contracts.Client/Responses/Conservation/CalculateEvapotranspirationResponse.cs
+++ src/API/WesternStatesWater.WestDaat.Contracts.Client/Responses/Conservation/CalculateEvapotranspirationResponse.cs
@@ -3,6 +3,7 @@
     public class CalculateEvapotranspirationResponse : ApplicationLoadResponseBase
     {
         public double AverageTotalEtInInches { get; set; }
+
         public int? ConservationPayment { get; set; } // dollar amount
     }
 }
