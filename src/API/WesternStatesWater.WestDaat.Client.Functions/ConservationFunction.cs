0a98426fcf91df0ab143b2c228ef3a230ee0a4b9
Ethan Weber <eweber@dontpaniclabs.com>
2025-01-27 15:13:35 -0600

    Create ConservationFunction.cs

---
+++ src/API/WesternStatesWater.WestDaat.Client.Functions/ConservationFunction.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+using WesternStatesWater.WestDaat.Contracts.Client;
+using WesternStatesWater.WestDaat.Contracts.Client.Requests.Conservation;
+using WesternStatesWater.WestDaat.Contracts.Client.Responses.Conservation;
+
+namespace WesternStatesWater.WestDaat.Client.Functions;
+
+public class ConservationFunction : FunctionBase
+{
+    private readonly IApplicationManager _applicationManager;
+    private readonly ILogger _logger;
+
+    private const string RouteBase = "Conservation";
+
+    public ConservationFunction(IApplicationManager applicationManager, ILogger<ConservationFunction> logger)
+    {
+        _applicationManager = applicationManager;
+        _logger = logger;
+    }
+
+    public async Task<HttpResponseData> CalculateEvapotranspiration(
+        [HttpTrigger(AuthorizationLevel.Function, "get", Route = $"{RouteBase}/calculateEt")],
+        HttpRequestData req)
+    {
+        var calculateEtRequest = await ParseRequestBody<CalculateEvapotranspirationRequest>(req);
+        var results = await _applicationManager.Load<CalculateEvapotranspirationRequest, CalculateEvapotranspirationResponse>(calculateEtRequest);
+        return await CreateResponse(req, results);
+    }
+}
