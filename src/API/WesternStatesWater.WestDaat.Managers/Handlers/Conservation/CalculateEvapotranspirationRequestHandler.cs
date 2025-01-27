9806c26576d6106b53ef930eb99291e4a700145d
Ethan Weber <eweber@dontpaniclabs.com>
2025-01-27 13:55:28 -0600

    [wip] handler - submit raster polygon requests to open et

--- src/API/WesternStatesWater.WestDaat.Managers/Handlers/Conservation/CalculateEvapotranspirationRequestHandler.cs
+++ src/API/WesternStatesWater.WestDaat.Managers/Handlers/Conservation/CalculateEvapotranspirationRequestHandler.cs
@@ -1,13 +1,29 @@
 ﻿using WesternStatesWater.Shared.Resolver;
 using WesternStatesWater.WestDaat.Contracts.Client.Requests.Conservation;
 using WesternStatesWater.WestDaat.Contracts.Client.Responses.Conservation;
+using WesternStatesWater.WestDaat.Managers.Mapping;
+using WesternStatesWater.WestDaat.Utilities;
+
+using CommonContracts = WesternStatesWater.WestDaat.Common.DataContracts;
 
 namespace WesternStatesWater.WestDaat.Managers.Handlers.Conservation;
 
 public class CalculateEvapotranspirationRequestHandler : IRequestHandler<CalculateEvapotranspirationRequest, CalculateEvapotranspirationResponse>
 {
-    public Task<CalculateEvapotranspirationResponse> Handle(CalculateEvapotranspirationRequest request)
+    public IOpenEtSdk OpenEtSdk { get; }
+
+    public CalculateEvapotranspirationRequestHandler(IOpenEtSdk openEtSdk)
     {
-        throw new NotImplementedException();
+        OpenEtSdk = openEtSdk;
+    }
+
+    public async Task<CalculateEvapotranspirationResponse> Handle(CalculateEvapotranspirationRequest request)
+    {
+        var rasterTimeSeriesPolygonRequests = request.Polygons.Select(polygon =>
+            DtoMapper.Map<CommonContracts.RasterTimeSeriesPolygonRequest>((request, polygon))
+        );
+        var rasterTimeSeriesPolygonResponses = await Task.WhenAll(rasterTimeSeriesPolygonRequests.Select(OpenEtSdk.RasterTimeseriesPolygon));
+
+
     }
 }
