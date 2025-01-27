52d11800d8ea1678cd3cada95e3435e8f3c3ea34
Ethan Weber <eweber@dontpaniclabs.com>
2025-01-27 13:29:01 -0600

    stub in handler

---
+++ src/API/WesternStatesWater.WestDaat.Managers/Handlers/Conservation/CalculateEvapotranspirationRequestHandler.cs
@@ -0,0 +1,13 @@
+﻿using WesternStatesWater.Shared.Resolver;
+using WesternStatesWater.WestDaat.Contracts.Client.Requests.Conservation;
+using WesternStatesWater.WestDaat.Contracts.Client.Responses.Conservation;
+
+namespace WesternStatesWater.WestDaat.Managers.Handlers.Conservation;
+
+public class CalculateEvapotranspirationRequestHandler : IRequestHandler<CalculateEvapotranspirationRequest, CalculateEvapotranspirationResponse>
+{
+    public Task<CalculateEvapotranspirationResponse> Handle(CalculateEvapotranspirationRequest request)
+    {
+        throw new NotImplementedException();
+    }
+}
