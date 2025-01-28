0b1855d015848b365cea3412f7b42e870ffeac08
Ethan Weber <eweber@dontpaniclabs.com>
2025-01-28 09:23:17 -0600

    split application and submission into two tables

--- src/API/WesternStatesWater.WestDaat.DbUp/Scripts/001/004 Conservation Applications.sql
+++ src/API/WesternStatesWater.WestDaat.DbUp/Scripts/001/004 Conservation Applications.sql
@@ -3,16 +3,25 @@ CREATE TABLE WaterConservationApplications
     Id 	                    UNIQUEIDENTIFIER NOT NULL CONSTRAINT PK_WaterConservationApplications_Id PRIMARY KEY NONCLUSTERED,
     ApplicantUserId         UNIQUEIDENTIFIER NOT NULL,
     FundingOrganizationId   UNIQUEIDENTIFIER NOT NULL,
-    SubmittedDate           DATETIMEOFFSET NOT NULL,
-    CompensationRateDollars int NOT NULL,
-    CompensationRateUnits   int NOT NULL,
-    ApplicationDisplayId    NVARCHAR(255) NOT NULL,
-    WaterRightNativeId      NVARCHAR(255) NOT NULL,
-    AcceptedDate            DATETIMEOFFSET NULL,
-    RejectedDate            DATETIMEOFFSET NULL,
     CONSTRAINT FK_Applications_Users FOREIGN KEY (ApplicantUserId) REFERENCES Users (Id),
     CONSTRAINT FK_Applications_FundingOrganizations FOREIGN KEY (FundingOrganizationId) REFERENCES Organizations (Id)
 );
 
 CREATE INDEX IX_WaterConservationApplications_ApplicantUserId ON WaterConservationApplications(ApplicantUserId);
 CREATE INDEX IX_WaterConservationApplications_FundingOrganizationId ON WaterConservationApplications(OrganizationId);
+
+CREATE TABLE WaterConservationApplicationSubmissions
+(
+    Id 	                            UNIQUEIDENTIFIER NOT NULL CONSTRAINT PK_WaterConservationApplicationSubmissions_Id PRIMARY KEY NONCLUSTERED,
+    WaterConservationApplicationId  UNIQUEIDENTIFIER NOT NULL UNIQUE, -- application can only have one submission
+    SubmittedDate                   DATETIMEOFFSET NOT NULL,
+    CompensationRateDollars         int NOT NULL,
+    CompensationRateUnits           int NOT NULL,
+    ApplicationDisplayId            NVARCHAR(255) NOT NULL,
+    WaterRightNativeId              NVARCHAR(255) NOT NULL,
+    AcceptedDate                    DATETIMEOFFSET NULL,
+    RejectedDate                    DATETIMEOFFSET NULL,
+    CONSTRAINT FK_WaterConservationApplicationSubmissions_WaterConservationApplications FOREIGN KEY (WaterConservationApplicationId) REFERENCES WaterConservationApplications (Id)
+)
+
+CREATE INDEX IX_WaterConservationApplicationSubmissions_WaterConservationApplicationId ON WaterConservationApplicationSubmissions(WaterConservationApplicationId);
