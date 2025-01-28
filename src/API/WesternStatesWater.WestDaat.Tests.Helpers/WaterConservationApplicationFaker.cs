422198df12e365baa549c77a14171308755a0aec
Ethan Weber <eweber@dontpaniclabs.com>
2025-01-28 09:28:05 -0600

    add/update fakers

--- src/API/WesternStatesWater.WestDaat.Tests.Helpers/WaterConservationApplicationFaker.cs
+++ src/API/WesternStatesWater.WestDaat.Tests.Helpers/WaterConservationApplicationFaker.cs
@@ -4,16 +4,6 @@ public class WaterConservationApplicationFaker : Faker<EFWD.WaterConservationApp
 {
     public WaterConservationApplicationFaker(EFWD.User user = null, EFWD.Organization organization = null)
     {
-        RuleFor(wca => wca.SubmittedDate, f => f.Date.PastOffset(1, DateTimeOffset.UtcNow));
-
-        RuleFor(wca => wca.CompensationRateDollars, f => f.Random.Int(100, 1000));
-
-        RuleFor(wca => wca.CompensationRateUnits, f => f.PickRandomWithout(Common.DataContracts.CompensationRateUnits.None));
-
-        RuleFor(wca => wca.ApplicationDisplayId, f => f.Random.String2(10));
-
-        RuleFor(wca => wca.WaterRightNativeId, f => f.Random.String(11, 'A', 'z'));
-
         if (user != null)
         {
             RuleFor(wca => wca.ApplicantUser, () => user);
---
+++ src/API/WesternStatesWater.WestDaat.Tests.Helpers/WaterConservationApplicationSubmissionFaker.cs
@@ -0,0 +1,22 @@
+﻿namespace WesternStatesWater.WestDaat.Tests.Helpers;
+
+public class WaterConservationApplicationSubmissionFaker : Faker<EFWD.WaterConservationApplicationSubmission>
+{
+    public WaterConservationApplicationSubmissionFaker(EFWD.WaterConservationApplication application = null)
+    {
+        RuleFor(wcas => wcas.SubmittedDate, f => f.Date.PastOffset(1, DateTimeOffset.UtcNow));
+
+        RuleFor(wcas => wcas.CompensationRateDollars, f => f.Random.Int(100, 1000));
+
+        RuleFor(wcas => wcas.CompensationRateUnits, f => f.PickRandomWithout(Common.DataContracts.CompensationRateUnits.None));
+
+        RuleFor(wcas => wcas.ApplicationDisplayId, f => f.Random.String2(10));
+
+        RuleFor(wcas => wcas.WaterRightNativeId, f => f.Random.String(11, 'A', 'z'));
+
+        if (application != null)
+        {
+            RuleFor(wcas => wcas.WaterConservationApplication, () => application);
+        }
+    }
+}
