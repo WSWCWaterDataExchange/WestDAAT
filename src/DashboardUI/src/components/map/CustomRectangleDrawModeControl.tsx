f5f5efd06fd858157a9fbd1c42ea793e9986caa5
Ethan Weber <eweber@dontpaniclabs.com>
2025-01-28 10:39:35 -0600

    update icon and text

--- src/DashboardUI/src/components/map/CustomRectangleDrawModeControl.tsx
+++ src/DashboardUI/src/components/map/CustomRectangleDrawModeControl.tsx
@@ -1,10 +1,10 @@
 import MapboxDraw from '@mapbox/mapbox-gl-draw';
 import { CustomMapControl } from './CustomMapControl';
-import { mdiRectangleOutline } from '@mdi/js';
+import { mdiVectorRectangle } from '@mdi/js';
 
 export class CustomRectangleDrawModeControl extends CustomMapControl {
   constructor(mapboxDraw: MapboxDraw | null) {
-    super(mdiRectangleOutline, 'rectangle draw tool', () => {
+    super(mdiVectorRectangle, 'Rectangle tool', () => {
       mapboxDraw?.changeMode('draw_rectangle');
     });
   }
