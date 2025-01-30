a7898cf4379b862e6470547b3a483207489b2864
Ethan Weber <eweber@dontpaniclabs.com>
2025-01-30 12:13:36 -0600

    Application store contracts / interface method

--- src/API/WesternStatesWater.WestDaat.Accessors/ApplicationAccessor.cs
+++ src/API/WesternStatesWater.WestDaat.Accessors/ApplicationAccessor.cs
@@ -1,13 +1,19 @@
 using Microsoft.Extensions.Logging;
+using WesternStatesWater.WestDaat.Common.DataContracts;
 
 namespace WesternStatesWater.WestDaat.Accessors;
 
 internal class ApplicationAccessor : AccessorBase, IApplicationAccessor
 {
+    private readonly EF.IDatabaseContextFactory _databaseContextFactory;
+
     public ApplicationAccessor(ILogger<ApplicationAccessor> logger, EF.IDatabaseContextFactory databaseContextFactory) : base(logger)
     {
         _databaseContextFactory = databaseContextFactory;
     }
 
-    private readonly EF.IDatabaseContextFactory _databaseContextFactory;
+    public Task<ApplicationStoreResponseBase> Store(ApplicationStoreRequestBase request)
+    {
+        throw new NotImplementedException();
+    }
 }
--- src/API/WesternStatesWater.WestDaat.Accessors/IApplicationAccessor.cs
+++ src/API/WesternStatesWater.WestDaat.Accessors/IApplicationAccessor.cs
@@ -1,7 +1,9 @@
 using WesternStatesWater.WestDaat.Common;
+using WesternStatesWater.WestDaat.Common.DataContracts;
 
 namespace WesternStatesWater.WestDaat.Accessors;
 
 public interface IApplicationAccessor : IServiceContractBase
 {
+    Task<ApplicationStoreResponseBase> Store(ApplicationStoreRequestBase request);
 }
