29c9aa8f6fe9bee8bf64943bcea363efdade50b1
Ethan Weber <eweber@dontpaniclabs.com>
2025-02-04 09:17:06 -0600

    Create WaterConservationApplicationCreateRequestHandler.cs

---
+++ src/API/WesternStatesWater.WestDaat.Managers/Handlers/Conservation/WaterConservationApplicationCreateRequestHandler.cs
@@ -0,0 +1,17 @@
+﻿using WesternStatesWater.Shared.Resolver;
+using WesternStatesWater.WestDaat.Contracts.Client.Requests.Conservation;
+using WesternStatesWater.WestDaat.Contracts.Client.Responses.Conservation;
+
+namespace WesternStatesWater.WestDaat.Managers.Handlers.Conservation;
+
+public class WaterConservationApplicationCreateRequestHandler :
+    IRequestHandler<WaterConservationApplicationCreateRequest, WaterConservationApplicationCreateResponse>
+{
+    public WaterConservationApplicationCreateRequestHandler()
+    {
+    }
+    public Task<WaterConservationApplicationCreateResponse> Handle(WaterConservationApplicationCreateRequest request)
+    {
+        throw new NotImplementedException();
+    }
+}
