f001737460c62afb5ac1ec9299a84861c3170922
Ethan Weber <eweber@dontpaniclabs.com>
2025-02-04 14:25:42 -0600

    add organization accessor tests for loading org details

--- src/API/WesternStatesWater.WestDaat.Tests.AccessorTests/OrganizationAccessorTests.cs
+++ src/API/WesternStatesWater.WestDaat.Tests.AccessorTests/OrganizationAccessorTests.cs
@@ -1,5 +1,8 @@
 using WesternStatesWater.WestDaat.Accessors;
 using WesternStatesWater.WestDaat.Common.DataContracts;
+using WesternStatesWater.WestDaat.Common.Exceptions;
+using WesternStatesWater.WestDaat.Database.EntityFramework;
+using WesternStatesWater.WestDaat.Tests.Helpers;
 
 namespace WesternStatesWater.WestDaat.Tests.AccessorTests;
 
@@ -8,13 +11,19 @@ public class OrganizationAccessorTests : AccessorTestBase
 {
     private IOrganizationAccessor _organizationAccessor;
 
+    private WestDaatDatabaseContext _db;
+
     [TestInitialize]
     public void TestInitialize()
     {
+        var dbFactory = CreateWestDaatDatabaseContextFactory();
+
         _organizationAccessor = new OrganizationAccessor(
             CreateLogger<OrganizationAccessor>(),
-            CreateWestDaatDatabaseContextFactory()
+            dbFactory
         );
+
+        _db = dbFactory.Create();
     }
 
     [TestMethod]
@@ -25,6 +34,45 @@ public class OrganizationAccessorTests : AccessorTestBase
 
         // Act + Assert
         Assert.ThrowsExceptionAsync<NotImplementedException>(() => _organizationAccessor.Load(request));
+    }
+
+    [TestMethod]
+    public async Task Load_GetOrganizationDetails_ShouldReturnOrganization()
+    {
+        // Arrange
+        var organization = new OrganizationFaker().Generate();
+
+        await _db.Organizations.AddAsync(organization);
+        await _db.SaveChangesAsync();
+
+        var request = new OrganizationLoadDetailsRequest
+        {
+            OrganizationId = organization.Id
+        };
+
+        // Act
+        var response = (OrganizationLoadDetailsResponse)await _organizationAccessor.Load(request);
+
+        // Assert
+        response.Should().NotBeNull();
+        response.Organization.Should().NotBeNull();
+        response.Organization.OrganizationId.Should().Be(organization.Id);
+        response.Organization.AgencyId.Should().Be(organization.AgencyId);
+    }
+
+    [TestMethod]
+    public async Task Load_GetOrganizationDetails_InvalidOrganizationId_ShouldThrow()
+    {
+        // Arrange
+        var request = new OrganizationLoadDetailsRequest
+        {
+            OrganizationId = Guid.NewGuid()
+        };
+
+        // Act + Assert
+        var call = () => _organizationAccessor.Load(request);
+
+        await call.Should().ThrowAsync<WestDaatException>();
     }
 
     private class InvalidOrganizationLoadRequest : OrganizationLoadRequestBase
