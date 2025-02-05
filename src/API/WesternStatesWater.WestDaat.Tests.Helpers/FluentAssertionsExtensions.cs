116afc4ec40f130d242e7487e953e4ae7f0116c4
Ethan Weber <eweber@dontpaniclabs.com>
2025-02-05 13:42:28 -0600

    fluent assertions extensions + test

---
+++ src/API/WesternStatesWater.WestDaat.Tests.Helpers/FluentAssertionsExtensions.cs
@@ -0,0 +1,18 @@
+﻿using FluentAssertions;
+using FluentAssertions.Numeric;
+
+namespace WesternStatesWater.WestDaat.Tests.Helpers;
+
+public static class FluentAssertionsExtensions
+{
+    public static void BeWithinPercentOf(this NumericAssertions<double> assertions, double expectedValue, double percentage)
+    {
+        var actualValue = assertions.Subject.Value;
+        var acceptableDifference = expectedValue * percentage / 100;
+
+        var maximumAcceptableValue = expectedValue + acceptableDifference;
+        var minimumAcceptableValue = expectedValue - acceptableDifference;
+
+        assertions.Subject.Should().BeInRange(minimumAcceptableValue, maximumAcceptableValue, $"because the value should be within {percentage}% of {expectedValue}");
+    }
+}
