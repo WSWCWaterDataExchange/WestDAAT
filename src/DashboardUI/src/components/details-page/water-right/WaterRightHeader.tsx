e11ea79e8dd9132c7fe860fec211a50ec8f4d5f6
Ethan Weber <eweber@dontpaniclabs.com>
2025-02-05 16:16:26 -0600

    stub in right-hand side of header

--- src/DashboardUI/src/components/details-page/water-right/WaterRightHeader.tsx
+++ src/DashboardUI/src/components/details-page/water-right/WaterRightHeader.tsx
@@ -1,9 +1,19 @@
 function WaterRightHeader() {
   return (
-    <div className="d-flex flex-row align-items-center justify-content-center title-header">
-      <h3 className="d-flex fw-bold">
-        <div>WaDE Water Right Landing Page</div>
-      </h3>
+    <div className="d-flex flex-row align-items-center justify-content-between title-header">
+      <div>
+        <h3 className="d-flex fw-bold">WaDE Water Right Landing Page</h3>
+      </div>
+
+      <div className="d-flex flex-column align-items-end">
+        <div className="d-flex flex-row gap-3">
+          <div>info circle button</div>
+          <div>estimate consumptive use button</div>
+        </div>
+        <div>
+          <span>T&C link text</span>
+        </div>
+      </div>
     </div>
   );
 }
