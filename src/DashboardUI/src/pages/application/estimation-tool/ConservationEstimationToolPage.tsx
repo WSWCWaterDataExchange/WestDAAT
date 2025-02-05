6463a1e7100be908d0c2547bf52d252807803f4e
Ethan Weber <eweber@dontpaniclabs.com>
2025-02-05 17:00:37 -0600

    grab waterRightNativeId from route params

--- src/DashboardUI/src/pages/application/estimation-tool/ConservationEstimationToolPage.tsx
+++ src/DashboardUI/src/pages/application/estimation-tool/ConservationEstimationToolPage.tsx
@@ -1,5 +1,10 @@
+import { useParams } from 'react-router-dom';
 import { NotImplementedPlaceholder } from '../../../components/NotImplementedAlert';
 
 export function ConservationEstimationToolPage() {
+  const routeParams = useParams();
+  const { waterRightNativeId } = routeParams;
+  console.log('waterRightNativeId', waterRightNativeId);
+
   return <NotImplementedPlaceholder />;
 }
