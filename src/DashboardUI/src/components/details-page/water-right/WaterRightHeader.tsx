06be1dcbf2e41353b1e6d78adfde000f7f6adc94
Ethan Weber <eweber@dontpaniclabs.com>
2025-02-05 16:23:23 -0600

    help icon, button, t&c link content

--- src/DashboardUI/src/components/details-page/water-right/WaterRightHeader.tsx
+++ src/DashboardUI/src/components/details-page/water-right/WaterRightHeader.tsx
@@ -1,3 +1,7 @@
+import { mdiHelpCircleOutline } from '@mdi/js';
+import Icon from '@mdi/react';
+import { Button } from 'react-bootstrap';
+
 function WaterRightHeader() {
   return (
     <div className="d-flex flex-row align-items-center justify-content-between title-header">
@@ -5,13 +9,19 @@ function WaterRightHeader() {
         <h3 className="d-flex fw-bold">WaDE Water Right Landing Page</h3>
       </div>
 
-      <div className="d-flex flex-column align-items-end">
-        <div className="d-flex flex-row gap-3">
-          <div>info circle button</div>
-          <div>estimate consumptive use button</div>
+      <div className="d-flex flex-column align-items-end gap-1">
+        <div className="d-flex flex-row gap-3 align-items-center">
+          <div>
+            <Icon path={mdiHelpCircleOutline} size="1.5em" />
+          </div>
+          <div>
+            <Button variant="primary">Estimate Consumptive Use</Button>
+          </div>
         </div>
         <div>
-          <span>T&C link text</span>
+          <span>
+            By clicking this button, you are agreeing to WestDAAT’s <a href="#">Terms & Conditions</a>
+          </span>
         </div>
       </div>
     </div>
