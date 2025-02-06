d67f1cdb6ed766419f886721e2e8ff5078f73742
Ethan Weber <eweber@dontpaniclabs.com>
2025-02-06 14:34:53 -0600

    update migration

--- src/API/WesternStatesWater.WestDaat.DbUp/Scripts/001/009 Organization and Application Updates.sql
+++ src/API/WesternStatesWater.WestDaat.DbUp/Scripts/001/009 Organization and Application Updates.sql
@@ -2,4 +2,4 @@ ALTER TABLE WaterConservationApplications
 	ADD CONSTRAINT UQ_WaterConservationApplications_ApplicationDisplayId UNIQUE (ApplicationDisplayId);
 
 ALTER TABLE Organizations
-	ADD AgencyId NVARCHAR(255) NOT NULL;
+	ADD AbbreviatedName NVARCHAR(255) NOT NULL;
