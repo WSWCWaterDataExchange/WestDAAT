eff58f3a12f18edc3f89fad7622769874d22d719
Ethan Weber <eweber@dontpaniclabs.com>
2025-02-10 14:07:27 -0600

    navbar brand header

--- src/DashboardUI/src/pages/application/estimation-tool/EstimationToolNavbar.tsx
+++ src/DashboardUI/src/pages/application/estimation-tool/EstimationToolNavbar.tsx
@@ -1,4 +1,4 @@
-import { mdiArrowLeft } from '@mdi/js';
+import { mdiArrowLeft, mdiWater } from '@mdi/js';
 import Icon from '@mdi/react';
 import { Button, Container, Nav, Navbar } from 'react-bootstrap';
 
@@ -12,16 +12,24 @@ export function EstimationToolNavbar(props: EstimationToolNavbarProps) {
       <Container fluid>
         <Nav>
           <Nav.Item onClick={props.navigateToWaterRightLandingPage}>
+            {/* render as <button> instead of <a role="button"> */}
             <Nav.Link as="button" className="text-dark">
               <div className="d-flex flex-row align-items-center gap-2">
                 <Icon path={mdiArrowLeft} size="1em" />
-                Back to Water Right Landing Page
+                <span>Back to Water Right Landing Page</span>
               </div>
             </Nav.Link>
           </Nav.Item>
         </Nav>
 
-        <Navbar.Brand>WSWC</Navbar.Brand>
+        <Navbar.Brand>
+          <div className="d-flex flex-row align-items-center gap-2">
+            <Icon path={mdiWater} size="1em" />
+            <span>Water Conservation Estimation Tool</span>
+          </div>
+        </Navbar.Brand>
+
+        <Nav>{/* placeholder element for flexbox alignment */}</Nav>
       </Container>
     </Navbar>
   );
