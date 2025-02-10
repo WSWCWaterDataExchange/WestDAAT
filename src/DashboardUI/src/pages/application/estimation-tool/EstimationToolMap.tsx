5945d5619472ee53224f89a6587c45b3bf675715
Ethan Weber <eweber@dontpaniclabs.com>
2025-02-10 14:26:08 -0600

    custom polygon change handler

--- src/DashboardUI/src/pages/application/estimation-tool/EstimationToolMap.tsx
+++ src/DashboardUI/src/pages/application/estimation-tool/EstimationToolMap.tsx
@@ -1,16 +1,14 @@
-import { useMapFitRequested } from '../../../components/home-page/water-rights-tab/map-options/hooks/useMapFitRequested';
-import { usePolylinesFilter } from '../../../components/home-page/water-rights-tab/map-options/hooks/usePolylinesFilter';
 import Map from '../../../components/map/Map';
 
 export function EstimationToolMap() {
-  const { polylinesOnMapUpdated } = usePolylinesFilter();
-  const { handleMapFitRequested } = useMapFitRequested();
+  const handleMapDrawnPolygonChange = (polygons: any) => {
+    alert('polygons updated');
+  };
 
   return (
     <Map
+      handleMapDrawnPolygonChange={handleMapDrawnPolygonChange}
       consumptiveUseAlertEnabled={false}
-      handleMapDrawnPolygonChange={polylinesOnMapUpdated}
-      handleMapFitChange={handleMapFitRequested}
       geocoderEnabled={false}
     />
   );
