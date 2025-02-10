b75a90525a88c9023e73a21852bc7dc9bcab0e59
Ethan Weber <eweber@dontpaniclabs.com>
2025-02-10 13:48:19 -0600

    navbar - style the back link

--- src/DashboardUI/src/pages/application/estimation-tool/EstimationToolNavbar.tsx
+++ src/DashboardUI/src/pages/application/estimation-tool/EstimationToolNavbar.tsx
@@ -1,4 +1,6 @@
-import { Container, Nav, Navbar } from 'react-bootstrap';
+import { mdiArrowLeft } from '@mdi/js';
+import Icon from '@mdi/react';
+import { Button, Container, Nav, Navbar } from 'react-bootstrap';
 
 interface EstimationToolNavbarProps {
   navigateToWaterRightLandingPage: () => void;
@@ -7,10 +9,18 @@ interface EstimationToolNavbarProps {
 export function EstimationToolNavbar(props: EstimationToolNavbarProps) {
   return (
     <Navbar className="p-0 second-nav estimate-tool-navbar">
-      <Container fluid className="p-0">
+      <Container fluid>
         <Nav>
-          <Nav.Link onClick={props.navigateToWaterRightLandingPage}>Back to Water Right Landing Page</Nav.Link>
+          <Nav.Item onClick={props.navigateToWaterRightLandingPage}>
+            <Nav.Link as="button" className="text-dark">
+              <div className="d-flex flex-row align-items-center gap-2">
+                <Icon path={mdiArrowLeft} size="1em" />
+                Back to Water Right Landing Page
+              </div>
+            </Nav.Link>
+          </Nav.Item>
         </Nav>
+
         <Navbar.Brand>WSWC</Navbar.Brand>
       </Container>
     </Navbar>
