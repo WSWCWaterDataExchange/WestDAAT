ebc3e52fdf09f0a62ef1aabea608ce7d417afd92
Jenny Nielsen <jnielsen@dontpaniclabs.com>
2025-02-10 15:48:43 -0600

    update property names to be camelCase

--- src/DashboardUI/src/data-contracts/ApplicationDashboardListItem.ts
+++ src/DashboardUI/src/data-contracts/ApplicationDashboardListItem.ts
@@ -2,21 +2,16 @@ import { CompensationRateUnits } from './CompensationRateUnits';
 import { ConservationApplicationStatus } from './ConservationApplicationStatus';
 
 export interface ApplicationDashboardListItem {
-  ApplicantFullName: string;
-  ApplicationDisplayId: string;
-  ApplicationId: string;
-  CompensationRateDollars: number;
-  CompensationRateUnits: CompensationRateUnits;
-  OrganizationName: string;
-  Status: ConservationApplicationStatus;
-  /**
-   * Date the application was submitted,
-   * in DateTimeOffset format
-   * (ex: "2025-02-02T18:40:04.2611554+00:00")
-   */
-  SubmittedDate: string;
-  TotalObligationDollars: number;
-  TotalWaterVolumeSavingsAcreFeet: number;
-  WaterRightNativeId: string;
-  WaterRightState: string;
+  applicantFullName: string;
+  applicationDisplayId: string;
+  applicationId: string;
+  compensationRateDollars: number;
+  compensationRateUnits: CompensationRateUnits;
+  organizationName: string;
+  status: ConservationApplicationStatus;
+  submittedDate: Date;
+  totalObligationDollars: number;
+  totalWaterVolumeSavingsAcreFeet: number;
+  waterRightNativeId: string;
+  waterRightState: string;
 }
