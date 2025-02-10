28da120cc3d6ee42a712373ba3fa6502cb819715
Ethan Weber <eweber@dontpaniclabs.com>
2025-02-10 14:17:06 -0600

    fix styling so page fills full height properly

--- src/DashboardUI/src/pages/application/estimation-tool/EstimationToolSidebar.tsx
+++ src/DashboardUI/src/pages/application/estimation-tool/EstimationToolSidebar.tsx
@@ -2,7 +2,7 @@ import { NotImplementedPlaceholder } from '../../../components/NotImplementedAle
 
 export function EstimationToolSidebar() {
   return (
-    <div className="position-relative flex-grow-1 panel-content" style={{ height: '90vh' }}>
+    <div className="position-relative flex-grow-1 panel-content">
       <NotImplementedPlaceholder />
     </div>
   );
