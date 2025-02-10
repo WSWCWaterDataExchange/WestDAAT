a5b70c514636bf0eb27f98b26b29a57b5da53c51
Ethan Weber <eweber@dontpaniclabs.com>
2025-02-10 10:14:08 -0600

    layout

--- src/DashboardUI/src/pages/application/estimation-tool/EstimationToolPage.tsx
+++ src/DashboardUI/src/pages/application/estimation-tool/EstimationToolPage.tsx
@@ -1,9 +1,33 @@
 import { useParams } from 'react-router-dom';
 import { NotImplementedPlaceholder } from '../../../components/NotImplementedAlert';
+import SidePanel from '../../../components/home-page/SidePanel';
+import { EstimationToolSidebar } from './EstimationToolSidebar';
+import MainPanel from '../../../components/home-page/MainPanel';
+import { EstimationToolMapHeader } from './EstimationToolMapHeader';
+import { EstimationToolMap } from './EstimationToolMap';
+import MapProvider from '../../../contexts/MapProvider';
 
 export function EstimationToolPage() {
   const routeParams = useParams();
   const { waterRightNativeId } = routeParams;
 
-  return <NotImplementedPlaceholder />;
+  return (
+    <MapProvider>
+      <EstimationToolLayout />
+    </MapProvider>
+  );
+}
+
+function EstimationToolLayout() {
+  return (
+    <>
+      <SidePanel>
+        <EstimationToolSidebar />
+      </SidePanel>
+      <MainPanel>
+        <EstimationToolMapHeader />
+        <EstimationToolMap />
+      </MainPanel>
+    </>
+  );
 }
