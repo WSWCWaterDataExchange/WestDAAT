bc9b0e3981707ea37e95cdebb8fcdeb9151988cf
Ethan Weber <eweber@dontpaniclabs.com>
2025-02-10 13:22:41 -0600

    [wip] navbar

--- src/DashboardUI/src/pages/application/estimation-tool/EstimationToolNavbar.tsx
+++ src/DashboardUI/src/pages/application/estimation-tool/EstimationToolNavbar.tsx
@@ -1,3 +1,18 @@
-export function EstimationToolNavbar() {
-  return <></>;
+import { Container, Nav, Navbar } from 'react-bootstrap';
+
+interface EstimationToolNavbarProps {
+  navigateToWaterRightLandingPage: () => void;
+}
+
+export function EstimationToolNavbar(props: EstimationToolNavbarProps) {
+  return (
+    <Navbar bg="light" className="p-0 second-nav">
+      <Container fluid className="p-0">
+        <Nav>
+          <Nav.Link onClick={props.navigateToWaterRightLandingPage}>Back to Water Right Landing Page</Nav.Link>
+        </Nav>
+        <Navbar.Brand>WSWC</Navbar.Brand>
+      </Container>
+    </Navbar>
+  );
 }
--- src/DashboardUI/src/pages/application/estimation-tool/EstimationToolPage.tsx
+++ src/DashboardUI/src/pages/application/estimation-tool/EstimationToolPage.tsx
@@ -1,4 +1,4 @@
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import SidePanel from '../../../components/home-page/SidePanel';
 import { EstimationToolSidebar } from './EstimationToolSidebar';
 import MainPanel from '../../../components/home-page/MainPanel';
@@ -7,30 +7,31 @@ import { EstimationToolMap } from './EstimationToolMap';
 import MapProvider from '../../../contexts/MapProvider';
 
 import './estimation-tool-page.scss';
+import { EstimationToolNavbar } from './EstimationToolNavbar';
 
 export function EstimationToolPage() {
+  const navigate = useNavigate();
   const routeParams = useParams();
   const { waterRightNativeId } = routeParams;
 
-  return (
-    <div className="d-inline-flex flex-grow-1 overflow-hidden align-items-stretch">
-      <MapProvider>
-        <EstimationToolLayout />
-      </MapProvider>
-    </div>
-  );
-}
+  const navigateToWaterRightLandingPage = () => {
+    navigate(-1);
+  };
 
-function EstimationToolLayout() {
   return (
-    <>
-      <SidePanel>
-        <EstimationToolSidebar />
-      </SidePanel>
-      <MainPanel>
-        <EstimationToolMapHeader />
-        <EstimationToolMap />
-      </MainPanel>
-    </>
+    <MapProvider>
+      <div className="estimation-tool-page d-flex flex-column">
+        <EstimationToolNavbar navigateToWaterRightLandingPage={navigateToWaterRightLandingPage} />
+        <div className="d-inline-flex flex-grow-1 overflow-hidden align-items-stretch">
+          <SidePanel>
+            <EstimationToolSidebar />
+          </SidePanel>
+          <MainPanel>
+            <EstimationToolMapHeader />
+            <EstimationToolMap />
+          </MainPanel>
+        </div>
+      </div>
+    </MapProvider>
   );
 }
