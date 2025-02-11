8ede00a7edd0cfcd526741d3c3c54494dfe1c47c
Ethan Weber <eweber@dontpaniclabs.com>
2025-02-11 16:53:38 -0600

    acreage display

--- src/DashboardUI/src/pages/application/estimation-tool/EstimationToolSidebar.tsx
+++ src/DashboardUI/src/pages/application/estimation-tool/EstimationToolSidebar.tsx
@@ -1,10 +1,19 @@
 import MapTheme from '../../../components/home-page/water-rights-tab/map-options/components/MapTheme';
 
 export function EstimationToolSidebar() {
+  const acreageSum = 0;
   return (
     <div className="flex-grow-1 panel-content">
       <div className="container-fluid">
-        <SidebarElement title="CALCULATED SHAPE AREA FOR ALL IRRIGATED FIELDS">number of Acres</SidebarElement>
+        <SidebarElement title="CALCULATED SHAPE AREA FOR ALL IRRIGATED FIELDS">
+          <div>
+            <span className="fs-5 fw-bold text-primary">{acreageSum} Acres</span>
+          </div>
+
+          <div>
+            <span className="text-muted">Note: Polygons must be smaller than 50,000 acres.</span>
+          </div>
+        </SidebarElement>
 
         <SidebarElement title="FUNDING ORGANIZATION">placeholder</SidebarElement>
 
@@ -36,7 +45,7 @@ function SidebarElement(props: SidebarElementProps) {
   return (
     <div className="sidebar-element mb-4">
       <div>
-        <span>{props.title}</span>
+        <span className="fs-5 fw-bold">{props.title}</span>
       </div>
       <div>{props.children}</div>
     </div>
