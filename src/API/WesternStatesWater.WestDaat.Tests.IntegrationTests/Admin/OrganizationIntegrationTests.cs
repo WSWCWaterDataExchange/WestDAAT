802308827a643d52a77630d143e11300b28e41b7
Branden Barber <bbarber@dontpaniclabs.com>
2025-02-11 14:53:33 -0600

    Role checking on who can be what

--- src/API/WesternStatesWater.WestDaat.Tests.IntegrationTests/Admin/OrganizationIntegrationTests.cs
+++ src/API/WesternStatesWater.WestDaat.Tests.IntegrationTests/Admin/OrganizationIntegrationTests.cs
@@ -1,4 +1,5 @@
 using Microsoft.Extensions.DependencyInjection;
+using WesternStatesWater.Shared.Errors;
 using WesternStatesWater.WestDaat.Common;
 using WesternStatesWater.WestDaat.Common.Context;
 using WesternStatesWater.WestDaat.Database.EntityFramework;
@@ -109,4 +110,66 @@ public class OrganizationIntegrationTests : IntegrationTestBase
         response.Error.Should().NotBeNull();
         response.Error!.LogMessage.Should().Contain("but did not have permission to do so.");
     }
+
+    [DataTestMethod]
+    [DataRow(true, Roles.Member, false, DisplayName = "Member should not be allowed to add members")]
+    [DataRow(true, Roles.TechnicalReviewer, false, DisplayName = "Technical reviewer should not be allowed to add members")]
+    [DataRow(true, Roles.OrganizationAdmin, true, DisplayName = "Organization admin should be allowed to add members")]
+    [DataRow(false, Roles.GlobalAdmin, true, DisplayName = "Global admin should be allowed to add members")]
+    public async Task Store_OrganizationMemberAddRequest_ShouldThrowIfUserHasInsufficientPermissions(bool isOrgRole, string role, bool isAllowed)
+    {
+        // Arrange
+        var organization = new OrganizationFaker().Generate();
+        var user = new UserFaker().Generate();
+        var userToBeAdded = new UserFaker().Generate();
+        var userOrg = new UserOrganizationFaker(user, organization).Generate();
+
+        await _dbContext.Organizations.AddAsync(organization);
+        await _dbContext.Users.AddRangeAsync(user, userToBeAdded);
+        await _dbContext.UserOrganizations.AddAsync(userOrg);
+        await _dbContext.SaveChangesAsync();
+
+        UseUserContext(new UserContext
+        {
+            UserId = user.Id,
+            Roles = !isOrgRole ? [role] : [],
+            OrganizationRoles = isOrgRole
+                ?
+                [
+                    new OrganizationRole
+                    {
+                        OrganizationId = organization.Id,
+                        RoleNames = [role]
+                    }
+                ]
+                : [],
+            ExternalAuthId = ""
+        });
+
+        // Act
+        var response = await _organizationManager.Store<OrganizationMemberAddRequest, OrganizationMemberAddResponse>(new OrganizationMemberAddRequest()
+        {
+            OrganizationId = organization.Id,
+            UserId = userToBeAdded.Id,
+            Role = Roles.Member
+        });
+
+        // Assert
+        if (isAllowed)
+        {
+            response.Error.Should().BeOfType<InternalError>(); // Due to temp NotImplementedException
+        }
+        else
+        {
+            response.Error.Should().NotBeNull();
+            response.Error.Should().BeOfType<ForbiddenError>();
+            response.Error!.LogMessage.Should().Contain("but did not have permission to do so.");
+        }
+    }
+
+    // TODO test that you can't assign someone to global admin
+
+    // TODO test that you can't add yourself
+
+    // TODO test that you can't add a user to an organization twice
 }
