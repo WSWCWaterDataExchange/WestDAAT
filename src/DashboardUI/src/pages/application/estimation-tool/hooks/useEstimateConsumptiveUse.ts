077a87b7860382ff5a0832d69de3d7fe8a4aeae7
Ethan Weber <eweber@dontpaniclabs.com>
2025-02-12 15:55:58 -0600

    Create useEstimateConsumptiveUse.ts

---
+++ src/DashboardUI/src/pages/application/estimation-tool/hooks/useEstimateConsumptiveUse.ts
@@ -0,0 +1,41 @@
+import { useQuery } from 'react-query';
+import { estimateConsumptiveUse } from '../../../../accessors/applicationAccessor';
+import { Feature, GeoJsonProperties, Geometry } from 'geojson';
+import { CompensationRateUnits } from '../../../../data-contracts/CompensationRateUnits';
+
+export function useEstimateConsumptiveUse(fields: {
+  waterConservationApplicationId: string | undefined;
+  waterRightNativeId: string | undefined;
+  model: number | undefined;
+  dateRangeStart: Date | undefined;
+  dateRangeEnd: Date | undefined;
+  polygons: Feature<Geometry, GeoJsonProperties>[] | undefined;
+  compensationRateDollars: number | undefined;
+  units: Exclude<CompensationRateUnits, CompensationRateUnits.None> | undefined;
+}) {
+  return useQuery(
+    ['estimateConsumptiveUse'],
+    async () =>
+      estimateConsumptiveUse({
+        waterConservationApplicationId: fields.waterConservationApplicationId!,
+        waterRightNativeId: fields.waterRightNativeId!,
+        model: fields.model!,
+        dateRangeStart: fields.dateRangeStart!,
+        dateRangeEnd: fields.dateRangeEnd!,
+        polygons: fields.polygons!,
+        compensationRateDollars: fields.compensationRateDollars,
+        units: fields.units,
+      }),
+    {
+      enabled:
+        !!fields.waterConservationApplicationId &&
+        !!fields.waterRightNativeId &&
+        !!fields.model &&
+        !!fields.dateRangeStart &&
+        !!fields.dateRangeEnd &&
+        !!fields.polygons &&
+        fields.polygons.length > 0,
+      // compensation rate is optional
+    },
+  );
+}
