3ba42c457d0cb6dc71fede1c2deb79aa9fee769e
Ethan Weber <eweber@dontpaniclabs.com>
2025-02-13 16:15:40 -0600

    add selected polygons to state when drawn

--- src/DashboardUI/src/pages/application/estimation-tool/EstimationToolMap.tsx
+++ src/DashboardUI/src/pages/application/estimation-tool/EstimationToolMap.tsx
@@ -1,8 +1,25 @@
+import { Feature, GeoJsonProperties, Geometry } from 'geojson';
 import Map from '../../../components/map/Map';
+import { EstimationFormMapPolygon } from '../../../data-contracts/EstimationFormMapPolygon';
+import { convertGeometryToWkt } from '../../../utilities/geometryWktConverter';
+import { area as areaInSquareMeters } from '@turf/area';
+import { useConservationApplicationContext } from '../../../contexts/ConservationApplicationProvider';
 
 export function EstimationToolMap() {
-  const handleMapDrawnPolygonChange = (polygons: any) => {
-    alert('polygons updated');
+  const { dispatch } = useConservationApplicationContext();
+
+  const handleMapDrawnPolygonChange = (polygons: Feature<Geometry, GeoJsonProperties>[]) => {
+    const polygonData: EstimationFormMapPolygon[] = polygons.map((polygonFeature) => ({
+      polygonWkt: convertGeometryToWkt(polygonFeature.geometry),
+      acreage: convertSquareMetersToAcres(areaInSquareMeters(polygonFeature)),
+    }));
+
+    dispatch({
+      type: 'MAP_SELECTED_POLYGONS_UPDATED',
+      payload: {
+        polygons: polygonData,
+      },
+    });
   };
 
   return (
@@ -15,3 +32,7 @@ export function EstimationToolMap() {
     </div>
   );
 }
+
+const convertSquareMetersToAcres = (squareMeters: number) => {
+  return squareMeters * 0.000247105;
+};
