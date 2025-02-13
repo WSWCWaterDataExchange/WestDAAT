dcc3a81c293709ccb607b21ab6e66e504b2b7341
Ethan Weber <eweber@dontpaniclabs.com>
2025-02-13 11:40:35 -0600

    remove rate limiter; throw service unavailable exception whenever openet call fails

--- src/API/WesternStatesWater.WestDaat.Utilities/OpenEtSdk.cs
+++ src/API/WesternStatesWater.WestDaat.Utilities/OpenEtSdk.cs
@@ -3,7 +3,6 @@ using Microsoft.Net.Http.Headers;
 using System.Net.Http;
 using System.Text;
 using System.Text.Json;
-using System.Threading.RateLimiting;
 using WesternStatesWater.WestDaat.Common.Configuration;
 using WesternStatesWater.WestDaat.Common.DataContracts;
 using WesternStatesWater.WestDaat.Common.Exceptions;
@@ -13,13 +12,11 @@ namespace WesternStatesWater.WestDaat.Utilities;
 internal class OpenEtSdk : IOpenEtSdk
 {
     private readonly HttpClient _httpClient;
-    private readonly RateLimiter _rateLimiter;
     private readonly ILogger _logger;
 
-    public OpenEtSdk(HttpClient httpClient, RateLimiter rateLimiter, OpenEtConfiguration openEtConfiguration, ILogger<OpenEtSdk> logger)
+    public OpenEtSdk(HttpClient httpClient, OpenEtConfiguration openEtConfiguration, ILogger<OpenEtSdk> logger)
     {
         _httpClient = httpClient;
-        _rateLimiter = rateLimiter;
         _logger = logger;
 
         _httpClient.DefaultRequestHeaders.Add(HeaderNames.Accept, "application/json");
@@ -28,31 +25,30 @@ internal class OpenEtSdk : IOpenEtSdk
 
     public async Task<RasterTimeSeriesPolygonResponse> RasterTimeseriesPolygon(RasterTimeSeriesPolygonRequest request)
     {
-        using var lease = await _rateLimiter.AcquireAsync();
-        if (!lease.IsAcquired)
+        try
         {
-            throw new ServiceUnavailableException("Rate limit exceeded");
+            var jsonString = JsonSerializer.Serialize(request);
+            var httpContent = new StringContent(jsonString, Encoding.UTF8, "application/json");
+
+            var uri = new Uri("raster/timeseries/polygon", UriKind.Relative);
+            var response = await _httpClient.PostAsync(uri, httpContent);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new ServiceUnavailableException($"OpenET API returned status code {response.StatusCode}");
+            }
+
+            var responseContent = await response.Content.ReadAsStringAsync();
+            var responseData = JsonSerializer.Deserialize<RasterTimeSeriesPolygonResponseDatapoint[]>(responseContent);
+
+            return new RasterTimeSeriesPolygonResponse
+            {
+                Data = responseData
+            };
         }
-
-        var jsonString = JsonSerializer.Serialize(request);
-        var httpContent = new StringContent(jsonString, Encoding.UTF8, "application/json");
-
-        var uri = new Uri("raster/timeseries/polygon", UriKind.Relative);
-        var response = await _httpClient.PostAsync(uri, httpContent);
-
-        if (!response.IsSuccessStatusCode)
+        catch (Exception e)
         {
-            throw new WestDaatException($"OpenET API returned status code {response.StatusCode}");
+            throw new ServiceUnavailableException("Error occurred while calling OpenET API", e);
         }
-
-        var responseContent = await response.Content.ReadAsStringAsync();
-        var responseData = JsonSerializer.Deserialize<RasterTimeSeriesPolygonResponseDatapoint[]>(responseContent);
-
-        lease.Dispose();
-
-        return new RasterTimeSeriesPolygonResponse
-        {
-            Data = responseData
-        };
     }
 }
