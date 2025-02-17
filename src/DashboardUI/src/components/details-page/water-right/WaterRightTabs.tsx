3d6cfca2cdbc1745af6b783dee098317cc3d36d6
Jack Nolley <jnolley@dontpaniclabs.com>
2025-02-17 15:27:45 -0600

    remove time series completely from water right tab

--- src/DashboardUI/src/components/details-page/water-right/WaterRightTabs.tsx
+++ src/DashboardUI/src/components/details-page/water-right/WaterRightTabs.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import { Tab, Tabs } from 'react-bootstrap';
-import { DataGrid, GridColDef, GridRenderCellParams } from '@mui/x-data-grid';
+import { DataGrid, GridColDef } from '@mui/x-data-grid';
 import { ActiveTabType, useWaterRightDetailsContext } from './Provider';
 import OverlayDetailsTable from '../OverlayDetailsTable';
 import QuickSearchToolbar from '../../QuickSearchToolbar';
-import { FormattedDate } from '../../FormattedDate';
 
 export default function WaterRightTabs() {
   const {
@@ -14,7 +13,6 @@ export default function WaterRightTabs() {
       siteInfoListQuery: { data: siteInfoList },
       sourceInfoListQuery: { data: sourceInfoList },
       waterRightsInfoListQuery: { data: waterRightsInfoList },
-      timeSeriesInfoListQuery: { data: timeSeriesInfoList },
     },
   } = useWaterRightDetailsContext();
 
@@ -60,40 +58,6 @@ export default function WaterRightTabs() {
     { field: 'waterSourceName', headerName: 'Water Source Name', flex: 1 },
     { field: 'waterSourceType', headerName: 'Water Source Type', flex: 1 },
     { field: 'gnisfeatureNameCv', headerName: 'GNIS ID', flex: 1 },
-  ];
-
-  const timeSeriesRows = React.useMemo(() => {
-    if (!timeSeriesInfoList) return [];
-    return timeSeriesInfoList.map((ts, index) => ({
-      id: `timeSeries-${ts.waDEVariableUuid}-${index}`,
-      ...ts,
-    }));
-  }, [timeSeriesInfoList]);
-
-  const timeSeriesColumns: GridColDef[] = [
-    { field: 'waDEVariableUuid', headerName: 'WaDE Variable UUID', flex: 1, sortable: true },
-    { field: 'waDEMethodUuid', headerName: 'WaDE Method UUID', flex: 1, sortable: true },
-    { field: 'waDEWaterSourceUuid', headerName: 'WaDE Water Source UUID', flex: 1, sortable: true },
-    {
-      field: 'timeframeStart',
-      headerName: 'Timeframe Start',
-      flex: 1,
-      sortable: true,
-      renderCell: (params: GridRenderCellParams) => <FormattedDate>{params.value}</FormattedDate>,
-    },
-    {
-      field: 'timeframeEnd',
-      headerName: 'Timeframe End',
-      flex: 1,
-      sortable: true,
-      renderCell: (params: GridRenderCellParams) => <FormattedDate>{params.value}</FormattedDate>,
-    },
-    { field: 'reportYear', headerName: 'Report Year', flex: 1, sortable: true },
-    { field: 'amount', headerName: 'Amount', flex: 1, sortable: true },
-    { field: 'primaryUse', headerName: 'Primary Use', flex: 1, sortable: true },
-    { field: 'populationServed', headerName: 'Population Served', flex: 1, sortable: true },
-    { field: 'cropDutyAmount', headerName: 'Crop Duty Amount', flex: 1, sortable: true },
-    { field: 'communityWaterSupplySystem', headerName: 'Community Water Supply System', flex: 1, sortable: true },
   ];
 
   return (
