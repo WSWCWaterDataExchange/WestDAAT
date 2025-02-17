2c2194db95ee2c2d899b68bcb397134eefccc104
Jack Nolley <jnolley@dontpaniclabs.com>
2025-02-17 11:47:00 -0600

    Updated styling according to sophie

--- src/DashboardUI/src/components/home-page/water-rights-tab/map-options/components/MapTheme.tsx
+++ src/DashboardUI/src/components/home-page/water-rights-tab/map-options/components/MapTheme.tsx
@@ -4,7 +4,7 @@ import { MapThemeSelector } from '../../../../map/MapThemeSelector';
 export function MapTheme() {
   return (
     <div className="mb-3 px-4">
-      <label className="fw-bold">MAP THEME</label>
+      <label className="fw-bold">Map Theme</label>
       <div className="mt-2">
         <MapThemeSelector />
       </div>
