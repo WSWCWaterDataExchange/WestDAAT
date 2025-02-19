c55d2937649d38414446765276a641063034bca0
Branden Barber <bbarber@dontpaniclabs.com>
2025-02-19 13:31:17 -0600

    Dto + validator + handler

---
+++ src/API/WesternStatesWater.WestDaat.Managers/Handlers/Admin/UserListRequestHandler.cs
@@ -0,0 +1,38 @@
+using WesternStatesWater.Shared.Resolver;
+using WesternStatesWater.WestDaat.Accessors;
+using WesternStatesWater.WestDaat.Common;
+using WesternStatesWater.WestDaat.Contracts.Client.Requests.Admin;
+using WesternStatesWater.WestDaat.Contracts.Client.Responses.Admin;
+
+namespace WesternStatesWater.WestDaat.Managers.Handlers.Admin;
+
+public class UserListRequestHandler : IRequestHandler<UserListRequest, UserListResponse>
+{
+    public IUserAccessor UserAccessor { get; }
+
+    public UserListRequestHandler(IUserAccessor userAccessor)
+    {
+        UserAccessor = userAccessor;
+    }
+
+    public async Task<UserListResponse> Handle(UserListRequest request)
+    {
+        await Task.CompletedTask;
+
+        return new UserListResponse
+        {
+            Users =
+            [
+                new UserListResult
+                {
+                    Email = "test@test.com",
+                    Role = Roles.Member,
+                    FirstName = "Tester",
+                    LastName = "McTesterson",
+                    UserId = Guid.NewGuid(),
+                    UserName = "tmctester"
+                }
+            ]
+        };
+    }
+}
