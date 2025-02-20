314d3b5ed455acf87ffceea2d01d896d16e955bb
Ethan Weber <eweber@dontpaniclabs.com>
2025-02-20 11:53:19 -0600

    Request Handler

---
+++ src/API/WesternStatesWater.WestDaat.Managers/Handlers/Admin/OrganizationFundingDetailsRequestHandler.cs
@@ -0,0 +1,26 @@
+﻿using WesternStatesWater.Shared.Resolver;
+using WesternStatesWater.WestDaat.Accessors;
+using WesternStatesWater.WestDaat.Contracts.Client.Requests.Admin;
+using WesternStatesWater.WestDaat.Contracts.Client.Responses.Admin;
+using WesternStatesWater.WestDaat.Managers.Mapping;
+
+using CommonContracts = WesternStatesWater.WestDaat.Common.DataContracts;
+
+namespace WesternStatesWater.WestDaat.Managers.Handlers.Admin;
+
+public class OrganizationFundingDetailsRequestHandler : IRequestHandler<OrganizationFundingDetailsRequest, OrganizationFundingDetailsResponse>
+{
+    public IOrganizationAccessor OrganizationAccessor { get; }
+
+    public OrganizationFundingDetailsRequestHandler(IOrganizationAccessor organizationAccessor)
+    {
+        OrganizationAccessor = organizationAccessor;
+    }
+
+    public async Task<OrganizationFundingDetailsResponse> Handle(OrganizationFundingDetailsRequest request)
+    {
+        var dtoRequest = request.Map<CommonContracts.OrganizationFundingDetailsRequest>();
+        var dtoResponse = (CommonContracts.OrganizationFundingDetailsResponse)await OrganizationAccessor.Load(dtoRequest);
+        return dtoResponse.Map<OrganizationFundingDetailsResponse>();
+    }
+}
