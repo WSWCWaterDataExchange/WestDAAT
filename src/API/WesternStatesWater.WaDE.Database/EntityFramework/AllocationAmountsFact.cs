d6aadaaf06c27ed4c8154dbd986facc62d342ec4
Ethan Weber <eweber@dontpaniclabs.com>
2025-02-20 15:58:17 -0600

    Revert "update predicate builder to take AllocationNativeId into account"
    This reverts commit 5a36ad19e3f7ecb8ac7e4d1c54a48eea5f4cea7f.

--- src/API/WesternStatesWater.WaDE.Database/EntityFramework/AllocationAmountsFact.cs
+++ src/API/WesternStatesWater.WaDE.Database/EntityFramework/AllocationAmountsFact.cs
@@ -236,15 +236,6 @@ namespace WesternStatesWater.WaDE.Database.EntityFramework
             return predicate;
         }
 
-        public static ExpressionStarter<AllocationAmountsFact> IsAllocationNativeId(string allocationNativeId)
-        {
-            var predicate = PredicateBuilder.New<AllocationAmountsFact>();
-
-            predicate = predicate.Or(x => x.AllocationNativeId == allocationNativeId);
-
-            return predicate;
-        }
-
         #endregion
     }
 }
