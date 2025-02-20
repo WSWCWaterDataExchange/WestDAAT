fb634af075057a1ec16bf0aff95caef855729009
Ethan Weber <eweber@dontpaniclabs.com>
2025-02-20 17:05:20 -0600

    Add all supported OpenET models to backend

--- src/API/WesternStatesWater.WestDaat.Common/DataContracts/RasterTimeseriesModel.cs
+++ src/API/WesternStatesWater.WestDaat.Common/DataContracts/RasterTimeseriesModel.cs
@@ -3,5 +3,12 @@
 public enum RasterTimeSeriesModel
 {
     None = 0,
-    SSEBop = 1
+    SSEBop = 1,
+    DisALEXI = 2,
+#pragma warning disable SA1300 // Element should begin with upper-case letter
+    eeMETRIC = 3,
+    geeSEBAL = 4,
+#pragma warning restore SA1300 // Element should begin with upper-case letter
+    Ensemble = 5,
+    PTJPL = 6
 }
