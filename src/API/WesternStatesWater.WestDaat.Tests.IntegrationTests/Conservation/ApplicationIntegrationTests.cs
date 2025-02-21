e4bd826626a3098484e9bb44ac1f829671351984
Ethan Weber <eweber@dontpaniclabs.com>
2025-02-21 14:48:40 -0600

    remove FundingOrganizationId field from request contract

--- src/API/WesternStatesWater.WestDaat.Tests.IntegrationTests/Conservation/ApplicationIntegrationTests.cs
+++ src/API/WesternStatesWater.WestDaat.Tests.IntegrationTests/Conservation/ApplicationIntegrationTests.cs
@@ -337,7 +337,7 @@ public class ApplicationIntegrationTests : IntegrationTestBase
             var requestedCompensationPerAcreFoot = 1000;
             var request = new EstimateConsumptiveUseRequest
             {
-                FundingOrganizationId = organization.Id,
+                FundingOrganizationId = Guid.Empty, // not trusting the frontend to provide this field
                 WaterConservationApplicationId = application.Id,
                 WaterRightNativeId = application.WaterRightNativeId,
                 Polygons = [memorialStadiumFootballField],
