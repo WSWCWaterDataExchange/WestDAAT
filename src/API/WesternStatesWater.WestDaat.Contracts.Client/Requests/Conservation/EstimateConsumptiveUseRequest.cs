175a2c0aadfb3995f6053cec22b8c3baff5d1371
Ethan Weber <eweber@dontpaniclabs.com>
2025-02-21 13:05:34 -0600

    update ECU request contract

--- src/API/WesternStatesWater.WestDaat.Contracts.Client/Requests/Conservation/EstimateConsumptiveUseRequest.cs
+++ src/API/WesternStatesWater.WestDaat.Contracts.Client/Requests/Conservation/EstimateConsumptiveUseRequest.cs
@@ -15,18 +15,6 @@ public class EstimateConsumptiveUseRequest : ApplicationStoreRequestBase
     /// </summary>
     public string[] Polygons { get; set; }
 
-    public RasterTimeSeriesModel Model { get; set; }
-
-    /// <summary>
-    /// The beginning year of the date range. Inclusive.
-    /// </summary>
-    public DateOnly DateRangeStart { get; set; }
-
-    /// <summary>
-    /// The ending year of the date range. Inclusive.
-    /// </summary>
-    public DateOnly DateRangeEnd { get; set; }
-
     public int? CompensationRateDollars { get; set; }
 
     public CompensationRateUnits? Units { get; set; }
--- src/API/WesternStatesWater.WestDaat.Contracts.Client/Requests/Conservation/EstimateConsumptiveUseRequestValidator.cs
+++ src/API/WesternStatesWater.WestDaat.Contracts.Client/Requests/Conservation/EstimateConsumptiveUseRequestValidator.cs
@@ -18,11 +18,6 @@ public class EstimateConsumptiveUseRequestValidator : AbstractValidator<Estimate
             polygonEntryValidator.RuleFor(polygon => polygon).NotEmpty();
         });
 
-        RuleFor(x => x.Model).NotEmpty();
-
-        RuleFor(x => x.DateRangeStart).NotEmpty().LessThanOrEqualTo(x => x.DateRangeEnd);
-        RuleFor(x => x.DateRangeEnd).NotEmpty().GreaterThanOrEqualTo(x => x.DateRangeStart);
-
         // if one property is non-null, then they both must be non-null
         RuleFor(x => x.CompensationRateDollars).NotEmpty().When(x => x.Units.HasValue);
         RuleFor(x => x.Units).NotEmpty().When(x => x.CompensationRateDollars.HasValue);
