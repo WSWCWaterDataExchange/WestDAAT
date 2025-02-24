23d2f1c93c6724af9aca93645c8b2c83b591491b
Branden Barber <bbarber@dontpaniclabs.com>
2025-02-24 16:03:57 -0600

    Fake it

--- src/API/WesternStatesWater.WestDaat.Tests.Helpers/UserProfileFaker.cs
+++ src/API/WesternStatesWater.WestDaat.Tests.Helpers/UserProfileFaker.cs
@@ -7,5 +7,8 @@ public class UserProfileFaker : Faker<EFWD.UserProfile>
         RuleFor(up => up.FirstName, f => f.Person.FirstName);
         RuleFor(up => up.LastName, f => f.Person.LastName);
         RuleFor(up => up.UserName, f => f.Person.UserName);
+        RuleFor(up => up.State, f => f.Address.StateAbbr());
+        RuleFor(up => up.County, f => f.Address.County());
+        RuleFor(up => up.PhoneNumber, f => f.Person.Phone);
     }
 }
