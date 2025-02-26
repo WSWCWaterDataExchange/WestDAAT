e5189e7458b5357edb5ccf106b10983ae5d97ec2
Branden Barber <bbarber@dontpaniclabs.com>
2025-02-26 12:34:25 -0600

    Limit to 50

--- src/API/WesternStatesWater.WestDaat.Tests.Helpers/UserProfileFaker.cs
+++ src/API/WesternStatesWater.WestDaat.Tests.Helpers/UserProfileFaker.cs
@@ -8,7 +8,7 @@ public class UserProfileFaker : Faker<EFWD.UserProfile>
         RuleFor(up => up.LastName, f => f.Person.LastName);
         RuleFor(up => up.UserName, f => f.Person.UserName);
         RuleFor(up => up.State, f => f.Address.StateAbbr());
-        RuleFor(up => up.Country, f => f.Address.Country());
+        RuleFor(up => up.Country, f => f.Address.Country().PadRight(50)[..50]); // Avoid sql column truncation
         RuleFor(up => up.PhoneNumber, f => f.Person.Phone);
     }
 }
