819f1c028845e165f0930cfd6b20d4102da03773
Ethan Weber <eweber@dontpaniclabs.com>
2025-02-26 10:22:28 -0600

    [wip] trying to parse wkt

--- src/DashboardUI/src/utilities/geometryWktConverter.ts
+++ src/DashboardUI/src/utilities/geometryWktConverter.ts
@@ -69,3 +69,222 @@ export const convertGeometryToWkt = (geometry: Geometry): string => {
     }
   }
 };
+
+const numberRegexp = /[-+]?([0-9]*\.[0-9]+|[0-9]+)([eE][-+]?[0-9]+)?/;
+// Matches sequences like '100 100' or '100 100 100'.
+const tuples = new RegExp('^' + numberRegexp.source + '(\\s' + numberRegexp.source + '){1,}');
+
+export const convertWktToGeometry = (input: string): Geometry => {
+  // example input: `SRID=4326;POINT(-44.3 60.1)`
+  const parts = input.split(';');
+  let geometryWkt = parts.pop()!;
+  const srid = (parts.shift() || '').split('=').pop();
+
+  let i = 0;
+
+  function $(re) {
+    const match = geometryWkt!.substring(i).match(re);
+    if (!match) return null;
+    else {
+      i += match[0].length;
+      return match[0];
+    }
+  }
+
+  function crs(obj) {
+    if (obj && srid!.match(/\d+/)) {
+      obj.crs = {
+        type: 'name',
+        properties: {
+          name: 'urn:ogc:def:crs:EPSG::' + srid,
+        },
+      };
+    }
+
+    return obj;
+  }
+
+  function white() {
+    $(/^\s*/);
+  }
+
+  function multicoords() {
+    white();
+    let depth = 0;
+    const rings: number[] = [];
+    const stack = [rings];
+    let pointer = rings;
+    let elem;
+
+    while ((elem = $(/^(\()/) || $(/^(\))/) || $(/^(,)/) || $(tuples))) {
+      if (elem === '(') {
+        stack.push(pointer);
+        pointer = [];
+        stack[stack.length - 1].push(...pointer); // verify correct
+        depth++;
+      } else if (elem === ')') {
+        // For the case: Polygon(), ...
+        if (pointer.length === 0) return null;
+
+        pointer = stack.pop()!;
+        // the stack was empty, input was malformed
+        if (!pointer) return null;
+        depth--;
+        if (depth === 0) break;
+      } else if (elem === ',') {
+        pointer = [];
+        stack[stack.length - 1].push(...pointer);
+      } else if (!elem.split(/\s/g).some((x) => isNaN(Number(x)))) {
+        pointer.push(...elem.split(/\s/g).map(parseFloat));
+      } else {
+        return null;
+      }
+      white();
+    }
+
+    if (depth !== 0) return null;
+
+    return rings;
+  }
+
+  function coords(): number[] | null {
+    const list: number[] = [];
+    let item: number[] = [];
+    let pt;
+    while ((pt = $(tuples) || $(/^(,)/))) {
+      if (pt === ',') {
+        list.push(...item);
+        item = [];
+      } else if (!pt.split(/\s/g).some((x) => isNaN(Number(x)))) {
+        if (!item) {
+          item = [];
+        }
+
+        item.push(...pt.split(/\s/g).map(parseFloat));
+      }
+      white();
+    }
+
+    if (item) {
+      list.push(...item);
+    } else {
+      return null;
+    }
+
+    return list.length ? list : null;
+  }
+
+  function point(): Point | null {
+    if (!$(/^(point(\sz)?)/i)) return null;
+    white();
+    if (!$(/^(\()/)) return null;
+    const c = coords();
+    if (!c) return null;
+    white();
+    if (!$(/^(\))/)) return null;
+    return {
+      type: 'Point',
+      coordinates: c[0],
+    };
+  }
+
+  function multipoint(): MultiPoint | null {
+    if (!$(/^(multipoint)/i)) return null;
+    white();
+    const newCoordsFormat = geometryWkt
+      .substring(geometryWkt.indexOf('(') + 1, geometryWkt.length - 1)
+      .replace(/\(/g, '')
+      .replace(/\)/g, '');
+    geometryWkt = 'MULTIPOINT (' + newCoordsFormat + ')';
+    const c = multicoords();
+    if (!c) return null;
+    white();
+    return {
+      type: 'MultiPoint',
+      coordinates: c,
+    };
+  }
+
+  function multilinestring(): MultiLineString | null {
+    if (!$(/^(multilinestring)/i)) return null;
+    white();
+    const c = multicoords();
+    if (!c) return null;
+    white();
+    return {
+      type: 'MultiLineString',
+      coordinates: c,
+    };
+  }
+
+  function linestring(): LineString | null {
+    if (!$(/^(linestring(\sz)?)/i)) return null;
+    white();
+    if (!$(/^(\()/)) return null;
+    const c = coords();
+    if (!c) return null;
+    if (!$(/^(\))/)) return null;
+    return {
+      type: 'LineString',
+      coordinates: c,
+    };
+  }
+
+  function polygon(): Polygon | null {
+    if (!$(/^(polygon(\sz)?)/i)) return null;
+    white();
+    const c = multicoords();
+    if (!c) return null;
+    return {
+      type: 'Polygon',
+      coordinates: c,
+    };
+  }
+
+  function multipolygon(): MultiPolygon | null {
+    if (!$(/^(multipolygon)/i)) return null;
+    white();
+    const c = multicoords();
+    if (!c) return null;
+    return {
+      type: 'MultiPolygon',
+      coordinates: c,
+    };
+  }
+
+  function geometrycollection(): GeometryCollection | null {
+    const geometries = [];
+    let geometry;
+
+    if (!$(/^(geometrycollection)/i)) return null;
+    white();
+
+    if (!$(/^(\()/)) return null;
+    while ((geometry = root())) {
+      geometries.push(geometry);
+      white();
+      $(/^(,)/);
+      white();
+    }
+    if (!$(/^(\))/)) return null;
+
+    return {
+      type: 'GeometryCollection',
+      geometries: geometries,
+    };
+  }
+
+  function root(): Geometry | null {
+    return (
+      point() ||
+      linestring() ||
+      polygon() ||
+      multipoint() ||
+      multilinestring() ||
+      multipolygon() ||
+      geometrycollection()
+    );
+  }
+
+  return crs(root());
+};
