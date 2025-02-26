24cdd264d8d624d5998d0bf5f6060b75239b2d77
Branden Barber <bbarber@dontpaniclabs.com>
2025-02-26 10:55:22 -0600

    Error state

--- src/DashboardUI/src/pages/account/AccountInformationPage.tsx
+++ src/DashboardUI/src/pages/account/AccountInformationPage.tsx
@@ -1,19 +1,40 @@
+import Alert from 'react-bootstrap/esm/Alert';
 import { NotImplementedPlaceholder } from '../../components/NotImplementedAlert';
 import { useUserProfile } from '../../hooks/queries/useUserQuery';
+import Placeholder from 'react-bootstrap/esm/Placeholder';
+import Icon from '@mdi/react';
+import { mdiInformationOutline } from '@mdi/js';
 
 export function AccountInformationPage() {
-  const { data: profileResponse } = useUserProfile();
+  const {
+    data: profileResponse, //
+    isLoading: isProfileLoading,
+    isError: hasProfileLoadError,
+  } = useUserProfile();
 
   const profile = profileResponse?.userProfile;
 
-  const labeledValue = (label: string, value: string) => (
-    <div className="col-xxl-2 col-lg-3 col-md-4">
-      <div className="mb-4">
-        <div className="text-muted">{label}</div>
-        <div>{value}</div>
+  const labeledValue = (label: string, value: string | undefined) => {
+    return (
+      <div className="col-xxl-2 col-lg-3 col-md-4">
+        <div className="mb-4">
+          {isProfileLoading && (
+            <Placeholder animation="glow">
+              <Placeholder xs={6} className="rounded" />
+              <Placeholder xs={10} className="rounded" />
+            </Placeholder>
+          )}
+
+          {!isProfileLoading && (
+            <>
+              <div className="text-muted">{label}</div>
+              <div>{value}</div>
+            </>
+          )}
+        </div>
       </div>
-    </div>
-  );
+    );
+  };
 
   return (
     <div className="container-fluid mt-3">
@@ -21,7 +42,14 @@ export function AccountInformationPage() {
       <h2 className="fs-4">Account Information</h2>
       <hr />
 
-      {profile && (
+      {hasProfileLoadError && (
+        <Alert variant="danger" className="d-flex align-items-center">
+          <Icon path={mdiInformationOutline} size={1.3} />
+          <span className="ms-2 fw-bold">There was an error loading your profile information. Please try again.</span>
+        </Alert>
+      )}
+
+      {!hasProfileLoadError && (
         <div className="row">
           {labeledValue('Name', profile?.firstName + ' ' + profile?.lastName)}
           {labeledValue('Email', profile?.email)}
