ca59020ed982356d20108df7385e64cbe104bb2c
Ethan Weber <eweber@dontpaniclabs.com>
2025-02-27 13:27:24 -0600

    consolidate contracts into existing Submission contract

--- src/API/WesternStatesWater.WestDaat.Database/EntityFramework/WaterConservationApplicationSubmission.cs
+++ src/API/WesternStatesWater.WestDaat.Database/EntityFramework/WaterConservationApplicationSubmission.cs
@@ -17,7 +17,68 @@ public class WaterConservationApplicationSubmission
 
     public DateTimeOffset? RejectedDate { get; set; }
 
+    public string AgentFirstName { get; set; } = null!;
+
+    public string AgentLastName { get; set; } = null!;
+
+    public string AgentEmail { get; set; } = null!;
+
+    public string AgentPhoneNumber { get; set; } = null!;
+
+    public string LandownerFirstName { get; set; } = null!;
+
+    public string LandownerLastName { get; set; } = null!;
+
+    public string LandownerEmail { get; set; } = null!;
+
+    public string LandownerPhoneNumber { get; set; } = null!;
+
+    public string LandownerAddress { get; set; } = null!;
+
+    public string LandownerCity { get; set; } = null!;
+
+    public string LandownerState { get; set; } = null!;
+
+    public string LandownerZipCode { get; set; } = null!;
+
+    public string CanalOrIrrigationEntityName { get; set; } = null!;
+
+    public string CanalOrIrrigationEntityEmail { get; set; } = null!;
+
+    public string CanalOrIrrigationEntityPhoneNumber { get; set; } = null!;
+
+    public int ConservationPlanFundingRequestDollarAmount { get; set; }
+
+    public string ConservationPlanDescription { get; set; } = null!;
+
+    public string ConservationPlanAdditionalInfo { get; set; } = null!;
+
+    public string EstimationSupplementaryDetails { get; set; } = null!;
+
+    public string ProjectLocation { get; set; } = null!;
+
+    public string PropertyAdditionalDetails { get; set; } = null!;
+
+    public string DiversionPoint { get; set; } = null!;
+
+    public string DiversionPointDetails { get; set; } = null!;
+
+    public string PermitNumber { get; set; } = null!;
+
+    public string FacilityDitchName { get; set; } = null!;
+
+    public DateOnly PriorityDate { get; set; }
+
+    public string CertificateNumber { get; set; } = null!;
+
+    // question: in the mockups this displays "share #'s"; will there be multiple share numbers? if so we may need another contract
+    public string ShareNumber { get; set; } = null!;
+
     public string WaterRightState { get; set; } = null!;
+
+    public string WaterUseDescription { get; set; } = null!;
+
+
 
     public virtual WaterConservationApplication WaterConservationApplication { get; set; } = null!;
 }
--- src/API/WesternStatesWater.WestDaat.Database/EntityFramework/WaterConservationApplicationSubmissionEstimationInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace WesternStatesWater.WestDaat.Database.EntityFramework;
-
-public class WaterConservationApplicationSubmissionEstimationInfo
-{
-    public Guid WaterConservationApplicationSubmissionId { get; set; }
-
-    public string EstimationSupplementaryDetails { get; set; } = null!;
-}
