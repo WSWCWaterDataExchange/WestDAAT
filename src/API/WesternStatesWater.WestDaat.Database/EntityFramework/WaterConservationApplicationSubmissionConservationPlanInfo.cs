12e3d8c67a4ccd5dcebfb680c14b855491d00c4b
Ethan Weber <eweber@dontpaniclabs.com>
2025-02-27 12:07:19 -0600

    remove question comment

--- src/API/WesternStatesWater.WestDaat.Database/EntityFramework/WaterConservationApplicationSubmissionConservationPlanInfo.cs
+++ src/API/WesternStatesWater.WestDaat.Database/EntityFramework/WaterConservationApplicationSubmissionConservationPlanInfo.cs
@@ -4,8 +4,6 @@ public class WaterConservationApplicationSubmissionConservationPlanInfo
 {
     public Guid WaterConservationApplicationSubmissionId { get; set; }
 
-    // question: will the user re-enter a dollar amount, or should we ignore adding this field
-    // and instead use the value WaterConservationApplicationEstimate.EstimatedCompensationDollars?
     public int FundingRequestDollarAmount { get; set; }
 
     public string ConservationPlanDescription { get; set; } = null!;
