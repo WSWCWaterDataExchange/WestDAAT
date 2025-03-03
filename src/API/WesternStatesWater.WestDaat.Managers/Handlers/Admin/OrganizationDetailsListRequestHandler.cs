c8e6e608461f98eb985e8a731572ce52c6e6aa53
Branden Barber <bbarber@dontpaniclabs.com>
2025-03-03 10:46:13 -0600

    Set current user in handler. Add validatino engine check

--- src/API/WesternStatesWater.WestDaat.Managers/Handlers/Admin/OrganizationDetailsListRequestHandler.cs
+++ src/API/WesternStatesWater.WestDaat.Managers/Handlers/Admin/OrganizationDetailsListRequestHandler.cs
@@ -1,8 +1,10 @@
 using WesternStatesWater.Shared.Resolver;
 using WesternStatesWater.WestDaat.Accessors;
+using WesternStatesWater.WestDaat.Common.Context;
 using WesternStatesWater.WestDaat.Contracts.Client.Requests.Admin;
 using WesternStatesWater.WestDaat.Contracts.Client.Responses.Admin;
 using WesternStatesWater.WestDaat.Managers.Mapping;
+using WesternStatesWater.WestDaat.Utilities;
 using CommonContracts = WesternStatesWater.WestDaat.Common.DataContracts;
 
 namespace WesternStatesWater.WestDaat.Managers.Handlers.Admin;
