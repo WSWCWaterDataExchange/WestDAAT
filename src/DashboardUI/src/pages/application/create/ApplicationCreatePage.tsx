ab395610fa5d5878a8e93b1856af8e20352a4d48
Ethan Weber <eweber@dontpaniclabs.com>
2025-03-03 09:39:34 -0600

    add header to Create page

--- src/DashboardUI/src/pages/application/create/ApplicationCreatePage.tsx
+++ src/DashboardUI/src/pages/application/create/ApplicationCreatePage.tsx
@@ -1,10 +1,26 @@
+import { useNavigate } from 'react-router-dom';
 import { NotImplementedPlaceholder } from '../../../components/NotImplementedAlert';
+import MapProvider from '../../../contexts/MapProvider';
+import { ApplicationNavbar } from '../components/ApplicationNavbar';
+import { useConservationApplicationContext } from '../../../contexts/ConservationApplicationProvider';
 
 export function ApplicationCreatePage() {
+  const { state } = useConservationApplicationContext();
+  const navigate = useNavigate();
+
+  const navigateToEstimationToolPage = () => {
+    navigate(`/application/${state.conservationApplication.waterRightNativeId}/estimation`);
+  };
+
   return (
-    <div>
-      <h1>Application Create</h1>
-      <NotImplementedPlaceholder />
-    </div>
+    <MapProvider>
+      <div className="application-create-page d-flex flex-column flex-grow-1 h-100">
+        <ApplicationNavbar
+          navigateBack={navigateToEstimationToolPage}
+          backButtonText="Back to Estimator"
+          centerText="Water Conservation Estimation Tool"
+        />
+      </div>
+    </MapProvider>
   );
 }
