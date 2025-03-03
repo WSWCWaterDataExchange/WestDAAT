87437887a86068669b166183bc024c3cef1f1535
Ethan Weber <eweber@dontpaniclabs.com>
2025-03-03 10:12:37 -0600

    stub in page sections

--- src/DashboardUI/src/pages/application/create/ApplicationCreatePage.tsx
+++ src/DashboardUI/src/pages/application/create/ApplicationCreatePage.tsx
@@ -1,8 +1,8 @@
 import { useNavigate } from 'react-router-dom';
-import { NotImplementedPlaceholder } from '../../../components/NotImplementedAlert';
 import MapProvider from '../../../contexts/MapProvider';
 import { ApplicationNavbar } from '../components/ApplicationNavbar';
 import { useConservationApplicationContext } from '../../../contexts/ConservationApplicationProvider';
+import Form from 'react-bootstrap/esm/Form';
 
 export function ApplicationCreatePage() {
   const { state } = useConservationApplicationContext();
@@ -20,7 +20,85 @@ export function ApplicationCreatePage() {
           backButtonText="Back to Estimator"
           centerText="Water Conservation Estimation Tool"
         />
+
+        <ApplicationCreatePageForm />
       </div>
     </MapProvider>
+  );
+}
+
+function ApplicationCreatePageForm() {
+  return (
+    <div className="flex-grow-1 overflow-y-auto p-4">
+      <div>
+        <span>New Application</span>
+      </div>
+
+      <div className="d-flex gap-3">
+        <span>Water Right Native ID:</span>
+
+        <span>Application ID: </span>
+
+        <span>Funding Organization :</span>
+      </div>
+
+      <div>
+        <span>
+          Complete the below fields in order to submit your application to your state agency for verification. Be sure
+          everything is filled out accurately and truthfully.
+        </span>
+      </div>
+
+      <Form>
+        <FormSection title="Applicant Information">
+          <Form.Group>todo</Form.Group>
+        </FormSection>
+
+        <FormSection title="Agent Information">
+          <Form.Group></Form.Group>
+        </FormSection>
+
+        <FormSection title="Property & Land Area Information">
+          <Form.Group></Form.Group>
+        </FormSection>
+
+        <FormSection title="Canal Company / Irrigation District">
+          <Form.Group></Form.Group>
+        </FormSection>
+
+        <FormSection title="Water Right Information">
+          <Form.Group></Form.Group>
+        </FormSection>
+
+        <FormSection title="Estimation Summary">
+          <Form.Group></Form.Group>
+        </FormSection>
+
+        <FormSection title="Conservation Plan">
+          <Form.Group></Form.Group>
+        </FormSection>
+
+        <FormSection title="Supporting Documents (Optional)">
+          <Form.Group></Form.Group>
+        </FormSection>
+      </Form>
+    </div>
+  );
+}
+
+interface FormSectionProps {
+  title: string;
+  children: React.ReactNode | undefined;
+}
+
+function FormSection(props: FormSectionProps) {
+  return (
+    <div>
+      <div>
+        <span>{props.title}</span>
+      </div>
+
+      <div>{props.children}</div>
+    </div>
   );
 }
