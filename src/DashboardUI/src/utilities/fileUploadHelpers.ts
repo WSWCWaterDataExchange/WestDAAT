12c1bc97ea04aed78225bc3043797173b80848dc
Jenny Nielsen <jnielsen@dontpaniclabs.com>
2025-03-10 16:26:11 -0500

    add azure blob file upload helper

---
+++ src/DashboardUI/src/utilities/fileUploadHelpers.ts
@@ -0,0 +1,49 @@
+import { BlobServiceClient, BlockBlobUploadResponse } from '@azure/storage-blob';
+import { BlobUpload } from '../data-contracts/BlobUpload';
+
+export enum ContainerName {
+  ApplicationDocuments = 'application-documents',
+}
+
+export const uploadFilesToBlobStorage = async (
+  containerName: ContainerName,
+  uploadDetails: BlobUpload[]
+): Promise<BlockBlobUploadResponse[]> => {
+  let uploadPromises: Promise<BlockBlobUploadResponse>[] = [];
+
+  uploadDetails.forEach(async (upload) => {
+    const sasQueryParam = upload.sasToken.split('?')[1];
+    const blobServiceClient = new BlobServiceClient(`${upload.hostname}?${sasQueryParam}`);
+    const containerClient = blobServiceClient.getContainerClient(containerName);
+    const blockBlobClient = containerClient.getBlockBlobClient(upload.blobname);
+    uploadPromises.push(blockBlobClient.upload(upload.data, upload.contentLength));
+  });
+
+  return await Promise.all(uploadPromises);
+};
+
+/**
+ * TODO: JN
+ * upload accessor
+ * 
+ * 1. go get the sas tokens from the API
+ * 2. use the sas tokens to upload the files to the blob storage
+ * 
+ * - this will be called from a useMutation from the component
+ * - need to create this accessor
+ */
+
+/**
+ * Upload button onClick calls this
+ *   const selectFile = () => {
+    const fileInput = document.createElement('input');
+    fileInput.type = 'file';
+    fileInput.onchange = (e) => {
+      const files = (e.target as HTMLInputElement).files;
+      if (files && files.length > 0) {
+        setSelectedFile(files[0]);
+      }
+    };
+    fileInput.click();
+  };
+ */
