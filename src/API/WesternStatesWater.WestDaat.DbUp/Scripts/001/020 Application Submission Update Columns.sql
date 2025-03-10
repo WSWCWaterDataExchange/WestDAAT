208e2025738547d31ac6c5cc5a6fe75f9a1542c8
Ethan Weber <eweber@dontpaniclabs.com>
2025-03-10 13:46:09 -0500

    column is required

--- src/API/WesternStatesWater.WestDaat.DbUp/Scripts/001/020 Application Submission Update Columns.sql
+++ src/API/WesternStatesWater.WestDaat.DbUp/Scripts/001/020 Application Submission Update Columns.sql
@@ -18,7 +18,7 @@ go;
 
 -- modify existing columns
 ALTER TABLE dbo.WaterConservationApplicationSubmissions ALTER COLUMN AgentFirstName NVARCHAR(255) NULL;
-ALTER TABLE dbo.WaterConservationApplicationSubmissions ALTER COLUMN LandownerFirstName NVARCHAR(255) NULL;
+ALTER TABLE dbo.WaterConservationApplicationSubmissions ALTER COLUMN LandownerFirstName NVARCHAR(255) NOT NULL;
 
 EXEC sp_rename 'dbo.WaterConservationApplicationSubmissions.AgentFirstName', 'AgentName', 'COLUMN';
 EXEC sp_rename 'dbo.WaterConservationApplicationSubmissions.LandownerFirstName', 'LandownerName', 'COLUMN';
