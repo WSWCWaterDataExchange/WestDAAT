78bc4562dde0fabf7b022307578e5f74d8b82f32
Ethan Weber <eweber@dontpaniclabs.com>
2025-03-10 10:20:10 -0500

    Create 020 Application Submission Update Columns.sql

---
+++ src/API/WesternStatesWater.WestDaat.DbUp/Scripts/001/020 Application Submission Update Columns.sql
@@ -0,0 +1,24 @@
+-- remove columns
+ALTER TABLE dbo.WaterConservationApplicationSubmissions DROP COLUMN 
+	AgentLastName,
+	LandownerLastName,
+	ProjectLocation,
+	PropertyAdditionalDetails,
+	DiversionPoint,
+	DiversionPointDetails;
+
+go;
+
+-- add new columns
+ALTER TABLE dbo.WaterConservationApplicationSubmissions ADD
+	AgentAdditionalDetails NVARCHAR(4000) NULL,
+	CanalOrIrrigationAdditionalDetails NVARCHAR(4000) NULL;
+
+go;
+
+-- modify existing columns
+ALTER TABLE dbo.WaterConservationApplicationSubmissions ALTER COLUMN AgentFirstName NVARCHAR(255) NULL;
+ALTER TABLE dbo.WaterConservationApplicationSubmissions ALTER COLUMN LandownerFirstName NVARCHAR(255) NULL;
+
+EXEC sp_rename 'dbo.WaterConservationApplicationSubmissions.AgentFirstName', 'AgentName', 'COLUMN';
+EXEC sp_rename 'dbo.WaterConservationApplicationSubmissions.LandownerFirstName', 'LandownerName', 'COLUMN';
