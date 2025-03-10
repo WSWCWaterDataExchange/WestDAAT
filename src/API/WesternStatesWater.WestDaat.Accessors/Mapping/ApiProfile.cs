19344b84575b4ddfcefee9df95e5efe12d7da548
Ethan Weber <eweber@dontpaniclabs.com>
2025-03-10 15:00:26 -0500

    remove redundant mapper logic

--- src/API/WesternStatesWater.WestDaat.Accessors/Mapping/ApiProfile.cs
+++ src/API/WesternStatesWater.WestDaat.Accessors/Mapping/ApiProfile.cs
@@ -347,8 +347,7 @@ namespace WesternStatesWater.WestDaat.Accessors.Mapping
                 .ForMember(dest => dest.SubmittedDate, opt => opt.MapFrom(_ => DateTimeOffset.UtcNow));
 
             CreateMap<ApplicationSubmissionFieldDetail, EFWD.WaterConservationApplicationEstimateLocation>(MemberList.Source)
-                .ForSourceMember(src => src.PolygonWkt, opt => opt.DoNotValidate())
-                .ForMember(dest => dest.AdditionalDetails, opt => opt.MapFrom(src => src.AdditionalDetails));
+                .ForSourceMember(src => src.PolygonWkt, opt => opt.DoNotValidate());
         }
     }
 }
