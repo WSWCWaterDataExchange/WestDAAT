b1822184385e268ba794fc5d00ee5cc590943c6a
Ethan Weber <eweber@dontpaniclabs.com>
2025-03-13 11:33:15 -0500

    submitted application dto contracts

---
+++ src/API/WesternStatesWater.WestDaat.Common/DataContracts/SubmittedApplicationExistsLoadResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace WesternStatesWater.WestDaat.Common.DataContracts;
+
+public class SubmittedApplicationExistsLoadResponse : ApplicationLoadResponseBase
+{
+    public bool ApplicationExists { get; set; }
+
+    // these will only be populated if the application exists
+    public Guid? ApplicantUserId { get; set; } = null!;
+
+    public Guid? FundingOrganizationId { get; set; } = null!;
+}
