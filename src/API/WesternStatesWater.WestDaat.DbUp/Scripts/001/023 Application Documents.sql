a56050e8f63ea463cb33b5d90302f610afcf7ba3
Jenny Nielsen <jnielsen@dontpaniclabs.com>
2025-03-13 08:19:09 -0500

    update constraints

--- src/API/WesternStatesWater.WestDaat.DbUp/Scripts/001/023 Application Documents.sql
+++ src/API/WesternStatesWater.WestDaat.DbUp/Scripts/001/023 Application Documents.sql
@@ -1,9 +1,8 @@
 CREATE TABLE ApplicationDocuments
 (
-    Id                              UNIQUEIDENTIFIER NOT NULL CONSTRAINT PK_ApplicationDocuments_Id PRIMARY KEY NONCLUSTERED,
-    WaterConservationApplicationId  UNIQUEIDENTIFIER NOT NULL,
-    BlobName                        NVARCHAR(255)    NOT NULL,
-    CONSTRAINT FK_ApplicationDocuments_WaterConservationApplications FOREIGN KEY (WaterConservationApplicationId) REFERENCES WaterConservationApplications (Id),
+    Id                              UNIQUEIDENTIFIER NOT NULL CONSTRAINT PK_ApplicationDocuments_Id PRIMARY KEY NONCLUSTERED CONSTRAINT DF_ApplicationDocuments_Id DEFAULT NEWID(),
+    WaterConservationApplicationId  UNIQUEIDENTIFIER NOT NULL CONSTRAINT FK_ApplicationDocuments_WaterConservationApplications FOREIGN KEY REFERENCES WaterConservationApplications (Id),
+    BlobName                        NVARCHAR(255)    NOT NULL
 );
 
 CREATE INDEX IX_ApplicationDocuments_WaterConservationApplicationId ON ApplicationDocuments(WaterConservationApplicationId);
