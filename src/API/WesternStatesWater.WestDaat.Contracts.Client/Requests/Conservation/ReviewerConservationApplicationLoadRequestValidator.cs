78b0fb6d6f49cddb79f248a199e341cdb2bbb4ff
Ethan Weber <eweber@dontpaniclabs.com>
2025-03-13 11:43:36 -0500

    request validators

---
+++ src/API/WesternStatesWater.WestDaat.Contracts.Client/Requests/Conservation/ReviewerConservationApplicationLoadRequestValidator.cs
@@ -0,0 +1,11 @@
+﻿using FluentValidation;
+
+namespace WesternStatesWater.WestDaat.Contracts.Client.Requests.Conservation;
+
+public class ReviewerConservationApplicationLoadRequestValidator : AbstractValidator<ReviewerConservationApplicationLoadRequest>
+{
+    public ReviewerConservationApplicationLoadRequestValidator()
+    {
+        RuleFor(x => x.ApplicationId).NotEmpty();
+    }
+}
