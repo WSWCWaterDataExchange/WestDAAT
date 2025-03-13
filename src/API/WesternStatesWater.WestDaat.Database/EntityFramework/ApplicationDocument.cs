a986dfa6387d7125af64983aa57758d40196fada
Jenny Nielsen <jnielsen@dontpaniclabs.com>
2025-03-13 08:27:50 -0500

    add document description column

--- src/API/WesternStatesWater.WestDaat.Database/EntityFramework/ApplicationDocument.cs
+++ src/API/WesternStatesWater.WestDaat.Database/EntityFramework/ApplicationDocument.cs
@@ -11,6 +11,8 @@ public class ApplicationDocument
     public Guid WaterConservationApplicationId { get; set; }
 
     public string BlobName { get; set; } = null!;
+    
+    public string? Description { get; set; }
 
     public virtual WaterConservationApplication Application { get; set; } = null!;
 }
--- src/API/WesternStatesWater.WestDaat.DbUp/Scripts/001/023 Application Documents.sql
+++ src/API/WesternStatesWater.WestDaat.DbUp/Scripts/001/023 Application Documents.sql
@@ -2,7 +2,8 @@ CREATE TABLE ApplicationDocuments
 (
     Id                              UNIQUEIDENTIFIER NOT NULL CONSTRAINT PK_ApplicationDocuments_Id PRIMARY KEY NONCLUSTERED CONSTRAINT DF_ApplicationDocuments_Id DEFAULT NEWID(),
     WaterConservationApplicationId  UNIQUEIDENTIFIER NOT NULL CONSTRAINT FK_ApplicationDocuments_WaterConservationApplications FOREIGN KEY REFERENCES WaterConservationApplications (Id),
-    BlobName                        NVARCHAR(255)    NOT NULL
+    BlobName                        NVARCHAR(255)    NOT NULL,
+    Description                     NVARCHAR(4000)
 );
 
 CREATE INDEX IX_ApplicationDocuments_WaterConservationApplicationId ON ApplicationDocuments(WaterConservationApplicationId);
