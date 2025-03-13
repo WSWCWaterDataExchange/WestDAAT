507a2206383de4b6a1356e315e89097204630d8f
Ethan Weber <eweber@dontpaniclabs.com>
2025-03-13 14:12:35 -0500

    stub in request handlers

---
+++ src/API/WesternStatesWater.WestDaat.Managers/Handlers/Conservation/ReviewerConservationApplicationLoadRequestHandler.cs
@@ -0,0 +1,13 @@
+﻿using WesternStatesWater.Shared.Resolver;
+using WesternStatesWater.WestDaat.Contracts.Client.Requests.Conservation;
+using WesternStatesWater.WestDaat.Contracts.Client.Responses.Conservation;
+
+namespace WesternStatesWater.WestDaat.Managers.Handlers.Conservation;
+
+public class ReviewerConservationApplicationLoadRequestHandler : IRequestHandler<ReviewerConservationApplicationLoadRequest, ReviewerConservationApplicationLoadResponse>
+{
+    public Task<ReviewerConservationApplicationLoadResponse> Handle(ReviewerConservationApplicationLoadRequest request)
+    {
+        throw new NotImplementedException();
+    }
+}
