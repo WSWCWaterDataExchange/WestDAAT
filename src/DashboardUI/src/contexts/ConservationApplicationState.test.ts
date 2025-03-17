f07b470861d87c9835cf1a8a3910f63b63c53bfa
Ethan Weber <eweber@dontpaniclabs.com>
2025-03-17 17:01:55 -0500

    new state test

--- src/DashboardUI/src/contexts/ConservationApplicationState.test.ts
+++ src/DashboardUI/src/contexts/ConservationApplicationState.test.ts
@@ -5,6 +5,7 @@ import { CompensationRateUnits } from '../data-contracts/CompensationRateUnits';
 import { ConservationApplicationStatus } from '../data-contracts/ConservationApplicationStatus';
 import { ConservationApplicationState, defaultState, reducer } from './ConservationApplicationState';
 import { MapSelectionPolygonData } from '../data-contracts/CombinedPolygonData';
+import { ApplicationDetails } from '../data-contracts/ApplicationDetails';
 
 const shouldBeAbleToPerformConsumptiveUseEstimate = (state: ConservationApplicationState, expected: boolean): void => {
   expect(state.canEstimateConsumptiveUse).toEqual(expected);
@@ -165,6 +166,8 @@ describe('ConservationApplicationState reducer', () => {
     // Assert
     expect(newState.conservationApplication.waterConservationApplicationId).toEqual('application-guid');
     expect(newState.conservationApplication.waterConservationApplicationDisplayId).toEqual('display-id');
+
+    expect(newState.isCreatingApplication).toBe(true);
 
     shouldBeAbleToPerformConsumptiveUseEstimate(newState, false);
     shouldBeAbleToContinueToApplication(newState, false);
@@ -522,37 +525,30 @@ describe('ConservationApplicationState reducer', () => {
     });
   });
 
-  describe('Additional Use Cases', () => {
-    it("should reset the Application Submission form's polygons' AdditionalDetails fields if the user updates their polygon selections.", () => {
-      // user selects map polygons
-      const polygonWkt = 'POLYGON((0 0, 0 1, 1 1, 1 0, 0 0))';
-      let newState = reducer(state, {
-        type: 'MAP_POLYGONS_UPDATED',
-        payload: {
-          polygons: [
+  describe('Review Page Use Cases', () => {
+    it('loading application should hydrate state', () => {
+      // Arrange
+      const applicationDetails: ApplicationDetails = {
+        id: 'application-guid',
+        applicantUserId: 'applicant-guid',
+        fundingOrganizationId: 'funding-organization-guid',
+        waterRightNativeId: 'water-right-native-id',
+        applicationDisplayId: '2025-ABCD-001',
+        estimate: {
+          id: 'estimate-guid',
+          compensationRateDollars: 100,
+          compensationRateUnits: CompensationRateUnits.AcreFeet,
+          estimatedCompensationDollars: 200,
+          totalAverageYearlyConsumptionEtAcreFeet: 300,
+          locations: [
             {
-              polygonWkt,
-              acreage: 1,
-            },
-          ],
-          doPolygonsOverlap: true,
-        },
-      });
-
-      // user then requests an estimate for said polygons
-      newState = reducer(newState, {
-        type: 'CONSUMPTIVE_USE_ESTIMATED',
-        payload: {
-          totalAverageYearlyEtAcreFeet: 100,
-          conservationPayment: 200,
-          dataCollections: [
-            {
-              waterConservationApplicationEstimateLocationId: 'location-guid',
-              polygonWkt: polygonWkt,
-              averageYearlyEtInAcreFeet: 50,
-              averageYearlyEtInInches: 400,
-              datapoints: [
+              id: 'location-guid',
+              polygonWkt: 'POLYGON((0 0, 0 1, 1 1, 1 0, 0 0))',
+              polygonAreaInAcres: 1,
+              additionalDetails: 'I, the user, have some things to say about this field.',
+              consumptiveUses: [
                 {
+                  id: 'consumptive-use-guid',
                   year: 2025,
                   etInInches: 400,
                 },
@@ -560,38 +556,219 @@ describe('ConservationApplicationState reducer', () => {
             },
           ],
         },
-      });
+        submission: {
+          id: 'submission-guid',
+          submittedDate: '2025-01-01T00:00:00.0000000 +00:00',
+          acceptedDate: null,
+          rejectedDate: null,
 
-      // user fills out part of the application submission form...
-      newState = reducer(newState, {
-        type: 'APPLICATION_SUBMISSION_FORM_UPDATED',
-        payload: {
-          formValues: {
-            ...defaultApplicationSubmissionForm(),
-            fieldDetails: [
-              {
-                waterConservationApplicationEstimateLocationId: 'location-guid',
-                additionalDetails: 'I, the user, have some things to say about this field.',
-              },
-            ],
-          },
+          agentName: 'Agent Name',
+          agentEmail: 'agent@email',
+          agentPhoneNumber: '555-555-5555',
+          agentAdditionalDetails: 'Agent additional details',
+
+          landownerName: 'Bobby Hill',
+          landownerEmail: 'landowner@email',
+          landownerPhoneNumber: '555-555-5555',
+          landownerAddress: '123 Main St',
+          landownerCity: 'Anytown',
+          landownerState: 'NE',
+          landownerZipCode: '12345',
+
+          canalOrIrrigationEntityName: 'Canal Entity Name',
+          canalOrIrrigationEntityEmail: 'canal@email',
+          canalOrIrrigationEntityPhoneNumber: '555-555-5555',
+          canalOrIrrigationAdditionalDetails: 'Canal additional details',
+
+          conservationPlanFundingRequestDollarAmount: 1000,
+          conservationPlanFundingRequestCompensationRateUnits: CompensationRateUnits.AcreFeet,
+          conservationPlanDescription: 'Conservation plan description',
+          conservationPlanAdditionalInfo: 'Conservation plan additional info',
+          estimationSupplementaryDetails: 'Estimation supplementary details',
+
+          permitNumber: 'Permit Number',
+          facilityDitchName: 'Facility Ditch Name',
+          priorityDate: '2025-01-01T00:00:00.0000000 +00:00',
+          certificateNumber: 'Certificate Number',
+          shareNumber: 'Share Number',
+          waterRightState: 'NE',
+          waterUseDescription: 'Water use description',
         },
-      });
+        supportingDocuments: [
+          {
+            id: 'document-guid',
+            blobName: 'blobname',
+            fileName: 'filename.pdf',
+            description: 'Document description',
+          },
+        ],
+      };
 
-      expect(newState.conservationApplication.applicationSubmissionForm.fieldDetails.length).toBeGreaterThan(0);
-
-      // user then goes back to change their polygon selections
-      newState = reducer(newState, {
-        type: 'MAP_POLYGONS_UPDATED',
+      // Act
+      const newState = reducer(state, {
+        type: 'APPLICATION_LOADED',
         payload: {
-          polygons: [],
-          doPolygonsOverlap: false,
+          application: applicationDetails,
+          notes: [],
         },
       });
 
       // Assert
-      // their AdditionalDetails field should be reset
-      expect(newState.conservationApplication.applicationSubmissionForm.fieldDetails.length).toEqual(0);
+      const application = newState.conservationApplication;
+
+      // application / application estimate
+      expect(application.waterConservationApplicationId).toEqual(applicationDetails.id);
+      expect(application.waterRightNativeId).toEqual(applicationDetails.waterRightNativeId);
+      expect(application.fundingOrganizationId).toEqual(applicationDetails.fundingOrganizationId);
+
+      expect(application.desiredCompensationDollars).toEqual(applicationDetails.estimate.compensationRateDollars);
+      expect(application.desiredCompensationUnits).toEqual(applicationDetails.estimate.compensationRateUnits);
+      expect(application.totalAverageYearlyEtAcreFeet).toEqual(
+        applicationDetails.estimate.totalAverageYearlyConsumptionEtAcreFeet,
+      );
+      expect(application.conservationPayment).toEqual(applicationDetails.estimate.estimatedCompensationDollars);
+
+      // application estimate locations
+      const location = application.estimateLocations[0];
+      const expectedLocation = applicationDetails.estimate.locations[0];
+      expect(application.estimateLocations.length).toEqual(applicationDetails.estimate.locations.length);
+      expect(location.waterConservationApplicationEstimateLocationId).toEqual(expectedLocation.id);
+      expect(location.polygonWkt).toEqual(expectedLocation.polygonWkt);
+      expect(location.acreage).toEqual(expectedLocation.polygonAreaInAcres);
+      expect(location.fieldName).toEqual('Field 1');
+      expect(location.additionalDetails).toEqual(expectedLocation.additionalDetails);
+
+      // application estimate location consumptive uses
+      const consumptiveUse = location.datapoints![0];
+      const expectedConsumptiveUse = expectedLocation.consumptiveUses[0];
+      expect(location.datapoints!.length).toEqual(expectedLocation.consumptiveUses.length);
+      expect(consumptiveUse.year).toEqual(expectedConsumptiveUse.year);
+      expect(consumptiveUse.etInInches).toEqual(expectedConsumptiveUse.etInInches);
+
+      // application supporting documents
+      const document = application.supportingDocuments[0];
+      const expectedDocument = applicationDetails.supportingDocuments[0];
+      expect(application.supportingDocuments.length).toEqual(applicationDetails.supportingDocuments.length);
+      expect(document.fileName).toEqual(expectedDocument.fileName);
+      expect(document.blobName).toEqual(expectedDocument.blobName);
+      expect(document.description).toEqual(expectedDocument.description);
+
+      // application submission
+      const submission = application.applicationSubmissionForm;
+      const expectedSubmission = applicationDetails.submission;
+      expect(submission.agentName).toEqual(expectedSubmission.agentName);
+      expect(submission.agentEmail).toEqual(expectedSubmission.agentEmail);
+      expect(submission.agentPhoneNumber).toEqual(expectedSubmission.agentPhoneNumber);
+      expect(submission.agentAdditionalDetails).toEqual(expectedSubmission.agentAdditionalDetails);
+
+      expect(submission.landownerName).toEqual(expectedSubmission.landownerName);
+      expect(submission.landownerEmail).toEqual(expectedSubmission.landownerEmail);
+      expect(submission.landownerPhoneNumber).toEqual(expectedSubmission.landownerPhoneNumber);
+      expect(submission.landownerAddress).toEqual(expectedSubmission.landownerAddress);
+      expect(submission.landownerCity).toEqual(expectedSubmission.landownerCity);
+      expect(submission.landownerState).toEqual(expectedSubmission.landownerState);
+      expect(submission.landownerZipCode).toEqual(expectedSubmission.landownerZipCode);
+
+      expect(submission.canalOrIrrigationEntityName).toEqual(expectedSubmission.canalOrIrrigationEntityName);
+      expect(submission.canalOrIrrigationEntityEmail).toEqual(expectedSubmission.canalOrIrrigationEntityEmail);
+      expect(submission.canalOrIrrigationEntityPhoneNumber).toEqual(
+        expectedSubmission.canalOrIrrigationEntityPhoneNumber,
+      );
+      expect(submission.canalOrIrrigationAdditionalDetails).toEqual(
+        expectedSubmission.canalOrIrrigationAdditionalDetails,
+      );
+
+      expect(submission.conservationPlanFundingRequestDollarAmount).toEqual(
+        expectedSubmission.conservationPlanFundingRequestDollarAmount,
+      );
+      expect(submission.conservationPlanFundingRequestCompensationRateUnits).toEqual(
+        expectedSubmission.conservationPlanFundingRequestCompensationRateUnits,
+      );
+      expect(submission.conservationPlanDescription).toEqual(expectedSubmission.conservationPlanDescription);
+      expect(submission.conservationPlanAdditionalInfo).toEqual(expectedSubmission.conservationPlanAdditionalInfo);
+
+      expect(submission.estimationSupplementaryDetails).toEqual(expectedSubmission.estimationSupplementaryDetails);
+
+      expect(submission.permitNumber).toEqual(expectedSubmission.permitNumber);
+      expect(submission.facilityDitchName).toEqual(expectedSubmission.facilityDitchName);
+      expect(submission.priorityDate).toEqual(expectedSubmission.priorityDate);
+      expect(submission.certificateNumber).toEqual(expectedSubmission.certificateNumber);
+      expect(submission.shareNumber).toEqual(expectedSubmission.shareNumber);
+      expect(submission.waterRightState).toEqual(expectedSubmission.waterRightState);
+      expect(submission.waterUseDescription).toEqual(expectedSubmission.waterUseDescription);
+    });
+
+    describe('Additional Use Cases', () => {
+      it("should reset the Application Submission form's polygons' AdditionalDetails fields if the user updates their polygon selections.", () => {
+        // user selects map polygons
+        const polygonWkt = 'POLYGON((0 0, 0 1, 1 1, 1 0, 0 0))';
+        let newState = reducer(state, {
+          type: 'MAP_POLYGONS_UPDATED',
+          payload: {
+            polygons: [
+              {
+                polygonWkt,
+                acreage: 1,
+              },
+            ],
+            doPolygonsOverlap: true,
+          },
+        });
+
+        // user then requests an estimate for said polygons
+        newState = reducer(newState, {
+          type: 'CONSUMPTIVE_USE_ESTIMATED',
+          payload: {
+            totalAverageYearlyEtAcreFeet: 100,
+            conservationPayment: 200,
+            dataCollections: [
+              {
+                waterConservationApplicationEstimateLocationId: 'location-guid',
+                polygonWkt: polygonWkt,
+                averageYearlyEtInAcreFeet: 50,
+                averageYearlyEtInInches: 400,
+                datapoints: [
+                  {
+                    year: 2025,
+                    etInInches: 400,
+                  },
+                ],
+              },
+            ],
+          },
+        });
+
+        // user fills out part of the application submission form...
+        newState = reducer(newState, {
+          type: 'APPLICATION_SUBMISSION_FORM_UPDATED',
+          payload: {
+            formValues: {
+              ...defaultApplicationSubmissionForm(),
+              fieldDetails: [
+                {
+                  waterConservationApplicationEstimateLocationId: 'location-guid',
+                  additionalDetails: 'I, the user, have some things to say about this field.',
+                },
+              ],
+            },
+          },
+        });
+
+        expect(newState.conservationApplication.applicationSubmissionForm.fieldDetails.length).toBeGreaterThan(0);
+
+        // user then goes back to change their polygon selections
+        newState = reducer(newState, {
+          type: 'MAP_POLYGONS_UPDATED',
+          payload: {
+            polygons: [],
+            doPolygonsOverlap: false,
+          },
+        });
+
+        // Assert
+        // their AdditionalDetails field should be reset
+        expect(newState.conservationApplication.applicationSubmissionForm.fieldDetails.length).toEqual(0);
+      });
     });
   });
 });
