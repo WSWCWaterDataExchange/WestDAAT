e99b21b48b63ade147c18acf52b1de70e1266b7e
Ethan Weber <eweber@dontpaniclabs.com>
2025-03-18 14:30:22 -0500

    stub in form layout component

--- src/DashboardUI/src/pages/application/review/ApplicationReviewPage.tsx
+++ src/DashboardUI/src/pages/application/review/ApplicationReviewPage.tsx
@@ -1,7 +1,27 @@
-import { NotImplementedPlaceholder } from '../../../components/NotImplementedAlert';
+import { useNavigate } from 'react-router-dom';
+import { ApplicationNavbar } from '../components/ApplicationNavbar';
+import ApplicationSubmissionFormLayout from '../components/ApplicationSubmissionFormLayout';
 
 function ApplicationReviewPage() {
-  return <NotImplementedPlaceholder />;
+  const navigate = useNavigate();
+
+  const navigateToApplicationOrganizationDashboard = () => {
+    navigate(`/application/organization/dashboard`);
+  };
+
+  return (
+    <div className="d-flex flex-column flex-grow-1 h-100">
+      <ApplicationNavbar
+        navigateBack={navigateToApplicationOrganizationDashboard}
+        backButtonText="Back to Dashboard"
+        centerText="Water Conservation Estimation Tool"
+      />
+
+      <div className="overflow-y-auto">
+        <ApplicationSubmissionFormLayout />
+      </div>
+    </div>
+  );
 }
 
 export default ApplicationReviewPage;
