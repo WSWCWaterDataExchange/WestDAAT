39ca83e213b68f320ad7fb5a6fa2daf20123c0f6
Ethan Weber <eweber@dontpaniclabs.com>
2025-03-18 15:44:40 -0500

    rename Content -> Data

--- src/DashboardUI/src/pages/application/components/ApplicationSubmissionFormData.tsx
+++ src/DashboardUI/src/pages/application/components/ApplicationSubmissionFormData.tsx
@@ -21,7 +21,7 @@ const responsiveOneQuarterWidthDefault = 'col-lg-3 col-md-4 col-sm-6 col-12';
 const responsiveOneThirdWidthDefault = 'col-lg-4 col-md-6 col-12';
 const responsiveHalfWidthDefault = 'col-lg-6 col-12';
 
-function ApplicationSubmissionFormContent() {
+function ApplicationSubmissionFormData() {
   const navigate = useNavigate();
   const { state, dispatch } = useConservationApplicationContext();
   const stateForm = state.conservationApplication.applicationSubmissionForm;
@@ -553,4 +553,4 @@ function ApplicationSubmissionFormContent() {
   );
 }
 
-export default ApplicationSubmissionFormContent;
+export default ApplicationSubmissionFormData;
