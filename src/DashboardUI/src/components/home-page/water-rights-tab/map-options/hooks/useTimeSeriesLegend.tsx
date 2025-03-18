c8f0800a8e8848cfa3c629241e8875bd120f6c63
Jack Nolley <jnolley@dontpaniclabs.com>
2025-03-18 16:16:27 -0500

    Fixed time series polygons so that they show

--- src/DashboardUI/src/components/home-page/water-rights-tab/map-options/hooks/useTimeSeriesLegend.tsx
+++ src/DashboardUI/src/components/home-page/water-rights-tab/map-options/hooks/useTimeSeriesLegend.tsx
@@ -8,18 +8,21 @@ export function useTimeSeriesLegend() {
   const { renderedFeatures } = useMapContext();
 
   const isTimeSeriesVisible = useMemo(() => {
-    return renderedFeatures.some((feature) => feature.layer?.id === mapLayerNames.timeSeriesPointsLayer);
+    return renderedFeatures.some((feature) =>
+      feature.layer?.id === mapLayerNames.timeSeriesPointsLayer ||
+      feature.layer?.id === mapLayerNames.timeSeriesPolygonsLayer
+    );
   }, [renderedFeatures]);
 
   const legendItems = useMemo(() => {
     if (!isTimeSeriesVisible) return undefined;
 
-    return (
-      <MapLegendCircleItem key="timeSeries" color={timeSeriesColor}>
-        Time Series Site
-      </MapLegendCircleItem>
-    );
-  }, [isTimeSeriesVisible, timeSeriesColor]);
+    return [
+      <MapLegendCircleItem key="timeSeriesSites" color={timeSeriesColor}>
+        Time Series Sites
+      </MapLegendCircleItem>,
+    ];
+  }, [isTimeSeriesVisible]);
 
   return { legendItems };
 }
