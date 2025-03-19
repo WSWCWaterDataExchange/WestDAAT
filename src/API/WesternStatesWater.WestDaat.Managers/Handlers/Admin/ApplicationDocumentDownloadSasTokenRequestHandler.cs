29f4994d5bd30e8e99c7671dae549e4ab9391238
Jenny Nielsen <jnielsen@dontpaniclabs.com>
2025-03-19 10:50:07 -0500

    file download call chain completed

--- src/API/WesternStatesWater.WestDaat.Managers/Handlers/Admin/ApplicationDocumentDownloadSasTokenRequestHandler.cs
+++ src/API/WesternStatesWater.WestDaat.Managers/Handlers/Admin/ApplicationDocumentDownloadSasTokenRequestHandler.cs
@@ -1,29 +1,37 @@
 using WesternStatesWater.Shared.Resolver;
 using WesternStatesWater.WestDaat.Accessors;
+using WesternStatesWater.WestDaat.Common.Constants;
 using WesternStatesWater.WestDaat.Common.DataContracts;
 using WesternStatesWater.WestDaat.Contracts.Client.Requests.Admin;
 using WesternStatesWater.WestDaat.Contracts.Client.Responses.Admin;
 using WesternStatesWater.WestDaat.Managers.Mapping;
+using WesternStatesWater.WestDaat.Utilities;
 
 namespace WesternStatesWater.WestDaat.Managers.Handlers.Admin;
 
 public class ApplicationDocumentDownloadSasTokenRequestHandler : IRequestHandler<ApplicationDocumentDownloadSasTokenRequest, ApplicationDocumentDownloadSasTokenResponse>
 {
-    public IApplicationAccessor ApplicationAccessor { get; }
+    private readonly IApplicationAccessor _applicationAccessor;
+    private readonly IBlobStorageSdk _blobStorageSdk;
 
-    public ApplicationDocumentDownloadSasTokenRequestHandler(IApplicationAccessor applicationAccessor)
+    public ApplicationDocumentDownloadSasTokenRequestHandler(IApplicationAccessor applicationAccessor, IBlobStorageSdk blobStorageSdk)
     {
-        ApplicationAccessor = applicationAccessor;
+        _applicationAccessor = applicationAccessor;
+        _blobStorageSdk = blobStorageSdk;
     }
     
     public async Task<ApplicationDocumentDownloadSasTokenResponse> Handle(ApplicationDocumentDownloadSasTokenRequest request)
     {
         var accessorRequest = request.Map<ApplicationDocumentLoadSingleRequest>();
-        var accessorResponse = (ApplicationDocumentLoadSingleResponse)await ApplicationAccessor.Load(accessorRequest);
+        var accessorResponse = (ApplicationDocumentLoadSingleResponse)await _applicationAccessor.Load(accessorRequest);
 
-        return new ApplicationDocumentDownloadSasTokenResponse
+        var blobName = accessorResponse.SupportingDocument.BlobName;
+        var sasUris = await _blobStorageSdk.GetSasUris(Containers.ApplicationDocuments, [blobName], TimeSpan.FromMinutes(10),
+            Azure.Storage.Sas.BlobContainerSasPermissions.Read);
+        
+         return new ApplicationDocumentDownloadSasTokenResponse
         {
-            SasToken = "sas-token",
+            SasToken = sasUris[blobName].ToString(),
             FileName = accessorResponse.SupportingDocument.FileName,
         };
     }
