05efb96e4cae384f5082049173c5fa2337e06fbe
Ethan Weber <eweber@dontpaniclabs.com>
2025-03-20 14:52:19 -0500

    dto contract + request handler

---
+++ src/API/WesternStatesWater.WestDaat.Managers/Handlers/Conservation/WaterConservationApplicationSubmissionUpdateRequestHandler.cs
@@ -0,0 +1,26 @@
+﻿using WesternStatesWater.Shared.Resolver;
+using WesternStatesWater.WestDaat.Accessors;
+using WesternStatesWater.WestDaat.Contracts.Client.Requests.Conservation;
+using WesternStatesWater.WestDaat.Contracts.Client.Responses.Conservation;
+using WesternStatesWater.WestDaat.Managers.Mapping;
+
+namespace WesternStatesWater.WestDaat.Managers.Handlers.Conservation;
+
+public class WaterConservationApplicationSubmissionUpdateRequestHandler
+    : IRequestHandler<WaterConservationApplicationSubmissionUpdateRequest, ApplicationStoreResponseBase>
+{
+    private readonly IApplicationAccessor _applicationAccessor;
+
+    public WaterConservationApplicationSubmissionUpdateRequestHandler(IApplicationAccessor applicationAccessor)
+    {
+        _applicationAccessor = applicationAccessor;
+    }
+
+    public async Task<ApplicationStoreResponseBase> Handle(WaterConservationApplicationSubmissionUpdateRequest request)
+    {
+        var dtoRequest = request.Map<Common.DataContracts.WaterConservationApplicationSubmissionUpdateRequest>();
+        await _applicationAccessor.Store(dtoRequest);
+
+        return new ApplicationStoreResponseBase();
+    }
+}
