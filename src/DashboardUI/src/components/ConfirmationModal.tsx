4b7a567df65a7984d5823cb1548c27f527dc9556
Ethan Weber <eweber@dontpaniclabs.com>
2025-03-21 09:30:49 -0500

    update Submit page to use generic confirmation modal

---
+++ src/DashboardUI/src/components/ConfirmationModal.tsx
@@ -0,0 +1,33 @@
+import Button from 'react-bootstrap/esm/Button';
+import Modal from 'react-bootstrap/esm/Modal';
+
+interface ConfirmationModalProps {
+  show: boolean;
+  onCancel: () => void;
+  onConfirm: () => void;
+  titleText: string;
+  cancelText: string;
+  confirmText: string;
+  children: React.ReactNode;
+}
+
+function ConfirmationModal(props: ConfirmationModalProps) {
+  return (
+    <Modal show={props.show} centered>
+      <Modal.Header closeButton onClick={() => props.onCancel()}>
+        <Modal.Title>{props.titleText}</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>{props.children}</Modal.Body>
+      <Modal.Footer>
+        <Button variant="secondary" onClick={props.onCancel}>
+          {props.cancelText}
+        </Button>
+        <Button variant="primary" onClick={props.onConfirm}>
+          {props.confirmText}
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
+
+export default ConfirmationModal;
