e623056f8c7494a5f07ed3e6737f762547680968
Ethan Weber <eweber@dontpaniclabs.com>
2025-03-27 14:46:01 -0500

    Update 027 EstimateLocation PolygonType.sql

--- src/API/WesternStatesWater.WestDaat.DbUp/Scripts/001/027 EstimateLocation PolygonType.sql
+++ src/API/WesternStatesWater.WestDaat.DbUp/Scripts/001/027 EstimateLocation PolygonType.sql
@@ -1,8 +1,8 @@
 ALTER TABLE dbo.WaterConservationApplicationEstimateLocations ADD
-	PolygonType INT NOT NULL 
-		CONSTRAINT DF_PolygonType_Temp DEFAULT 0;
+	DrawToolType INT NOT NULL 
+		CONSTRAINT DF_DrawToolType_Temp DEFAULT 0;
 
 GO;
 
 ALTER TABLE dbo.WaterConservationApplicationEstimateLocations DROP
-	CONSTRAINT DF_PolygonType_Temp;
+	CONSTRAINT DF_DrawToolType_Temp;
