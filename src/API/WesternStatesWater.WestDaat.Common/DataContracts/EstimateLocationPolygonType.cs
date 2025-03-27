25e0f3373f203ee2f7090e63bf931fba54eb69a0
Ethan Weber <eweber@dontpaniclabs.com>
2025-03-27 10:48:01 -0500

    Create EstimateLocationPolygonType.cs

---
+++ src/API/WesternStatesWater.WestDaat.Common/DataContracts/EstimateLocationPolygonType.cs
@@ -0,0 +1,9 @@
+﻿namespace WesternStatesWater.WestDaat.Common.DataContracts;
+
+public enum EstimateLocationPolygonType
+{
+    Unknown = 0,
+    Freeform = 1,
+    Circle = 2,
+    Rectangle = 3,
+}
