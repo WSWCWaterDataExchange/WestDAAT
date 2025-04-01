59d6ae444dec31c506a97251988bc2acfd782de4
Ethan Weber <eweber@dontpaniclabs.com>
2025-04-01 11:51:49 -0500

    update faker to generate new fields

--- src/API/WesternStatesWater.WestDaat.Tests.Helpers/LocationWaterMeasurementFaker.cs
+++ src/API/WesternStatesWater.WestDaat.Tests.Helpers/LocationWaterMeasurementFaker.cs
@@ -4,9 +4,13 @@ public class LocationWaterMeasurementFaker : Faker<EFWD.LocationWaterMeasurement
 {
     public LocationWaterMeasurementFaker(EFWD.WaterConservationApplicationEstimateLocation location = null)
     {
-        RuleFor(wcaelcu => wcaelcu.Year, f => f.Date.Past(5, DateTime.UtcNow).Year);
+        RuleFor(x => x.Year, f => f.Date.Past(5, DateTime.UtcNow).Year);
 
-        RuleFor(wcaelcu => wcaelcu.TotalEtInInches, f => f.Random.Number(10, 1000));
+        RuleFor(x => x.TotalEtInInches, f => f.Random.Number(10, 100));
+
+        RuleFor(x => x.EffectivePrecipitationInInches, f => f.Random.Number(1, 10));
+
+        RuleFor(x => x.NetEtInInches, (f, x) => x.TotalEtInInches - x.EffectivePrecipitationInInches);
 
         if (location != null)
         {
