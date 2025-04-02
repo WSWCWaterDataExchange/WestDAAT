133d5d394b679e1bace395d3ba3abb8a92cec050
Ethan Weber <eweber@dontpaniclabs.com>
2025-04-02 15:04:04 -0500

    new fakers

---
+++ src/API/WesternStatesWater.WestDaat.Tests.Helpers/ControlLocationWaterMeasurementFaker.cs
@@ -0,0 +1,16 @@
+﻿namespace WesternStatesWater.WestDaat.Tests.Helpers;
+
+public class ControlLocationWaterMeasurementFaker : Faker<EFWD.ControlLocationWaterMeasurement>
+{
+    public ControlLocationWaterMeasurementFaker(EFWD.WaterConservationApplicationEstimateControlLocation location = null)
+    {
+        RuleFor(x => x.Year, f => f.Date.Past(5, DateTime.UtcNow).Year);
+
+        RuleFor(x => x.TotalEtInInches, f => f.Random.Number(10, 100));
+
+        if (location != null)
+        {
+            RuleFor(x => x.Location, () => location);
+        }
+    }
+}
