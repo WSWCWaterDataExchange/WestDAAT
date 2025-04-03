ecb3cbb302006c7fc3a72f61dd4a3e75963d92b7
Ethan Weber <eweber@dontpaniclabs.com>
2025-04-03 11:02:19 -0500

    stub in new request handler

---
+++ src/API/WesternStatesWater.WestDaat.Managers/Handlers/Conservation/EstimateConsumptiveUseReviewerRequestHandler.cs
@@ -0,0 +1,17 @@
+﻿using WesternStatesWater.Shared.Resolver;
+using WesternStatesWater.WestDaat.Contracts.Client.Requests.Conservation;
+using WesternStatesWater.WestDaat.Contracts.Client.Responses.Conservation;
+
+namespace WesternStatesWater.WestDaat.Managers.Handlers.Conservation;
+
+public class EstimateConsumptiveUseReviewerRequestHandler : IRequestHandler<EstimateConsumptiveUseReviewerRequest, EstimateConsumptiveUseReviewerResponse>
+{
+    public EstimateConsumptiveUseReviewerRequestHandler()
+    {
+    }
+
+    public async Task<EstimateConsumptiveUseReviewerResponse> Handle(EstimateConsumptiveUseReviewerRequest request)
+    {
+
+    }
+}
