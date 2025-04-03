94c73768094eab636dd145792a2b88d6e4212d6b
Ethan Weber <eweber@dontpaniclabs.com>
2025-04-03 12:53:35 -0500

    [wip] request handler

--- src/API/WesternStatesWater.WestDaat.Managers/Handlers/Conservation/EstimateConsumptiveUseReviewerRequestHandler.cs
+++ src/API/WesternStatesWater.WestDaat.Managers/Handlers/Conservation/EstimateConsumptiveUseReviewerRequestHandler.cs
@@ -1,13 +1,31 @@
 ﻿using WesternStatesWater.Shared.Resolver;
+using WesternStatesWater.WestDaat.Accessors;
 using WesternStatesWater.WestDaat.Contracts.Client.Requests.Conservation;
 using WesternStatesWater.WestDaat.Contracts.Client.Responses.Conservation;
+using WesternStatesWater.WestDaat.Engines;
+using WesternStatesWater.WestDaat.Utilities;
 
 namespace WesternStatesWater.WestDaat.Managers.Handlers.Conservation;
 
 public class EstimateConsumptiveUseReviewerRequestHandler : IRequestHandler<EstimateConsumptiveUseReviewerRequest, EstimateConsumptiveUseReviewerResponse>
 {
-    public EstimateConsumptiveUseReviewerRequestHandler()
+    public ICalculationEngine CalculationEngine { get; }
+    public IOpenEtSdk OpenEtSdk { get; }
+    public IApplicationAccessor ApplicationAccessor { get; }
+    public IOrganizationAccessor OrganizationAccessor { get; }
+    public IWaterAllocationAccessor WaterAllocationAccessor { get; }
+
+    public EstimateConsumptiveUseReviewerRequestHandler(ICalculationEngine calculationEngine,
+        IOpenEtSdk openEtSdk,
+        IApplicationAccessor applicationAccessor,
+        IOrganizationAccessor organizationAccessor,
+        IWaterAllocationAccessor waterAllocationAccessor)
     {
+        CalculationEngine = calculationEngine;
+        OpenEtSdk = openEtSdk;
+        ApplicationAccessor = applicationAccessor;
+        OrganizationAccessor = organizationAccessor;
+        WaterAllocationAccessor = waterAllocationAccessor;
     }
 
     public async Task<EstimateConsumptiveUseReviewerResponse> Handle(EstimateConsumptiveUseReviewerRequest request)
