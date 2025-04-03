6429d650ab08e3e89f724a437d7df31bfa5d0f64
Ethan Weber <eweber@dontpaniclabs.com>
2025-04-03 10:42:15 -0500

    Create EstimateConsumptiveUseReviewerRequestValidator.cs

---
+++ src/API/WesternStatesWater.WestDaat.Contracts.Client/Requests/Conservation/EstimateConsumptiveUseReviewerRequestValidator.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+
+namespace WesternStatesWater.WestDaat.Contracts.Client.Requests.Conservation;
+
+public class EstimateConsumptiveUseReviewerRequestValidator : AbstractValidator<EstimateConsumptiveUseReviewerRequest>
+{
+    public EstimateConsumptiveUseReviewerRequestValidator()
+    {
+        RuleFor(x => x.WaterConservationApplicationId).NotEmpty();
+
+        RuleFor(x => x.WaterRightNativeId).NotEmpty();
+
+        RuleFor(x => x.Polygons).NotEmpty().Must(polygons => polygons.Length <= 20);
+        RuleForEach(x => x.Polygons).ChildRules(polygonEntryValidator =>
+        {
+            polygonEntryValidator.RuleFor(polygon => polygon).NotNull();
+            polygonEntryValidator.RuleFor(polygon => polygon.PolygonWkt).NotEmpty();
+            polygonEntryValidator.RuleFor(polygon => polygon.DrawToolType).NotEmpty();
+        });
+
+        RuleFor(x => x.ControlLocation).NotEmpty();
+        RuleFor(x => x.ControlLocation.PointWkt).NotEmpty().MaximumLength(100).Must(pointWkt => pointWkt.Contains("POINT"));
+    }
+}
