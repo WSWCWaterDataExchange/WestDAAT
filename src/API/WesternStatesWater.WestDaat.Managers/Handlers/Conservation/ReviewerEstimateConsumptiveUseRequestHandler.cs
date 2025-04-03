cb4509401fccc514467416e27f293d494263fba2
Ethan Weber <eweber@dontpaniclabs.com>
2025-04-03 16:41:38 -0500

    implement the request handler
    I think I've got the flow figured out, need to test still though of course

--- src/API/WesternStatesWater.WestDaat.Managers/Handlers/Conservation/ReviewerEstimateConsumptiveUseRequestHandler.cs
+++ src/API/WesternStatesWater.WestDaat.Managers/Handlers/Conservation/ReviewerEstimateConsumptiveUseRequestHandler.cs
@@ -1,8 +1,10 @@
 ﻿using WesternStatesWater.Shared.Resolver;
 using WesternStatesWater.WestDaat.Accessors;
+using WesternStatesWater.WestDaat.Common.DataContracts;
 using WesternStatesWater.WestDaat.Contracts.Client.Requests.Conservation;
 using WesternStatesWater.WestDaat.Contracts.Client.Responses.Conservation;
 using WesternStatesWater.WestDaat.Engines;
+using WesternStatesWater.WestDaat.Managers.Mapping;
 using WesternStatesWater.WestDaat.Utilities;
 
 namespace WesternStatesWater.WestDaat.Managers.Handlers.Conservation;
@@ -30,6 +32,51 @@ public class ReviewerEstimateConsumptiveUseRequestHandler : IRequestHandler<Revi
 
     public async Task<ReviewerEstimateConsumptiveUseResponse> Handle(ReviewerEstimateConsumptiveUseRequest request)
     {
+        // determine application's linked funding org
+        var applicationDetailsRequest = request.Map<ApplicationLoadSingleRequest>();
+        var applicationDetailsResponse = (ApplicationLoadSingleResponse)await ApplicationAccessor.Load(applicationDetailsRequest);
 
+        // we need funding org details to determine the parameters for computing each location's ET
+        var fundingOrgDetailsRequest = applicationDetailsResponse.Map<OrganizationFundingDetailsRequest>();
+        var fundingOrgDetailsResponse = (OrganizationFundingDetailsResponse)await OrganizationAccessor.Load(fundingOrgDetailsRequest);
+
+        // compute ET
+        var evapotranspirationRequest = DtoMapper.Map<MultiPolygonYearlyEtRequest>((request, fundingOrgDetailsResponse.Organization));
+        var evapotranspirationResponse = (MultiPolygonYearlyEtResponse)await CalculationEngine.Calculate(evapotranspirationRequest);
+
+        var dataCollections = evapotranspirationResponse.Map<Contracts.Client.PolygonEtDataCollection[]>();
+
+        EstimateConservationPaymentResponse estimateConservationPaymentResponse = null;
+        if (request.OverwriteEstimate)
+        {
+            var originalEstimate = applicationDetailsResponse.Application.Estimate;
+            var estimateConservationPaymentRequest = DtoMapper.Map<EstimateConservationPaymentRequest>((request, evapotranspirationResponse, originalEstimate));
+            estimateConservationPaymentResponse = (EstimateConservationPaymentResponse)await CalculationEngine.Calculate(estimateConservationPaymentRequest);
+
+            // store estimate
+            var storeEstimateRequest = DtoMapper.Map<ApplicationEstimateStoreRequest>((
+                request,
+                fundingOrgDetailsResponse.Organization,
+                evapotranspirationResponse,
+                estimateConservationPaymentResponse
+            ));
+
+            var storeEstimateResponse = (ApplicationEstimateStoreResponse)await ApplicationAccessor.Store(storeEstimateRequest);
+
+            // connect the EstimateLocation Ids to the ET data
+            foreach (var collection in dataCollections)
+            {
+                var matchingEstimateLocation = storeEstimateResponse.Details.Single(detail => detail.PolygonWkt == collection.PolygonWkt);
+                collection.WaterConservationApplicationEstimateLocationId = matchingEstimateLocation.WaterConservationApplicationEstimateLocationId;
+            }
+        }
+
+        return new ReviewerEstimateConsumptiveUseResponse
+        {
+            ConservationPayment = null,
+            CumulativeTotalEtInAcreFeet = evapotranspirationResponse.DataCollections.Sum(dc => dc.AverageYearlyTotalEtInAcreFeet),
+            CumulativeNetEtInAcreFeet = evapotranspirationResponse.DataCollections.Sum(dc => dc.AverageYearlyNetEtInAcreFeet.Value),
+            DataCollections = dataCollections,
+        };
     }
 }
