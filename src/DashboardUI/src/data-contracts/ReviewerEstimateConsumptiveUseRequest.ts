b0b1c1df33adb50ebc0f12d6b6cf40820be4f4ac
Ethan Weber <eweber@dontpaniclabs.com>
2025-04-07 14:01:00 -0500

    request contract

---
+++ src/DashboardUI/src/data-contracts/ReviewerEstimateConsumptiveUseRequest.ts
@@ -0,0 +1,11 @@
+import { ApplicationStoreRequestBase } from './ApplicationStoreRequestBase';
+import { MapPoint } from './MapPoint';
+import { MapPolygon } from './MapPolygon';
+
+export interface ReviewerEstimateConsumptiveUseRequest extends ApplicationStoreRequestBase {
+  $type: 'ReviewerEstimateConsumptiveUseRequest';
+  waterConservationApplicationId: string;
+  polygons: MapPolygon[];
+  controlLocation: MapPoint;
+  overwriteEstimate: boolean;
+}
