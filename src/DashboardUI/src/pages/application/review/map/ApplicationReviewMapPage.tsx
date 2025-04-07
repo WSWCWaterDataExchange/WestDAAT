6bf65deefe588796ee832565de4befc7c22a522e
Ethan Weber <eweber@dontpaniclabs.com>
2025-04-07 15:13:41 -0500

    [wip] update map to support reviewer ECU

--- src/DashboardUI/src/pages/application/review/map/ApplicationReviewMapPage.tsx
+++ src/DashboardUI/src/pages/application/review/map/ApplicationReviewMapPage.tsx
@@ -3,19 +3,47 @@ import { useConservationApplicationContext } from '../../../../contexts/Conserva
 import { EstimationToolSidebar } from '../../estimation-tool/EstimationToolSidebar';
 import { EstimationToolMapHeader } from '../../estimation-tool/EstimationToolMapHeader';
 import ReviewMap from './ReviewMap';
+import { reviewerEstimateConsumptiveUse } from '../../../../accessors/applicationAccessor';
+import { useMsal } from '@azure/msal-react';
+import { MapPolygon } from '../../../../data-contracts/MapPolygon';
+import { ReviewerEstimateConsumptiveUseResponse } from '../../../../data-contracts/ReviewerEstimateConsumptiveUseResponse';
+import { toast } from 'react-toastify';
 
 export function ApplicationReviewMapPage() {
-  const { state } = useConservationApplicationContext();
+  const context = useMsal();
+  const { state, dispatch } = useConservationApplicationContext();
 
   const estimateConsumptiveUseMutation = useMutation({
-    mutationFn: async () => {
-      alert('Not implemented. This feature will be available in a future release.');
-      return;
+    mutationFn: async (overwriteEstimate: boolean) => {
+      const fields: Parameters<typeof reviewerEstimateConsumptiveUse>[1] = {
+        waterConservationApplicationId: state.conservationApplication.waterConservationApplicationId!,
+        polygons: state.conservationApplication.estimateLocations.map(
+          (polygon): MapPolygon => ({
+            polygonWkt: polygon.polygonWkt!,
+            drawToolType: polygon.drawToolType!,
+          }),
+        ),
+        controlLocation: {
+          pointWkt: 'POINT (-106.1264 34.9799)',
+        },
+        overwriteEstimate: overwriteEstimate,
+      };
+
+      validateFields(fields);
+
+      return await reviewerEstimateConsumptiveUse(context, fields);
+    },
+    onSuccess: (result: ReviewerEstimateConsumptiveUseResponse) => {
+      toast.success('Consumptive use estimate successfully calculated.');
+      console.log(result);
+    },
+    onError: (error: Error) => {
+      toast.error('Failed to calculate consumptive use estimate.');
     },
   });
 
   const handleEstimateConsumptiveUseClicked = async () => {
-    await estimateConsumptiveUseMutation.mutateAsync();
+    await estimateConsumptiveUseMutation.mutateAsync(false);
   };
 
   return (
@@ -41,3 +69,17 @@ export function ApplicationReviewMapPage() {
     </div>
   );
 }
+
+const validateFields = (fields: Parameters<typeof reviewerEstimateConsumptiveUse>[1]) => {
+  const isValid: boolean =
+    !!fields.waterConservationApplicationId &&
+    !!fields.polygons &&
+    fields.polygons.length > 0 &&
+    fields.polygons.every((polygon) => !!polygon.polygonWkt && !!polygon.drawToolType) &&
+    !!fields.controlLocation &&
+    !!fields.controlLocation.pointWkt;
+
+  if (!isValid) {
+    throw new Error('Invalid fields');
+  }
+};
