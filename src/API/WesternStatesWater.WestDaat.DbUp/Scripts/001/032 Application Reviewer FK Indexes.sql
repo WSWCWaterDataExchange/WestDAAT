5c6bdf3db0a264f6188c7183ba69bcc19f06d37b
Jenny Nielsen <jnielsen@dontpaniclabs.com>
2025-04-08 11:31:41 -0500

    add index to foreign keys for application reveiwer user ids

---
+++ src/API/WesternStatesWater.WestDaat.DbUp/Scripts/001/032 Application Reviewer FK Indexes.sql
@@ -0,0 +1,7 @@
+    CREATE UNIQUE INDEX IX_WaterConservationApplicationSubmissions_RecommendedByUserId
+        ON dbo.WaterConservationApplicationSubmissions (RecommendedByUserId)
+        WHERE RecommendedByUserId IS NOT NULL;
+    CREATE UNIQUE INDEX IX_WaterConservationApplicationSubmissions_ApprovedByUserId
+        ON dbo.WaterConservationApplicationSubmissions (ApprovedByUserId)
+        WHERE ApprovedByUserId IS NOT NULL;
+GO
