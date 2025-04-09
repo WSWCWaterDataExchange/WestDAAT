9b99cdbc6f2f0cf4f90ad677a44df19f977b60f7
Ethan Weber <eweber@dontpaniclabs.com>
2025-04-09 14:53:14 -0500

    Create ControlLocationDetails.cs

---
+++ src/API/WesternStatesWater.WestDaat.Common/DataContracts/ControlLocationDetails.cs
@@ -0,0 +1,10 @@
+﻿namespace WesternStatesWater.WestDaat.Common.DataContracts;
+
+public class ControlLocationDetails
+{
+    public Guid Id { get; set; }
+
+    public string PointWkt { get; set; } = null!;
+
+    public ControlLocationWaterMeasurementDetails[] WaterMeasurements { get; set; } = null!;
+}
