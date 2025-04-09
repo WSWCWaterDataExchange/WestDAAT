fd80a5cd7594040e5368f54cf2172750afcb6296
Branden Barber <bbarber@dontpaniclabs.com>
2025-04-09 09:39:44 -0500

    Cleanup

--- src/DashboardUI/src/pages/application/components/ApplicationReviewHeader.tsx
+++ src/DashboardUI/src/pages/application/components/ApplicationReviewHeader.tsx
@@ -2,6 +2,7 @@ import Icon from '@mdi/react';
 import { useConservationApplicationContext } from '../../../contexts/ConservationApplicationProvider';
 import { mdiCircle } from '@mdi/js';
 import {
+  ConservationApplicationStatus,
   ConservationApplicationStatusDisplayNames,
   getApplicationStatusIconClass,
 } from '../../../data-contracts/ConservationApplicationStatus';
@@ -16,16 +17,20 @@ function ApplicationReviewHeader(props: ApplicationReviewHeaderProps) {
   return (
     <div className="my-4">
       <div className="d-flex">
-        <h1 className="fs-4 fw-bold">Application for Water Right Native ID: {state.conservationApplication.waterRightNativeId}</h1>
+        <h1 className="fs-4 fw-bold">
+          Application for Water Right Native ID: {state.conservationApplication.waterRightNativeId}
+        </h1>
       </div>
-      <div className="d-flex align-items-center gap-2 mb-2">
-        <Icon
-          size={0.7}
-          path={mdiCircle}
-          className={`flex-shrink-0 ` + getApplicationStatusIconClass(state.conservationApplication.status)}
-        />
-        {ConservationApplicationStatusDisplayNames[state.conservationApplication.status]}
-      </div>
+      {state.conservationApplication.status !== ConservationApplicationStatus.Unknown && (
+        <div className="d-flex align-items-center gap-2 mb-2">
+          <Icon
+            size={0.7}
+            path={mdiCircle}
+            className={`flex-shrink-0 ` + getApplicationStatusIconClass(state.conservationApplication.status)}
+          />
+          {ConservationApplicationStatusDisplayNames[state.conservationApplication.status]}
+        </div>
+      )}
       <div className="d-flex gap-3">
         <span className="fw-bold">Water Right Native ID: {state.conservationApplication.waterRightNativeId}</span>
 
@@ -36,7 +41,7 @@ function ApplicationReviewHeader(props: ApplicationReviewHeaderProps) {
         <span className="fw-bold">Funding Organization: {state.conservationApplication.fundingOrganizationName}</span>
       </div>
 
-      <div className="mb-4">
+      <div className="mb-4 mt-2">
         <span>{props.additionalText}</span>
       </div>
     </div>
