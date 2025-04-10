7eb90230e87062359035de92351915c1bd1682b6
Ethan Weber <eweber@dontpaniclabs.com>
2025-04-10 11:47:20 -0500

    fix issue - test is now contained in the correct `describe` block

--- src/DashboardUI/src/utilities/geometryHelpers.test.ts
+++ src/DashboardUI/src/utilities/geometryHelpers.test.ts
@@ -31,50 +31,50 @@ describe('geometryHelpers', () => {
 
       expect(result).toEqual(true);
     });
-  });
 
-  it('should return false if the polygons do not intersect', () => {
-    // Arrange
-    const polygon1: Feature<Polygon, GeoJsonProperties> = {
-      type: 'Feature',
-      properties: {},
-      geometry: {
-        type: 'Polygon',
-        coordinates: [
-          [
-            [0, 0],
-            [0, 1],
-            [1, 1],
-            [1, 0],
-            [0, 0],
+    it('should return false if the polygons do not intersect', () => {
+      // Arrange
+      const polygon1: Feature<Polygon, GeoJsonProperties> = {
+        type: 'Feature',
+        properties: {},
+        geometry: {
+          type: 'Polygon',
+          coordinates: [
+            [
+              [0, 0],
+              [0, 1],
+              [1, 1],
+              [1, 0],
+              [0, 0],
+            ],
           ],
-        ],
-      },
-    };
+        },
+      };
 
-    const polygon2: Feature<Polygon, GeoJsonProperties> = {
-      type: 'Feature',
-      properties: {},
-      geometry: {
-        type: 'Polygon',
-        coordinates: [
-          [
-            [2, 2],
-            [2, 3],
-            [3, 3],
-            [3, 2],
-            [2, 2],
+      const polygon2: Feature<Polygon, GeoJsonProperties> = {
+        type: 'Feature',
+        properties: {},
+        geometry: {
+          type: 'Polygon',
+          coordinates: [
+            [
+              [2, 2],
+              [2, 3],
+              [3, 3],
+              [3, 2],
+              [2, 2],
+            ],
           ],
-        ],
-      },
-    };
+        },
+      };
 
-    const polygons = [polygon1, polygon2];
+      const polygons = [polygon1, polygon2];
 
-    // Act
-    const result = doPolygonsIntersect(polygons);
+      // Act
+      const result = doPolygonsIntersect(polygons);
 
-    // Assert
-    expect(result).toEqual(false);
+      // Assert
+      expect(result).toEqual(false);
+    });
   });
 });
