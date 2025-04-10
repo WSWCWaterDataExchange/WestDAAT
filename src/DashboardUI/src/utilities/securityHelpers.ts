799b95c4d7a2f69988146c0ec977f7d8c771384f
Branden Barber <bbarber@dontpaniclabs.com>
2025-04-10 12:31:41 -0500

    Move to security helper

--- src/DashboardUI/src/utilities/securityHelpers.ts
+++ src/DashboardUI/src/utilities/securityHelpers.ts
@@ -1,18 +1,29 @@
 import { Role } from '../config/role';
 import { User } from '../hooks/useAuthenticationContext';
-
-export const hasUserRole = (user: User | null, role: Role): boolean => {
-  return user?.roles?.some((userRole) => userRole === role) ?? false;
-};
-
-export const hasOrganizationRole = (user: User | null, role: Role): boolean => {
-  return (
-    user?.organizationRoles?.some((organizationRole) => organizationRole.roles.some((userRole) => userRole === role)) ??
-    false
-  );
-};
+import { Permission, RolePermissions } from '../roleConfig';
 
 export const getUserOrganization = (user: User | null): string | null => {
   // Assumes user only has one organization
   return user?.organizationRoles?.[0]?.organizationId ?? null;
+};
+
+export const hasPermission = (user: User | null, permission: Permission): boolean => {
+  if (!user) {
+    return false;
+  }
+
+  if (hasUserRole(user, Role.GlobalAdmin)) {
+    return true;
+  }
+
+  const nonOrgRoles = user.roles ?? [];
+  const orgRoles = user.organizationRoles?.flatMap((orgRole) => orgRole.roles) ?? [];
+  const userRoles = [...nonOrgRoles, ...orgRoles];
+
+  return userRoles.some((role) => RolePermissions[role].includes(permission));
+};
+
+// Keep as private method. Rely on hasPermission for checking access to permissions instead
+const hasUserRole = (user: User | null, role: Role): boolean => {
+  return user?.roles?.some((userRole) => userRole === role) ?? false;
 };
