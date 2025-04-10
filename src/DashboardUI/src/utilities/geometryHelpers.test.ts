e5f7769b5a76fb53ac063801cb613995e592fafc
Ethan Weber <eweber@dontpaniclabs.com>
2025-04-10 11:59:20 -0500

    add tests to verify functionality

--- src/DashboardUI/src/utilities/geometryHelpers.test.ts
+++ src/DashboardUI/src/utilities/geometryHelpers.test.ts
@@ -1,7 +1,7 @@
 import './geometryHelpers.mock';
 
-import { Feature, GeoJsonProperties, Polygon } from 'geojson';
-import { doPolygonsIntersect } from './geometryHelpers';
+import { Feature, GeoJsonProperties, Point, Polygon } from 'geojson';
+import { doesPointExistWithinPolygon, doPolygonsIntersect } from './geometryHelpers';
 
 describe('geometryHelpers', () => {
   describe('doPolygonsIntersect', () => {
@@ -76,5 +76,119 @@ describe('geometryHelpers', () => {
       // Assert
       expect(result).toEqual(false);
     });
+  });
+
+  describe('doesPointIntersectWithPolygon', () => {
+    it('should return true if the point is contained within the polygon', () => {
+      // Arrange
+      const point: Feature<Point, GeoJsonProperties> = {
+        type: 'Feature',
+        properties: {},
+        geometry: {
+          type: 'Point',
+          coordinates: [0.5, 0.5],
+        },
+      };
+
+      const polygon: Feature<Polygon, GeoJsonProperties> = {
+        type: 'Feature',
+        properties: {},
+        geometry: {
+          type: 'Polygon',
+          coordinates: [
+            [
+              [0, 0],
+              [0, 1],
+              [1, 1],
+              [1, 0],
+              [0, 0],
+            ],
+          ],
+        },
+      };
+
+      // Act
+      const result = doesPointExistWithinPolygon(point, polygon);
+
+      // Assert
+      expect(result).toEqual(true);
+    });
+  });
+
+  it('should return false if the point is not contained within the polygon', () => {
+    // Arrange
+    const point: Feature<Point, GeoJsonProperties> = {
+      type: 'Feature',
+      properties: {},
+      geometry: {
+        type: 'Point',
+        coordinates: [1.5, 1.5],
+      },
+    };
+
+    const polygon: Feature<Polygon, GeoJsonProperties> = {
+      type: 'Feature',
+      properties: {},
+      geometry: {
+        type: 'Polygon',
+        coordinates: [
+          [
+            [0, 0],
+            [0, 1],
+            [1, 1],
+            [1, 0],
+            [0, 0],
+          ],
+        ],
+      },
+    };
+
+    // Act
+    const result = doesPointExistWithinPolygon(point, polygon);
+
+    // Assert
+    expect(result).toEqual(false);
+  });
+
+  it('it should return the correct result if the point exists on the boundary of the polygon, depending on the options', () => {
+    // Arrange
+    const point: Feature<Point, GeoJsonProperties> = {
+      type: 'Feature',
+      properties: {},
+      geometry: {
+        type: 'Point',
+        coordinates: [1, 1],
+      },
+    };
+
+    const polygon: Feature<Polygon, GeoJsonProperties> = {
+      type: 'Feature',
+      properties: {},
+      geometry: {
+        type: 'Polygon',
+        coordinates: [
+          [
+            [0, 0],
+            [0, 1],
+            [1, 1],
+            [1, 0],
+            [0, 0],
+          ],
+        ],
+      },
+    };
+
+    // Act
+    const ignoreBoundaryResult = doesPointExistWithinPolygon(point, polygon, {
+      ignoreBoundary: true,
+    });
+
+    const includeBoundaryResult = doesPointExistWithinPolygon(point, polygon, {
+      ignoreBoundary: false,
+    });
+
+    // Assert
+    expect(ignoreBoundaryResult).toEqual(false);
+    expect(includeBoundaryResult).toEqual(true);
   });
 });
