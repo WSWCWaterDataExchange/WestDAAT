ac9d71f723adb3f5ab83c0a5a6f7a17887c45ca7
Jenny Nielsen <69592396+nielsjanni@users.noreply.github.com>
2025-04-14 16:43:56 -0500

    Update src/API/WesternStatesWater.WestDaat.Contracts.Client/Requests/Conservation/WaterConservationApplicationSubmissionNoteCreateRequest.cs
    Co-authored-by: Branden Barber <bbarber@dontpaniclabs.com>

--- src/API/WesternStatesWater.WestDaat.Contracts.Client/Requests/Conservation/WaterConservationApplicationSubmissionNoteCreateRequest.cs
+++ src/API/WesternStatesWater.WestDaat.Contracts.Client/Requests/Conservation/WaterConservationApplicationSubmissionNoteCreateRequest.cs
@@ -3,5 +3,6 @@ namespace WesternStatesWater.WestDaat.Contracts.Client.Requests.Conservation;
 public class WaterConservationApplicationSubmissionNoteCreateRequest : ApplicationStoreRequestBase
 {
     public Guid WaterConservationApplicationId { get; set; }
+
     public string Note { get; set; }
 }
