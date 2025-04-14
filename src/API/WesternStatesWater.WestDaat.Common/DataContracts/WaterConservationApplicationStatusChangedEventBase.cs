60ff0acc060466249999b0d72a0b62ade6c9802d
Branden Barber <bbarber@dontpaniclabs.com>
2025-04-14 15:27:25 -0500

    Ope

--- src/API/WesternStatesWater.WestDaat.Common/DataContracts/WaterConservationApplicationStatusChangedEventBase.cs
+++ src/API/WesternStatesWater.WestDaat.Common/DataContracts/WaterConservationApplicationStatusChangedEventBase.cs
@@ -3,6 +3,4 @@ namespace WesternStatesWater.WestDaat.Common.DataContracts;
 public class WaterConservationApplicationStatusChangedEventBase
 {
     public Guid ApplicationId { get; set; }
-
-    public string ApprovalNote { get; set; }
 }
