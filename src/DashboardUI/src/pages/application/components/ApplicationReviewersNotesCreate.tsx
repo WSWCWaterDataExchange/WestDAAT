db9c3f19daeaf0c36d271119c0f34667a3a2f7af
Jenny Nielsen <jnielsen@dontpaniclabs.com>
2025-04-15 13:26:11 -0500

    button is working and looks decent

--- src/DashboardUI/src/pages/application/components/ApplicationReviewersNotesCreate.tsx
+++ src/DashboardUI/src/pages/application/components/ApplicationReviewersNotesCreate.tsx
@@ -1,7 +1,4 @@
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/esm/Form';
 import { useRef, useState } from 'react';
-import { InputGroup } from 'react-bootstrap';
 import { Icon } from '@mdi/react';
 import { mdiSend } from '@mdi/js';
 import { useMutation } from 'react-query';
@@ -10,32 +7,32 @@ import { createApplicationReviewerNote } from '../../../accessors/applicationAcc
 import { ApplicationReviewNote } from '../../../data-contracts/ApplicationReviewNote';
 import { toast } from 'react-toastify';
 import { useConservationApplicationContext } from '../../../contexts/ConservationApplicationProvider';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
 
 export interface ApplicationReviewersNotesCreateProps {
   applicationId: string;
 }
 
 export function ApplicationReviewersNotesCreate() {
-  const { state, dispatch } = useConservationApplicationContext();
-
   const context = useMsal();
-  const [validated, setValidated] = useState(false);
-
-  //   const [reviewerNotes, setReviewerNotes] = useState<string | undefined>(undefined);
+  const { state, dispatch } = useConservationApplicationContext();
+  const [reviewerNotes, setReviewerNotes] = useState<string | undefined>(undefined);
   const formRef = useRef<HTMLFormElement>(null);
-  const notesRef = useRef<HTMLTextAreaElement>(null);
 
   const createNoteMutation = useMutation({
     mutationFn: async () => {
       const response = await createApplicationReviewerNote(context, {
         applicationId: state.conservationApplication.waterConservationApplicationId!,
-        note: notesRef.current?.value!,
+        note: reviewerNotes!,
       });
       return response.note;
     },
     onSuccess: (note: ApplicationReviewNote) => {
       dispatch({ type: 'APPLICATION_NOTE_ADDED', payload: { note } });
-      //   setReviewerNotes(undefined);
+      setReviewerNotes(undefined);
+      formRef.current?.reset();
     },
     onError: () => {
       toast.error('Error adding application note. Please try again.');
@@ -43,36 +40,22 @@ export function ApplicationReviewersNotesCreate() {
   });
 
   const handleAddNoteClick = () => {
-    // if (!state.conservationApplication.waterConservationApplicationId) {
-    //   throw new Error('Application must be loaded in order to submit reviewer notes');
-    // }
-
-    // if (!reviewerNotes || reviewerNotes.trim().length === 0) {
-    //   throw new Error('Reviewer notes cannot be empty');
-    // }
-    const isFormValid = formRef.current!.checkValidity();
-    setValidated(true);
-    console.log(`form validitiy: ${JSON.stringify(formRef.current?.checkValidity())}`);
-    if (isFormValid) {
-      createNoteMutation.mutate();
-      formRef.current?.reset();
-      setValidated(false);
+    if (!state.conservationApplication.waterConservationApplicationId) {
+      throw new Error('Application must be loaded in order to submit reviewer notes');
     }
+
+    if (!reviewerNotes || reviewerNotes.trim().length === 0) {
+      throw new Error('Reviewer notes cannot be empty');
+    }
+
+    createNoteMutation.mutate();
   };
 
   return (
     <>
-      {'is Validated? ' + validated}
-      <br />
-      {'is form valid? ' + formRef.current?.checkValidity()}
-      <Form
-        noValidate
-        ref={formRef}
-        validated={validated}
-        //   onChange={() => setValidated(false)}
-      >
+      <Form ref={formRef}>
         <Form.Group>
-          <InputGroup>
+          <InputGroup className="mb-3">
             <Form.Control
               required
               id="application-reviewer-notes"
@@ -82,21 +65,17 @@ export function ApplicationReviewersNotesCreate() {
               as="textarea"
               type="text"
               maxLength={4000}
-              ref={notesRef}
-              //   onChange={(e) => setReviewerNotes(e.target.value)}
+              onChange={(e) => setReviewerNotes(e.target.value)}
             />
-            <Form.Control.Feedback type="invalid">A message is required.</Form.Control.Feedback>
-            <InputGroup.Text className="py-0">
-              <Button
-                onClick={handleAddNoteClick}
-                disabled={createNoteMutation.isLoading}
-                variant="link"
-                className="px-1 py-0 text-primary"
-                aria-label="Create reviewer note"
-              >
-                <Icon path={mdiSend} size="1em" />
-              </Button>
-            </InputGroup.Text>
+            <Button
+              variant="outline-primary"
+              className="border-left-0"
+              aria-label="Create new reviewer note"
+              disabled={!reviewerNotes || reviewerNotes.trim().length === 0 || createNoteMutation.isLoading}
+              onClick={handleAddNoteClick}
+            >
+              <Icon path={mdiSend} size="1em" />
+            </Button>
           </InputGroup>
         </Form.Group>
       </Form>
