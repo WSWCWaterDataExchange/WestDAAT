a65cff2a03c887df0661b9d621fab4b8cba9f325
Ethan Weber <eweber@dontpaniclabs.com>
2025-04-16 17:07:23 -0500

    separate actions depending on page

--- src/DashboardUI/src/pages/application/review/ApplicationReviewPage.tsx
+++ src/DashboardUI/src/pages/application/review/ApplicationReviewPage.tsx
@@ -2,7 +2,11 @@ import { useMemo } from 'react';
 import Alert from 'react-bootstrap/esm/Alert';
 import { Outlet, useLocation, useNavigate, useParams } from 'react-router-dom';
 import { useConservationApplicationContext } from '../../../contexts/ConservationApplicationProvider';
-import { useFundingOrganizationQuery, useGetApplicationQuery } from '../../../hooks/queries/useApplicationQuery';
+import {
+  useFundingOrganizationQuery,
+  useGetApplicationQuery,
+  useReviewerEstimateConsumptiveUseMutation,
+} from '../../../hooks/queries/useApplicationQuery';
 import { ApplicationNavbar } from '../components/ApplicationNavbar';
 import ApplicationReviewHeader from '../components/ApplicationReviewHeader';
 import { useAuthenticationContext } from '../../../hooks/useAuthenticationContext';
@@ -31,16 +35,41 @@ function ApplicationReviewPage() {
     return isOnMapPage ? 'Back to Application' : 'Back to Dashboard';
   }, [isOnMapPage]);
 
+  const rightButtonText: string = useMemo(() => {
+    return isOnMapPage ? 'Save Updated Estimate' : 'Continue to Final Approval';
+  }, [isOnMapPage]);
+
   useGetApplicationQuery(applicationId, 'reviewer', true);
   useFundingOrganizationQuery(state.conservationApplication.waterRightNativeId);
 
   const canApproveApplication = hasPermission(user, Permission.ApplicationApprove);
+
+  const rightButtonDisplayed = useMemo(() => {
+    return isOnMapPage ? true : canApproveApplication;
+  }, [isOnMapPage, state.canEstimateConsumptiveUse, canApproveApplication]);
 
   const navigateToApprovalPage = () => {
     const id = state.conservationApplication.waterConservationApplicationId;
     if (id) {
       navigate(`/application/${id}/approve`);
     }
+  };
+
+  const reviewerEstimateConsumptiveUseMutation = useReviewerEstimateConsumptiveUseMutation();
+
+  const onRightButtonClick = async () => {
+    if (isOnMapPage) {
+      // todo: why does this not share the mutation state with the map page?
+      return await reviewerEstimateConsumptiveUse();
+    } else {
+      return navigateToApprovalPage();
+    }
+  };
+
+  const reviewerEstimateConsumptiveUse = async () => {
+    return await reviewerEstimateConsumptiveUseMutation.mutateAsync({
+      updateEstimate: false,
+    });
   };
 
   return (
@@ -51,10 +80,10 @@ function ApplicationReviewPage() {
         centerText="Application Review"
         centerTextIsLoading={false}
         displayWaterIcon={false}
-        rightButtonDisplayed={canApproveApplication}
-        rightButtonText="Continue to Final Approval"
+        rightButtonDisplayed={rightButtonDisplayed}
+        rightButtonText={rightButtonText}
         rightButtonIcon={mdiArrowRight}
-        onRightButtonClick={navigateToApprovalPage}
+        onRightButtonClick={onRightButtonClick}
       />
 
       <div className="overflow-y-auto">
