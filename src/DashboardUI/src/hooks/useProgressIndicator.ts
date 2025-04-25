2d36dfed3998825812eea34a757ae619cb07a88f
Jenny Nielsen <jnielsen@dontpaniclabs.com>
2025-04-25 12:54:00 -0500

    kind of works but not great

--- src/DashboardUI/src/hooks/useProgressIndicator.ts
+++ src/DashboardUI/src/hooks/useProgressIndicator.ts
@@ -1,9 +1,8 @@
-import { useEffect, useMemo, useRef } from 'react';
-import { toast, ToastContent } from 'react-toastify';
+import { useEffect, useMemo } from 'react';
+import { Id, toast, ToastContent } from 'react-toastify';
 
 function useProgressIndicator(progress: number | boolean[], content: ToastContent) {
-  const toastContainer = document.querySelector('#app-toast-container');
-  const loadingFilterDataToast = useRef<string | number | null>(null);
+  let toastId: Id | null = null;
 
   const calculatedProgress = useMemo(() => {
     if (Array.isArray(progress)) {
@@ -13,27 +12,20 @@ function useProgressIndicator(progress: number | boolean[], content: ToastConten
   }, [progress]);
 
   useEffect(() => {
-    // Wait for the toast container to be available in the dom.
-    // If you don't, then toast messages on initial page load will not show.
-    if (toastContainer == null) {
-      return;
+    if (toastId === null) {
+      toastId = toast.info(content);
+    } else {
+      if (calculatedProgress < 1) {
+        toast.update(toastId, {
+          progress: calculatedProgress,
+          autoClose: false,
+          type: 'info',
+          theme: 'colored',
+        });
+      } else {
+        toast.dismiss(toastId);
+      }
     }
-
-    if (loadingFilterDataToast.current == null && calculatedProgress < 1) {
-      loadingFilterDataToast.current = toast.info(content, {
-        progress: calculatedProgress,
-        autoClose: false,
-        type: 'info',
-        theme: 'colored',
-      });
-    } else if (loadingFilterDataToast.current != null && calculatedProgress < 1) {
-      toast.update(loadingFilterDataToast.current!, {
-        progress: calculatedProgress,
-      });
-    } else if (loadingFilterDataToast.current != null) {
-      toast.dismiss(loadingFilterDataToast.current!);
-      loadingFilterDataToast.current = null;
-    }
-  }, [calculatedProgress, content, toastContainer]);
+  }, [calculatedProgress, content, toastId]);
 }
 export default useProgressIndicator;
