523a68400b2a24a2c74497974007f28cf1ce4c23
Branden Barber <bbarber@dontpaniclabs.com>
2025-04-28 08:29:33 -0500

    Handle load error

--- src/DashboardUI/src/pages/application/components/ApplicationStaticMap.tsx
+++ src/DashboardUI/src/pages/application/components/ApplicationStaticMap.tsx
@@ -1,8 +1,21 @@
+import { useState } from 'react';
+import Alert from 'react-bootstrap/esm/Alert';
+
 interface ApplicationStaticMapProps {
   mapImageUrl: string | undefined;
 }
 function ApplicationStaticMap(props: ApplicationStaticMapProps) {
-  return <img src={props.mapImageUrl} alt="Map" className="w-100" />;
+  const [imageLoadErrored, setImageLoadError] = useState(false);
+
+  if (imageLoadErrored) {
+    return (
+      <Alert variant="warning" className="fw-bold  d-flex justify-content-center">
+        Map image failed to load
+      </Alert>
+    );
+  }
+
+  return <img src={props.mapImageUrl} alt="Map" className="w-100" onError={() => setImageLoadError(true)} />;
 }
 
 export default ApplicationStaticMap;
