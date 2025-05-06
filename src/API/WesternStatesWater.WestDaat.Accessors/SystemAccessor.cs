81c12872c86a8011781aaf45be98c20b97c7b856
Jack Nolley <jnolley@dontpaniclabs.com>
2025-05-06 09:54:55 -0500

    One call to DB

--- src/API/WesternStatesWater.WestDaat.Accessors/SystemAccessor.cs
+++ src/API/WesternStatesWater.WestDaat.Accessors/SystemAccessor.cs
@@ -9,7 +9,9 @@ namespace WesternStatesWater.WestDaat.Accessors
     {
         private readonly EF.IDatabaseContextFactory _databaseContextFactory;
 
-        public SystemAccessor(ILogger<SystemAccessor> logger, EF.IDatabaseContextFactory databaseContextFactory)
+        public SystemAccessor(
+            ILogger<SystemAccessor> logger,
+            EF.IDatabaseContextFactory databaseContextFactory)
             : base(logger)
         {
             _databaseContextFactory = databaseContextFactory;
@@ -17,23 +19,69 @@ namespace WesternStatesWater.WestDaat.Accessors
 
         public async Task<DashboardFilters> LoadFilters()
         {
-            var overlayTypes = await GetFilterValues("OverlayTypes");
-            var overlayWaterSources = await GetFilterValues("OverlayWaterSources");
-            var overlayStates = await GetFilterValues("OverlayStates");
+            var filterTypes = new[]
+            {
+                FilterTypeConstants.OverlayTypes,
+                FilterTypeConstants.OverlayWaterSources,
+                FilterTypeConstants.OverlayStates,
+                FilterTypeConstants.WaterRightOwnerClassifications,
+                FilterTypeConstants.WaterRightAllocationTypes,
+                FilterTypeConstants.WaterRightLegalStatuses,
+                FilterTypeConstants.WaterRightSiteTypes,
+                FilterTypeConstants.WaterRightWaterSources,
+                FilterTypeConstants.WaterRightStates,
+                FilterTypeConstants.TimeSeriesSiteTypes,
+                FilterTypeConstants.TimeSeriesPrimaryUses,
+                FilterTypeConstants.TimeSeriesVariableTypes,
+                FilterTypeConstants.TimeSeriesWaterSources,
+                FilterTypeConstants.TimeSeriesStates
+            };
+
+            await using var db = _databaseContextFactory.Create();
+            var rawFilters = await db.Filters
+                .AsNoTracking()
+                .Where(f => filterTypes.Contains(f.FilterType)
+                            && !string.IsNullOrWhiteSpace(f.WaDeName))
+                .Select(f => new
+                {
+                    f.FilterType,
+                    Name = f.WaDeName.Trim()
+                })
+                .Distinct()
+                .ToListAsync();
+
+            var lookup = rawFilters
+                .GroupBy(x => x.FilterType)
+                .ToDictionary(
+                    g => g.Key,
+                    g => g.Select(x => x.Name)
+                          .OrderBy(n => n)
+                          .ToArray());
+
+            string[] Get(string key) =>
+                lookup.TryGetValue(key, out var arr)
+                    ? arr
+                    : Array.Empty<string>();
+
+            var overlayTypes = Get(FilterTypeConstants.OverlayTypes);
+            var overlayWaterSources = Get(FilterTypeConstants.OverlayWaterSources);
+            var overlayStates = Get(FilterTypeConstants.OverlayStates);
+
+            var ownerClassifications = Get(FilterTypeConstants.WaterRightOwnerClassifications);
+            var allocationTypes = Get(FilterTypeConstants.WaterRightAllocationTypes);
+            var legalStatuses = Get(FilterTypeConstants.WaterRightLegalStatuses);
+            var siteTypes = Get(FilterTypeConstants.WaterRightSiteTypes);
+            var waterSources = Get(FilterTypeConstants.WaterRightWaterSources);
+            var wrStates = Get(FilterTypeConstants.WaterRightStates);
+
+            var tsSiteTypes = Get(FilterTypeConstants.TimeSeriesSiteTypes);
+            var tsPrimaryUses = Get(FilterTypeConstants.TimeSeriesPrimaryUses);
+            var tsVariableTypes = Get(FilterTypeConstants.TimeSeriesVariableTypes);
+            var tsWaterSources = Get(FilterTypeConstants.TimeSeriesWaterSources);
+            var tsStates = Get(FilterTypeConstants.TimeSeriesStates);
+
 
             var beneficialUses = await GetBeneficialUses();
-            var ownerClassifications = await GetFilterValues("WaterRightOwnerClassifications");
-            var allocationTypes = await GetFilterValues("WaterRightAllocationTypes");
-            var legalStatuses = await GetFilterValues("WaterRightLegalStatuses");
-            var siteTypes = await GetFilterValues("WaterRightSiteTypes");
-            var waterSources = await GetFilterValues("WaterRightWaterSources");
-            var wrStates = await GetFilterValues("WaterRightStates");
-
-            var tsSiteTypes = await GetFilterValues("TimeSeriesSiteTypes");
-            var tsPrimaryUses = await GetFilterValues("TimeSeriesPrimaryUses");
-            var tsVariableTypes = await GetFilterValues("TimeSeriesVariableTypes");
-            var tsWaterSources = await GetFilterValues("TimeSeriesWaterSources");
-            var tsStates = await GetFilterValues("TimeSeriesStates");
 
             return new DashboardFilters
             {
@@ -65,18 +113,6 @@ namespace WesternStatesWater.WestDaat.Accessors
             };
         }
 
-        private async Task<string[]> GetFilterValues(string filterType)
-        {
-            await using var db = _databaseContextFactory.Create();
-            return await db.Filters
-                .AsNoTracking()
-                .Where(f => f.FilterType == filterType && !string.IsNullOrWhiteSpace(f.WaDeName))
-                .Select(f => f.WaDeName.Trim())
-                .Distinct()
-                .OrderBy(f => f)
-                .ToArrayAsync();
-        }
-
         private async Task<BeneficialUseItem[]> GetBeneficialUses()
         {
             await using var db = _databaseContextFactory.Create();
@@ -89,7 +125,7 @@ namespace WesternStatesWater.WestDaat.Accessors
                 {
                     Name = !string.IsNullOrWhiteSpace(cv.WaDEName) ? cv.WaDEName : cv.Name,
                     Category = cv.ConsumptionCategoryType.ToString()
-                        ?? Common.ConsumptionCategory.Unspecified.ToString()
+                               ?? Common.ConsumptionCategory.Unspecified.ToString()
                 })
                 .ToListAsync();
 
@@ -99,8 +135,8 @@ namespace WesternStatesWater.WestDaat.Accessors
                 {
                     BeneficialUseName = g.Key,
                     ConsumptionCategory = g.Max(x =>
-                        Enum.TryParse<Common.ConsumptionCategory>(x.Category, out var category)
-                            ? category
+                        Enum.TryParse<Common.ConsumptionCategory>(x.Category, out var cat)
+                            ? cat
                             : Common.ConsumptionCategory.Unspecified)
                 })
                 .OrderBy(x => x.BeneficialUseName)
