f63949d9ef3e3417dfb5eb6cb7cc52b81d0c3046
Jack Nolley <jnolley@dontpaniclabs.com>
2025-05-22 13:12:54 -0500

    Fixed to make it work like the other ones

--- src/DashboardUI/src/components/home-page/water-rights-tab/map-options/hooks/useAnalyticsGroupingUrlParameter.ts
+++ src/DashboardUI/src/components/home-page/water-rights-tab/map-options/hooks/useAnalyticsGroupingUrlParameter.ts
@@ -1,45 +1,9 @@
-import { useCallback } from 'react';
-import { useLocation } from 'react-router-dom';
+import urlParameterKeys from '../../../../../hooks/url-parameters/urlParameterKeys';
+import { useUrlParameters } from '../../../../../hooks/url-parameters/useUrlParameters';
 import { DropdownOption } from '../../../../../data-contracts/DropdownOption';
 
-export const useAnalyticsGroupingUrlParameter = () => {
-  const location = useLocation();
+const paramName = urlParameterKeys.homePage.analyticsGrouping;
 
-  const getParameter = useCallback(() => {
-    const query = new URLSearchParams(location.search);
-    const groupingParam = query.get('analyticsGrouping');
-
-    if (groupingParam) {
-      try {
-        return JSON.parse(decodeURIComponent(groupingParam)) as DropdownOption;
-      } catch (e) {
-        console.error('Error parsing analyticsGrouping parameter', e);
-      }
-    }
-    return null;
-  }, [location.search]);
-
-  const setParameter = useCallback(
-    (analyticsGroupingOption: DropdownOption | null) => {
-      // Create a new URL search params instance
-      const query = new URLSearchParams(location.search);
-
-      if (analyticsGroupingOption) {
-        const serialized = encodeURIComponent(JSON.stringify(analyticsGroupingOption));
-        query.set('analyticsGrouping', serialized);
-      } else {
-        query.delete('analyticsGrouping');
-      }
-
-      // Update URL without using history directly
-      window.history.replaceState(
-        {},
-        '',
-        `${location.pathname}${query.toString() ? `?${query.toString()}` : ''}`
-      );
-    },
-    [location]
-  );
-
-  return { getParameter, setParameter };
-};
+export function useAnalyticsGroupingUrlParameter() {
+  return useUrlParameters<DropdownOption | null>(paramName, null);
+}
